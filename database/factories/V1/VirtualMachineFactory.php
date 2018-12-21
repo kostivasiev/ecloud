7b7fac71fed1e723204c61b7337b6f36fdf32b60
Umair Majeed <umairbmajeed@gmail.com>
2018-12-21 12:30:03 +0000

    tweak server active to make always active

--- database/factories/V1/VirtualMachineFactory.php
+++ database/factories/V1/VirtualMachineFactory.php
@@ -49,7 +49,6 @@ $factory->define(App\Models\V1\VirtualMachine::class, function (Faker\Generator 
         'servers_reseller_id' => 1,
         'servers_type' => 'ecloud vm',
         'servers_subtype_id' => 1,
-        'servers_active' => (rand(1, 10) > 1) ? 'y' : 'n',  // active 90% of 10 times
         'servers_ecloud_ucs_reseller_id' => 1, //999999
         'servers_friendly_name' => $faker->sentence(2),
         'servers_hostname' => '172.16.28.173.srvlist.ukfast.net',
