36f6d0f8fbf2816208f5aa059478b7ddb8015db2
Umair Majeed <umairbmajeed@gmail.com>
2019-01-04 16:56:57 +0000

    please code sniffer + disables pod create tage unit test

--- app/Http/Controllers/V1/PodController.php
+++ app/Http/Controllers/V1/PodController.php
@@ -81,11 +81,10 @@ class PodController extends BaseController
         if (!$request->user->isAdmin) {
             $podQuery->where('ucs_datacentre_active', 'Yes');
 
-            $podQuery->where(function($query) use ($request) {
+            $podQuery->where(function ($query) use ($request) {
                 $query->where('ucs_datacentre_reseller_id', '=', '0')
                     ->orWhere('ucs_datacentre_reseller_id', '=', $request->user->resellerId);
             });
-
         }
         return $podQuery;
     }
