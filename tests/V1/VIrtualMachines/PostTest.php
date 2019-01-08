bc4e91021fc31efde6e7dbb5a737078d72231199
Umair Majeed <umairbmajeed@gmail.com>
2019-01-08 10:56:16 +0000

    add test for disabled public VM create

--- tests/V1/VIrtualMachines/PostTest.php
+++ tests/V1/VIrtualMachines/PostTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 
 use App\Models\V1\VirtualMachine;
+//use App\Models\V1\Solution;
 
 class PostTest extends TestCase
 {
@@ -15,6 +16,46 @@ class PostTest extends TestCase
     {
         parent::setUp();
     }
+
+    public function testPublicCreateDisabled()
+    {
+        $this->json('POST', '/v1/vms/', [
+            'environment' => 'Public',
+        ], [
+            'X-consumer-custom-id' => '1-1',
+            'X-consumer-groups' => 'ecloud.write',
+        ]);
+
+        $this->assertResponseStatus(403);
+    }
+
+//    todo need to mock kingpin/intapi services
+//    public function testHybridCreate()
+//    {
+//        $this->missingFromDatabase('servers', [
+//            'servers_id' => 123,
+//        ]);
+//
+//        factory(Solution::class, 1)->create([
+//            'ucs_reseller_id' => 12345,
+//        ]);
+//
+//        $this->json('POST', '/v1/vms/', [
+//            'environment' => 'Hybrid',
+//            'template' => 'CentOS 7 64-bit',
+//            'solution_id' => '12345',
+//            'cpu' => 1,
+//            'ram' => 2,
+//            'hdd' => 30,
+//        ], [
+//            'X-consumer-custom-id' => '1-1',
+//            'X-consumer-groups' => 'ecloud.write',
+//        ]);
+//
+//        dd($this->response->getContent());
+//
+//        $this->assertResponseStatus(403);
+//    }
 
     public function testPublicCloneDisabled()
     {
