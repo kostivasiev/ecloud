36dfc0a4d56b3e6d916bfd03b66955c4db0da875
Umair Majeed <umairbmajeed@gmail.com>
2019-04-01 17:28:53 +0100

    ssh validation regex updated to be compatible with ed25519 SSH keys

--- app/Rules/V1/IsVaidSSHPublicKey.php
+++ app/Rules/V1/IsVaidSSHPublicKey.php
@@ -13,7 +13,7 @@ class IsValidSSHPublicKey implements Rule
 {
     public function passes($attribute, $value)
     {
-        $publicKeyFormatRegex = '^(ssh-[a-z]{3}) (?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?(?:.*)$';
+        $publicKeyFormatRegex = '^(ssh-[[:alnum:]]{3,7}) (?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?(?:.*)$';
 
         return (preg_match('/' . $publicKeyFormatRegex . '/', $value) === 1);
     }
