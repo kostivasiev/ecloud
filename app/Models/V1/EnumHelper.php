5de63e00c145b7ee048c8c6dd841977714773afa
Umair Majeed <umairbmajeed@gmail.com>
2019-07-01 12:57:51 +0100

    Only loop if there are enum values

--- app/Models/V1/EnumHelper.php
+++ app/Models/V1/EnumHelper.php
@@ -18,11 +18,12 @@ trait EnumHelper
     public static function getEnumValues($name)
     {
         $type = DB::select(DB::raw('SHOW COLUMNS FROM ' . (new static)->getTable() . ' WHERE Field = "' . $name . '"'))[0]->Type;
-        preg_match('/^enum\((.*)\)$/', $type, $matches);
-        $enum = array();
-        foreach (explode(',', $matches[1]) as $value) {
-            $v = trim($value, "'");
-            $enum[] = $v;
+        $enum = [];
+        if (preg_match('/^enum\((.*)\)$/', $type, $matches) === 1) {
+            foreach (explode(',', $matches[1]) as $value) {
+                $v = trim($value, "'");
+                $enum[] = $v;
+            }
         }
         return $enum;
     }
