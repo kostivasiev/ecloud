3b86416ae4bdcf783e98f6567be0464a7b321456
Umair Majeed <umairbmajeed@gmail.com>
2019-07-12 11:53:12 +0100

    servers_gpu_profile_uuid column updated to servers_ecloud_gpu_profile_uuid

--- app/Resources/V1/VirtualMachineResource.php
+++ app/Resources/V1/VirtualMachineResource.php
@@ -115,7 +115,7 @@ class VirtualMachineResource extends CustomResource
         ];
 
         if ($this->resource->servers_ecloud_type == 'GPU') {
-            $data['gpu_profile'] = $this->resource->servers_gpu_profile_uuid;
+            $data['gpu_profile'] = $this->resource->servers_ecloud_gpu_profile_uuid;
         }
 
         return $this->filterProperties($request, $data);
