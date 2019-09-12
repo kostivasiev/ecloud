bb5876b11a3225abdb4cfd4570d7a5c6afd35526
Umair Majeed <umairbmajeed@gmail.com>
2019-09-12 15:18:04 +0100

    Add host to host set endpoint

--- app/Http/Controllers/V1/HostSetController.php
+++ app/Http/Controllers/V1/HostSetController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\V1;
 use App\Exceptions\V1\ArtisanException;
 use App\Models\V1\HostSet;
 use App\Models\V1\Solution;
+use App\Rules\V1\IsValidUuid;
 use App\Services\Artisan\V1\ArtisanService;
 use Illuminate\Http\Request;
+use UKFast\Api\Exceptions\UnprocessableEntityException;
 
 class HostSetController extends BaseController
 {
@@ -14,7 +16,7 @@ class HostSetController extends BaseController
 
     /**
      * Create a host set
-     * When creating a host set we need to create it on all SAN's on teh Pod for the reseller's solution
+     * When creating a host set we need to create it on all SAN's on the Pod for the reseller's solution
      * @param Request $request
      * @return \Illuminate\Http\Response
      * @throws ArtisanException
@@ -51,6 +53,43 @@ class HostSetController extends BaseController
         return $this->respondSave($request, $hostSet, 201);
     }
 
+    /**
+     * Add a host to a host set
+     * We need to add the host to the host set  on all SAN's on the Pod for the reseller's solution
+     * @param Request $request
+     * @param $hostSetId
+     * @return \Illuminate\Http\Response
+     * @throws UnprocessableEntityException
+     * @throws \App\Exceptions\V1\HostNotFoundException
+     */
+    public function addHost(Request $request, $hostSetId)
+    {
+        $rules = [
+            'host_set_id' => ['required', new IsValidUuid()],
+            'host_id' => ['required', 'integer']
+        ];
+        $request['host_set_id'] = $hostSetId;
+        $this->validate($request, $rules);
+        $hostSet = HostSetController::getById($request, $hostSetId);
+        $host = HostController::getHostById($request, $request->input('host_id'));
+
+        if (empty($host->ucs_node_internal_name)) {
+            throw new UnprocessableEntityException('Host is not mapped to a SAN entity');
+        }
+
+        $solution = $host->solution;
+        $solution->pod->sans->each(function ($san) use ($solution, $hostSet, $host) {
+            $artisan = app()->makeWith(ArtisanService::class, [['solution'=>$solution, 'san' => $san]]);
+
+            $artisaResponse = $artisan->addHostToHostSet($hostSet->name, $host->ucs_node_internal_name);
+            if (!$artisaResponse) {
+                throw new ArtisanException('Failed to add host to host set: ' . $artisan->getLastError());
+            }
+        });
+
+        return $this->respondEmpty();
+    }
+
 
     /**
      * Host sets are of the format MCS_G0_SET_17106_(x),  where x is an increment for the solution.
@@ -75,7 +114,6 @@ class HostSetController extends BaseController
 
         return ++$index;
     }
-
 
     /**
      * get item by ID
