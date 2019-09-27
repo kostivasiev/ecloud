6ae8ab39fee34dd3238575782b9510cb3e91477c
Umair Majeed <umairbmajeed@gmail.com>
2019-09-27 10:09:27 +0100

    CI/CD file changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,8 +22,9 @@ $app = new Laravel\Lumen\Application(
 );
 
 $app->configure('database');
-
+$app->configure('logging');
 $app->configure('mail');
+
 $app->alias('mailer', Illuminate\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
