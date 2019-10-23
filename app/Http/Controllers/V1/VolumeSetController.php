523366d99c5e7d68fda8b1f2ce6ae36cb72bf719
Umair Majeed <umairbmajeed@gmail.com>
2019-10-23 11:52:10 +0100

    removes volume set empty check on delete volume set record

--- app/Http/Controllers/V1/VolumeSetController.php
+++ app/Http/Controllers/V1/VolumeSetController.php
@@ -288,8 +288,7 @@ class VolumeSetController extends BaseController
 
 
     /**
-     * Delete volume set record. Check the SAN to see if the volume set contains any volumes to
-     * prevent delete of non empty volume sets.
+     * Delete volume set record.
      * @param Request $request
      * @param $volumeSetId
      * @return \Illuminate\Http\Response
@@ -301,27 +300,6 @@ class VolumeSetController extends BaseController
         $this->validate($request, $rules);
 
         $volumeSet = static::getById($request, $volumeSetId);
-
-        $solution = $volumeSet->solution;
-        // Check if the volume set is empty, as we cant target a specific san we'll need to check all the sans for the solutions pod
-        $solution->pod->sans->each(function ($san) use ($solution, $volumeSet) {
-            $artisan = app()->makeWith(ArtisanService::class, [['solution'=>$solution, 'san' => $san]]);
-
-            $artisanResponse = $artisan->getVolumeSet($volumeSet->name);
-
-            if (!$artisanResponse) {
-                $error = $artisan->getLastError();
-                if (strpos($error, 'Set does not exist') !== false) {
-                    return true; //continue to the next san
-                }
-
-                throw new ArtisanException('Failed to load volume set: ' . $error);
-            }
-
-            if (!empty($artisanResponse->volumes)) {
-                throw new ArtisanException('Failed to delete volume set. The volume set is not empty.');
-            }
-        });
 
         $volumeSet->delete();
 
