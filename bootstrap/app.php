2b0c5d95a3f7a59df4761dde61c317b98a5796c2
Umair Majeed <umairbmajeed@gmail.com>
2020-02-26 11:04:46 +0000

    enable launching public VMs via the api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -86,18 +86,23 @@ $app->routeMiddleware([
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
 $app->register(Illuminate\Mail\MailServiceProvider::class);
 $app->register(App\Providers\PaginationServiceProvider::class);
-$app->register(App\Providers\KingpinServiceProvider::class);
-$app->register(App\Providers\NetworkingServiceProvider::class);
-$app->register(App\Providers\AccountsServiceProvider::class);
-$app->register(App\Providers\IntapiServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
+
+// ukfast
+$app->register(App\Providers\IntapiServiceProvider::class);
 $app->register(UKFast\HealthCheck\HealthCheckServiceProvider::class);
 $app->register(UKFast\Api\Auth\Providers\AuthServiceProvider::class);
 $app->register(UKFast\Api\Exceptions\Providers\UKFastExceptionServiceProvider::class);
 $app->register(UKFast\Api\Resource\ResourceServiceProvider::class);
 
-// Artisan service provider
+// ecloud service providers
+$app->register(App\Providers\KingpinServiceProvider::class);
 $app->register(App\Providers\ArtisanServiceProvider::class);
+
+// apio service providers
+$app->register(App\Providers\NetworkingServiceProvider::class);
+$app->register(App\Providers\AccountsServiceProvider::class);
+$app->register(App\Providers\BillingServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
