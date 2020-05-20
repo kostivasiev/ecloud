472af4e62fe5321d24d2e0e02724232d80a9799c
Umair Majeed <umairbmajeed@gmail.com>
2020-05-20 12:26:25 +0100

    Tweaked host definition to include hardwareVersion
    Relates to #263

--- app/Models/V1/Host.php
+++ app/Models/V1/Host.php
@@ -418,6 +418,7 @@ class Host extends Model implements Filterable, Sortable
             'specification' => $responseObj->specification ?? '',
             'name' => $responseObj->name ?? '',
             'interfaces' => $interfaces ?? '',
+            'hardwareVersion' => $responseObj->hardwareVersion ?? '',
         ];
     }
 
