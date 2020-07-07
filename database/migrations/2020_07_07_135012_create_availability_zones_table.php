a14c1a5f318d0871badc99240b988409a0858a18
Umair Majeed <umairbmajeed@gmail.com>
2020-07-07 16:45:05 +0100

    Initial migration to create availability_zones table
    Relates to #271

---
+++ database/migrations/2020_07_07_135012_create_availability_zones_table.php
@@ -0,0 +1,36 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvailabilityZonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availability_zones', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('code');
+            $table->string('name');
+            $table->integer('site_id', false)->index();
+            $table->timestamp('created_at', 0)->useCurrent();
+            $table->timestamp('updated_at', 0)
+                ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('availability_zones');
+    }
+}
