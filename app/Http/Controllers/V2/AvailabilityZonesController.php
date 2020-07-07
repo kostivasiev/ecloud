404a26b518e4064630add5caaf0044180f3bbca2
Umair Majeed <umairbmajeed@gmail.com>
2020-07-07 23:02:32 +0100

    Have added additional code to fire before and after events for Create, Update and Delete methods. The event classes themselves don't currently do anything, so there's nothing to test with these.
    Relates to #271

--- app/Http/Controllers/V2/AvailabilityZonesController.php
+++ app/Http/Controllers/V2/AvailabilityZonesController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers\V2;
 
+use App\Events\V2\AvailabilityZones\AfterCreateEvent;
+use App\Events\V2\AvailabilityZones\AfterDeleteEvent;
+use App\Events\V2\AvailabilityZones\AfterUpdateEvent;
+use App\Events\V2\AvailabilityZones\BeforeCreateEvent;
+use App\Events\V2\AvailabilityZones\BeforeDeleteEvent;
+use App\Events\V2\AvailabilityZones\BeforeUpdateEvent;
 use App\Http\Requests\V2\CreateAvailabilityZonesRequest;
 use App\Http\Requests\V2\UpdateAvailabilityZonesRequest;
 use App\Models\V2\AvailabilityZones;
@@ -56,11 +62,13 @@ class AvailabilityZonesController extends BaseController
      */
     public function create(CreateAvailabilityZonesRequest $request)
     {
+        event(new BeforeCreateEvent());
         $availabilityZone = new AvailabilityZones($request->only([
             'code', 'name', 'site_id',
         ]));
         $availabilityZone->save();
         $availabilityZone->refresh();
+        event(new AfterCreateEvent());
         return $this->responseIdMeta($request, $availabilityZone->getKey(), 201);
     }
 
@@ -71,11 +79,13 @@ class AvailabilityZonesController extends BaseController
      */
     public function update(UpdateAvailabilityZonesRequest $request, string $zoneId)
     {
+        event(new BeforeUpdateEvent());
         $availabilityZone = AvailabilityZones::findOrFail($zoneId);
         $availabilityZone->fill($request->only([
             'code', 'name', 'site_id',
         ]));
         $availabilityZone->save();
+        event(new AfterUpdateEvent());
         return $this->responseIdMeta($request, $availabilityZone->getKey(), 202);
     }
 
@@ -86,8 +96,10 @@ class AvailabilityZonesController extends BaseController
      */
     public function destroy(Request $request, string $zoneId)
     {
+        event(new BeforeDeleteEvent());
         $availabilityZone = AvailabilityZones::findOrFail($zoneId);
         $availabilityZone->delete();
+        event(new AfterDeleteEvent());
         return response()->json([], 204);
     }
 }
