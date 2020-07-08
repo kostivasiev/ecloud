7122d154dfd92f825b074ad95329c31bad2c5be1
Umair Majeed <umairbmajeed@gmail.com>
2020-07-08 16:00:08 +0100

    Virtual Data Centre Model
    Relates to #272

---
+++ app/Models/V2/VirtualDataCentres.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Models\V2;
+
+use App\Traits\V2\UUIDHelper;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use UKFast\Api\Resource\Property\DateTimeProperty;
+use UKFast\Api\Resource\Property\IdProperty;
+use UKFast\Api\Resource\Property\StringProperty;
+use UKFast\DB\Ditto\Factories\FilterFactory;
+use UKFast\DB\Ditto\Factories\SortFactory;
+use UKFast\DB\Ditto\Filter;
+use UKFast\DB\Ditto\Filterable;
+use UKFast\DB\Ditto\Sortable;
+
+/**
+ * Class VirtualDataCentres
+ * @package App\Models\V2
+ * @method static findOrFail(string $vdcUuid)
+ */
+class VirtualDataCentres extends Model implements Filterable, Sortable
+{
+    use UUIDHelper, SoftDeletes;
+
+    protected $connection = 'ecloud';
+    protected $table = 'virtual_data_centre';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'name'];
+    protected $visible = ['id', 'name', 'created_at', 'updated_at'];
+
+    public $incrementing = false;
+    public $timestamps = true;
+
+    /**
+     * @param \UKFast\DB\Ditto\Factories\FilterFactory $factory
+     * @return array|\UKFast\DB\Ditto\Filter[]
+     */
+    public function filterableColumns(FilterFactory $factory)
+    {
+        return [
+            $factory->create('id', Filter::$stringDefaults),
+            $factory->create('name', Filter::$stringDefaults),
+            $factory->create('created_at', Filter::$dateDefaults),
+            $factory->create('updated_at', Filter::$dateDefaults)
+        ];
+    }
+
+    /**
+     * @param \UKFast\DB\Ditto\Factories\SortFactory $factory
+     * @return array|\UKFast\DB\Ditto\Sort[]
+     * @throws \UKFast\DB\Ditto\Exceptions\InvalidSortException
+     */
+    public function sortableColumns(SortFactory $factory)
+    {
+        return [
+            $factory->create('id'),
+            $factory->create('name'),
+            $factory->create('created_at'),
+            $factory->create('updated_at')
+        ];
+    }
+
+    /**
+     * @param \UKFast\DB\Ditto\Factories\SortFactory $factory
+     * @return array|\UKFast\DB\Ditto\Sort|\UKFast\DB\Ditto\Sort[]|null
+     * @throws \UKFast\DB\Ditto\Exceptions\InvalidSortException
+     */
+    public function defaultSort(SortFactory $factory)
+    {
+        return [
+            $factory->create('name', 'asc'),
+        ];
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function databaseNames()
+    {
+        return [
+            'id'         => 'id',
+            'name'       => 'name',
+            'created_at' => 'appliance_created_at',
+            'updated_at' => 'appliance_updated_at',
+        ];
+    }
+
+    /**
+     * @return array
+     * @throws \UKFast\Api\Resource\Exceptions\InvalidPropertyException
+     */
+    public function properties()
+    {
+        return [
+            IdProperty::create('id', 'id', null, 'uuid'),
+            StringProperty::create('name', 'name'),
+            DateTimeProperty::create('created_at', 'created_at'),
+            DateTimeProperty::create('updated_at', 'updated_at')
+        ];
+    }
+
+}
