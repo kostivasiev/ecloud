acdef7a82027349c97da406571a7e1d601d125d0
Umair Majeed <umairbmajeed@gmail.com>
2020-07-09 08:13:23 +0100

    Create and Update validations
    Relates to #275

---
+++ app/Http/Requests/V2/CreateRoutersRequest.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Http\Requests\V2;
+
+use UKFast\FormRequests\FormRequest;
+
+class CreateRoutersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return ($this->user()->isAdmin());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'    => 'required|string',
+            'gateway_id' => 'required|uuid',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'The :attribute field is required',
+            'gateway_id.required' => 'The :attribute field is required',
+        ];
+    }
+}
