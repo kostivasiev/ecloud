50f58499a0271ffd921f3987ee5b1a44bb48b97f
Umair Majeed <umairbmajeed@gmail.com>
2020-07-10 14:37:40 +0100

    Created AvailabilityZonesResource to replace usage of old resource types.
    Relates to #281

---
+++ app/Resources/V2/AvailabilityZonesResource.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Resources\V2;
+
+use Illuminate\Support\Carbon;
+use UKFast\Responses\UKFastResource;
+
+/**
+ * Class AvailabilityZonesResource
+ * @package App\Http\Resources\V2
+ * @property string id
+ * @property string code
+ * @property string name
+ * @property int site_id
+ * @property string created_at
+ * @property string updated_at
+ */
+class AvailabilityZonesResource extends UKFastResource
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->id,
+            'code'       => $this->code,
+            'name'       => $this->name,
+            'site_id'    => (int) $this->site_id,
+            'created_at' => Carbon::parse(
+                $this->created_at,
+                new \DateTimeZone(config('app.timezone'))
+            )->toIso8601String(),
+            'updated_at' => Carbon::parse(
+                $this->updated_at,
+                new \DateTimeZone(config('app.timezone'))
+            )->toIso8601String(),
+        ];
+    }
+
+}
