94a29eb1abd7489f7ff4d5b742b39a8733f43103
Umair Majeed <umairbmajeed@gmail.com>
2020-08-21 15:37:37 +0100

    fix

--- app/Models/V2/AvailabilityZone.php
+++ app/Models/V2/AvailabilityZone.php
@@ -75,7 +75,7 @@ class AvailabilityZone extends Model implements Filterable, Sortable
     public function nsxClient() : NsxService
     {
         if (!$this->nsxService) {
-            $this->nsxService = $this->app->makeWith('NsxService', [
+            $this->nsxService = app()->makeWith(NsxService::class, [
                 'endpoint' => $this->nsx_manager_endpoint,
             ]);
         }
