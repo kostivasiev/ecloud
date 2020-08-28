d48fb63934d6408574fcec67a602e86d1e9665ae
Umair Majeed <umairbmajeed@gmail.com>
2020-08-28 10:21:58 +0100

    disabling firewall rule deploy debug code

--- app/Console/Commands/Nsx/TestFirewallRuleDeploy.php
+++ app/Console/Commands/Nsx/TestFirewallRuleDeploy.php
@@ -48,4 +48,41 @@ class TestFirewallRuleDeploy extends Command
 
         dd($response->getBody()->getContents());
     }
+
+//    public function handle()
+//    {
+//        $router = Router::findOrFail($this->argument('routerId'));
+//        $availabilityZone = $router->vpc->region->availabilityZones()->first();
+//        $nsxClient = $availabilityZone->nsxClient();
+//
+//        try {
+//            //$response = $nsxClient->get('policy/api/v1/infra/tier-1s/' . $router->id . '/gateway-firewall');
+//            $response = $nsxClient->get('policy/api/v1/infra/domains/default/gateway-policies/Policy_Default_Infra/rules/' . $router->id . '-tier1-default_blacklist_rule');
+//            $original = json_decode($response->getBody()->getContents(), true);
+//            $original['action'] = 'REJECT';
+//            //$original['ip_protocol'] = 'IPV4_IPV6';
+//            $original = array_filter($original, function ($key) {
+//                return strpos($key, '_') !== 0;
+//            }, ARRAY_FILTER_USE_KEY);
+//
+//
+//            $keysToDelete = [
+//                'disabled',
+//                'destination_groups',
+//            ];
+//            foreach ($keysToDelete as $keyToDelete) {
+//                $original[$keyToDelete] = null;
+//                unset($original[$keyToDelete]);
+//            }
+//
+//
+//            $response = $nsxClient->patch('policy/api/v1/infra/domains/default/gateway-policies/Policy_Default_Infra/rules/' . $router->id . '-tier1-default_blacklist_rule', [
+//                'json' => $original
+//            ]);
+//        } catch (RequestException $exception) {
+//            dd($exception->getResponse()->getBody()->getContents());
+//        }
+//
+//        dd($response->getBody()->getContents());
+//    }
 }
