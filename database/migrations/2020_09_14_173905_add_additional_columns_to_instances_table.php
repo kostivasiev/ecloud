a1a153bf8f1886035d0378f94241e379b5c11842
Umair Majeed <umairbmajeed@gmail.com>
2020-09-14 18:26:01 +0100

    Added new migration which includes all of the new columns required by this branch.
    Relates to #423

---
+++ database/migrations/2020_09_14_173905_add_additional_columns_to_instances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdditionalColumnsToInstancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('ecloud')->table('instances', function (Blueprint $table) {
+            $table->uuid('appliance_id')->after('vpc_id')->default('');
+            $table->uuid('vcpu_tier')->after('appliance_id')->default('');
+            $table->integer('vcpu_count')->after('vcpu_tier')->default(1);
+            $table->integer('ram_capacity')->after('vcpu_count')->default(1024);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('ecloud')->table('instances', function (Blueprint $table) {
+            $table->dropColumn(['appliance_id', 'vcpu_tier', 'vcpu_count', 'ram_capacity']);
+        });
+    }
+}
