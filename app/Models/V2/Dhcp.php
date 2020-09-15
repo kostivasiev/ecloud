6db47edaf7235fdf895800617210dcd9b19888e6
Umair Majeed <umairbmajeed@gmail.com>
2020-09-15 17:23:03 +0100

    Added the DefaultAvailabilityZone trait to the Dhcp model.
    Relates to #453

--- app/Models/V2/Dhcp.php
+++ app/Models/V2/Dhcp.php
@@ -5,6 +5,7 @@ namespace App\Models\V2;
 use App\Events\V2\DhcpCreated;
 use App\Events\V2\VpcCreated;
 use App\Traits\V2\CustomKey;
+use App\Traits\V2\DefaultAvailabilityZone;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use UKFast\DB\Ditto\Factories\FilterFactory;
@@ -20,7 +21,7 @@ use UKFast\DB\Ditto\Sortable;
  */
 class Dhcp extends Model implements Filterable, Sortable
 {
-    use CustomKey, SoftDeletes;
+    use CustomKey, SoftDeletes, DefaultAvailabilityZone;
 
     public $keyPrefix = 'dhcp';
     protected $keyType = 'string';
