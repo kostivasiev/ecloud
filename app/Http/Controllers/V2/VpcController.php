5a9b330cefeec7a30ac67f355b9cda1a03e296ca
Umair Majeed <umairbmajeed@gmail.com>
2020-09-15 16:50:59 +0100

    Docs

--- app/Http/Controllers/V2/VpcController.php
+++ app/Http/Controllers/V2/VpcController.php
@@ -105,11 +105,11 @@ class VpcController extends BaseController
 
         // Create a new network
         Network::withoutEvents(function () use ($router) {
-            $instance = new Network();
-            $instance::addCustomKey($instance);
-            $instance->name = $instance->id;
-            $instance->router()->associate($router);
-            $instance->save();
+            $network = new Network();
+            $network::addCustomKey($network);
+            $network->name = $network->id;
+            $network->router()->associate($router);
+            $network->save();
         });
 
         // Deploy router and network
