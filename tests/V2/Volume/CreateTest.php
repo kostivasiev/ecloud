40bdfaf58374b8f40f8e119d0634026e2bdd6bb6
Umair Majeed <umairbmajeed@gmail.com>
2020-09-16 16:43:02 +0100

    Added test to prove that the availability_zone_id is an optional parameter.
    Relates to #453

--- tests/V2/Volume/CreateTest.php
+++ tests/V2/Volume/CreateTest.php
@@ -201,4 +201,27 @@ class CreateTest extends TestCase
         $this->assertNotNull($volume);
     }
 
+    public function testAzIsOptionalParameter()
+    {
+        $data = [
+            'name'    => 'Volume 1',
+            'vpc_id' => $this->vpc->getKey(),
+            'capacity' => (config('volume.capacity.min')+1),
+        ];
+
+        $this->post(
+            '/v2/volumes',
+            $data,
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )
+            ->assertResponseStatus(201);
+
+        $volumeId = (json_decode($this->response->getContent()))->data->id;
+        $volume = Volume::find($volumeId);
+        $this->assertNotNull($volume);
+        $this->assertNotNull($volume->availability_zone_id);
+    }
 }
