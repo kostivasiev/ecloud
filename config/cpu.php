60dc4ad5119e9ebc141cb8d70e4e6c62a54d148c
Umair Majeed <umairbmajeed@gmail.com>
2020-09-18 16:04:37 +0100

    Created config options for RAM and CPU Min/Max
    Relates to #423

---
+++ config/cpu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'cores' => [
+        'min' => env('CPU_CORES_MIN', 1),
+        'max' => env('CPU_CORES_MAX', 10),
+    ]
+];
---
+++ config/ram.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'capacity' => [
+        'min' => env('RAM_MIN', 1024),
+        'max' => env('RAM_MAX', 65536),
+    ]
+];
