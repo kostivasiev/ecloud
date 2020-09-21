fdc0ef20c9927fc3523624d485d37f072160a519
Umair Majeed <umairbmajeed@gmail.com>
2020-09-21 16:14:43 +0100

    Adding job logic

--- app/Jobs/Instance/Deploy/PowerOn.php
+++ app/Jobs/Instance/Deploy/PowerOn.php
@@ -3,6 +3,9 @@
 namespace App\Jobs\Instance\Deploy;
 
 use App\Jobs\Job;
+use App\Services\V2\KingpinService;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
 
 class PowerOn extends Job
@@ -19,6 +22,28 @@ class PowerOn extends Job
      */
     public function handle()
     {
-        Log::info('PowerOn');
+        Log::info('Attempted to PowerOn '.$this->data['instance_id']);
+
+        $kingpinService = app()->make(KingpinService::class);
+        try {
+            /** @var Response $response */
+            $response = $kingpinService->post(
+                '/api/v2/vpc/'.$this->data['vpc_id'].'/instance/'.$this->data['instance_id'].'/power'
+            );
+            if ($response->getStatusCode() == 200) {
+                Log::info('PowerOn job fin'.$this->data['instance_id']);
+                return;
+            }
+            $message = 'Failed to PowerOn '.$this->data['instance_id'].' with : ' .
+                $response->getReasonPhrase();
+            Log::error($message);
+            $this->fail(new \Exception($message));
+        } catch (GuzzleException $exception) {
+            $message = 'PowerOn job for instance '.$this->data['vpc_id'].' failed with : ' .
+                $exception->getResponse()->getBody()->getContents();
+            Log::error($message);
+            $this->fail(new \Exception($message));
+            return;
+        }
     }
 }
