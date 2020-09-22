e8aa1f699b83d59b9cc413a2c491835e6ac8f0f0
Umair Majeed <umairbmajeed@gmail.com>
2020-09-22 11:55:22 +0100

    Adding AZ to the Kingpin service provider make() but needs the instance AZ issue merging in before it will work.

--- app/Jobs/Instance/Deploy/PowerOn.php
+++ app/Jobs/Instance/Deploy/PowerOn.php
@@ -3,6 +3,7 @@
 namespace App\Jobs\Instance\Deploy;
 
 use App\Jobs\Job;
+use App\Models\V2\Instance;
 use App\Services\V2\KingpinService;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
@@ -22,24 +23,25 @@ class PowerOn extends Job
      */
     public function handle()
     {
-        Log::info('Attempted to PowerOn '.$this->data['instance_id']);
+        $instance = Instance::find($this->data['instance_id']);
 
-        $kingpinService = app()->make(KingpinService::class);
+        Log::info('Attempted to PowerOn '.$instance->id);
+
+        $kingpinService = app()->make(KingpinService::class, $instance->availabilityZone);
         try {
             /** @var Response $response */
             $response = $kingpinService->post(
-                '/api/v2/vpc/'.$this->data['vpc_id'].'/instance/'.$this->data['instance_id'].'/power'
+                '/api/v2/vpc/'.$this->data['vpc_id'].'/instance/'.$instance->id.'/power'
             );
             if ($response->getStatusCode() == 200) {
-                Log::info('PowerOn job fin'.$this->data['instance_id']);
+                Log::info('PowerOn job fin'.$instance->id);
                 return;
             }
-            $message = 'Failed to PowerOn '.$this->data['instance_id'].' with : ' .
-                $response->getReasonPhrase();
+            $message = 'Failed to PowerOn '.$instance->id.' with : '.$response->getReasonPhrase();
             Log::error($message);
             $this->fail(new \Exception($message));
         } catch (GuzzleException $exception) {
-            $message = 'PowerOn job for instance '.$this->data['vpc_id'].' failed with : ' .
+            $message = 'PowerOn job for instance '.$this->data['vpc_id'].' failed with : '.
                 $exception->getResponse()->getBody()->getContents();
             Log::error($message);
             $this->fail(new \Exception($message));
