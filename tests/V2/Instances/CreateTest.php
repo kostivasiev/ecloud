b6274fb044a90cd621fbf8595ddd0ada1e2fb892
Umair Majeed <umairbmajeed@gmail.com>
2020-09-23 11:50:35 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- tests/V2/Instances/CreateTest.php
+++ tests/V2/Instances/CreateTest.php
@@ -41,7 +41,7 @@ class CreateTest extends TestCase
         ]);
         $this->appliance_version = factory(ApplianceVersion::class)->create([
             'appliance_version_appliance_id' => $this->appliance->appliance_id,
-        ])->refresh();
+        ]);
         $this->instance = factory(Instance::class)->create([
             'appliance_version_id' => $this->appliance_version->appliance_version_uuid,
             'availability_zone_id' => $this->availability_zone->getKey(),
