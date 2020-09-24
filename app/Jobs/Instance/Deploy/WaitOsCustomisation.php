9c6a282b465900a24ecd46d998a45fc29f3d1d93
Umair Majeed <umairbmajeed@gmail.com>
2020-09-24 09:35:09 +0100

    WIP

--- app/Jobs/Instance/Deploy/WaitOsCustomisation.php
+++ app/Jobs/Instance/Deploy/WaitOsCustomisation.php
@@ -6,11 +6,16 @@ use App\Jobs\Job;
 use App\Models\V2\Instance;
 use App\Models\V2\Vpc;
 use App\Services\V2\KingpinService;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
 
 class WaitOsCustomisation extends Job
 {
+    const RETRY_ATTEMPTS = 360; // Retry every 5 seconds for 20 minutes
+
+    const RETRY_DELAY = 5;
+
     private $data;
 
     public function __construct($data)
@@ -19,7 +24,7 @@ class WaitOsCustomisation extends Job
     }
 
     /**
-     * @see Missing Issue?
+     * @see https://gitlab.devops.ukfast.co.uk/ukfast/api.ukfast/ecloud/-/issues/329
      */
     public function handle()
     {
@@ -30,17 +35,36 @@ class WaitOsCustomisation extends Job
         try {
             /** @var Response $response */
             $response = $kingpinService->post('/api/v2/vpc/'.$vpc->id.'/instance/'.$instance->id.'/oscustomization/status');
-            if ($response->getStatusCode() == 200) {
-                Log::info('WaitOsCustomisation finished successfully for instance '.$instance->id);
+            if ($response->getStatusCode() != 200) {
+                $this->fail(new \Exception(
+                    'WaitOsCustomisation failed for '.$instance->id.', Kingpin status was '.$response->getStatusCode()
+                ));
                 return;
             }
-            $this->fail(new \Exception(
-                'Failed to WaitOsCustomisation '.$instance->id.', Kingpin status was '.$response->getStatusCode()
-            ));
-            return;
+
+            $data = json_decode($response->getBody());
+            if (!$data) {
+                $this->fail(new \Exception('WaitOsCustomisation failed for '.$instance->id.', could not decode response'));
+                return;
+            }
+
+            if ($data->status !== 'Succeeded') {
+                if ($this->attempts() <= static::RETRY_ATTEMPTS) {
+                    $this->release(static::RETRY_DELAY);
+                    Log::info(
+                        'Check for WaitOsCustomisation for '.$instance->id.' returned "'.
+                        $data->status.'", retrying in '.static::RETRY_DELAY.' seconds'
+                    );
+                    return;
+                } else {
+                    $this->fail(new \Exception('Timed out on WaitOsCustomisation for '.$instance->id));
+                    return;
+                }
+            }
         } catch (GuzzleException $exception) {
             $this->fail(new \Exception(
-                'Failed to WaitOsCustomisation '.$instance->id.' : '.$exception->getResponse()->getBody()->getContents()
+                'WaitOsCustomisation failed for '.$instance->id.' : '.
+                $exception->getResponse()->getBody()->getContents()
             ));
             return;
         }
