71a3d9a2c020870b6f81d90bbec4abee64fb40a0
Umair Majeed <umairbmajeed@gmail.com>
2020-09-24 15:34:32 +0100

    WIP

--- app/Jobs/Instance/Deploy/OsCustomisation.php
+++ app/Jobs/Instance/Deploy/OsCustomisation.php
@@ -3,7 +3,12 @@
 namespace App\Jobs\Instance\Deploy;
 
 use App\Jobs\Job;
+use App\Models\V2\Instance;
+use App\Models\V2\Vpc;
+use App\Services\V2\KingpinService;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
+use UKFast\Admin\Devices\AdminClient;
 
 class OsCustomisation extends Job
 {
@@ -19,6 +24,32 @@ class OsCustomisation extends Job
      */
     public function handle()
     {
-        Log::info('OsCustomisation');
+        Log::info('Starting OsCustomisation for instance '.$this->data['instance_id']);
+        $instance = Instance::findOrFail($this->data['instance_id']);
+        $vpc = Vpc::findOrFail($this->data['vpc_id']);
+        $kingpinService = app()->make(KingpinService::class, $instance->availabilityZone);
+        $devicesAdminClient = app()->make(AdminClient::class);
+        $license = $devicesAdminClient->licenses()->getById(
+            $instance->applianceVersions->appliance_version_server_license_id
+        );
+        dd($license);
+
+        try {
+            /** @var Response $response */
+            $response = $kingpinService->post('/api/v2/vpc/'.$vpc->id.'/instance/'.$instance->id.'/power');
+            if ($response->getStatusCode() == 200) {
+                Log::info('PowerOn finished successfully for instance '.$instance->id);
+                return;
+            }
+            $this->fail(new \Exception(
+                'Failed to PowerOn '.$instance->id.', Kingpin status was '.$response->getStatusCode()
+            ));
+            return;
+        } catch (GuzzleException $exception) {
+            $this->fail(new \Exception(
+                'Failed to PowerOn '.$instance->id.' : '.$exception->getResponse()->getBody()->getContents()
+            ));
+            return;
+        }
     }
 }
