d9a4ec8ee956b3fcabba9e5dbc9f2940a9bede08
Umair Majeed <umairbmajeed@gmail.com>
2020-09-25 10:58:00 +0100

    typo

--- app/Console/Commands/Kingpin/Instance/Delete.php
+++ app/Console/Commands/Kingpin/Instance/Delete.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
 /**
- * Class DeleteInstance
+ * Class Delete
  * Delete an instance from vmware
  * @package App\Console\Commands\Kingpin\Instance
  * @param string instanceId
