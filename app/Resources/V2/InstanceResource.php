c340c488bd64f1abb4cfb2c6bc5d6fe694aeb9b0
Umair Majeed <umairbmajeed@gmail.com>
2020-09-25 08:04:14 +0100

    Modified resource to check for the presence of the instanceId route parameter. If it is present then we're getting the model directly, and append the power_state to the array.
    Relates to #451

--- app/Resources/V2/InstanceResource.php
+++ app/Resources/V2/InstanceResource.php
@@ -17,6 +17,7 @@ use UKFast\Responses\UKFastResource;
  * @property integer ram_capacity
  * @property string availability_zone_id
  * @property boolean locked
+ * @property string power_state
  * @property string created_at
  * @property string updated_at
  */
@@ -48,6 +49,9 @@ class InstanceResource extends UKFastResource
         ];
         if ($request->user->isAdministrator) {
             $response['appliance_version_id'] = $this->appliance_version_id;
+        }
+        if ($request->route('instanceId')) {
+            $response['power_state'] = $this->power_state;
         }
         return $response;
     }
