1951c119c6ae9374fc66f7944fb2c15c0c298d6e
Umair Majeed <umairbmajeed@gmail.com>
2020-09-25 08:04:51 +0100

    Added tests to check for the power_state attribute wherever it should be present.
    Relates to #451

---
+++ tests/V2/Instances/PowerStateTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\V2\Instances;
+
+use App\Models\V2\Appliance;
+use App\Models\V2\ApplianceVersion;
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Instance;
+use App\Models\V2\Region;
+use App\Models\V2\Vpc;
+use App\Services\V2\KingpinService;
+use Faker\Factory as Faker;
+use GuzzleHttp\Client;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class PowerStateTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected \Faker\Generator $faker;
+    protected $availability_zone;
+    protected $instance;
+    protected $appliance;
+    protected $appliance_version;
+    protected $credentials;
+    protected $region;
+    protected $vpc;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+        $this->region = factory(Region::class)->create();
+        $this->availability_zone = factory(AvailabilityZone::class)->create([
+            'region_id' => $this->region->getKey()
+        ]);
+        Vpc::flushEventListeners();
+        $this->vpc = factory(Vpc::class)->create([
+            'region_id' => $this->region->getKey()
+        ]);
+        $this->appliance = factory(Appliance::class)->create([
+            'appliance_name' => 'Test Appliance',
+        ])->refresh();
+        $this->appliance_version = factory(ApplianceVersion::class)->create([
+            'appliance_version_appliance_id' => $this->appliance->id,
+        ])->refresh();
+        $this->instance = factory(Instance::class)->create([
+            'vpc_id'               => $this->vpc->getKey(),
+            'name'                 => 'GetTest Default',
+            'appliance_version_id' => $this->appliance_version->uuid,
+            'vcpu_cores'           => 1,
+            'ram_capacity'         => 1024,
+        ]);
+        $mockKingpinService = \Mockery::mock(new KingpinService(new Client()))->makePartial();
+        $mockKingpinService->shouldReceive('get')->andReturn(json_encode([
+            'id' => 'string',
+            'powerState' => 'string',
+            'hostname' => 'string',
+            'toolsStatus' => 'string',
+            'toolsRunningStatus' => 'string',
+            'toolsVersionStatus' => 'string',
+            'connectionState' => 'string',
+            'guestOperationsReady' => true,
+            'nics' => [
+                [
+                    'macAddress' => 'string',
+                ]
+            ],
+            'volumes' => [
+                [
+                    'uuid' => 'string',
+                    'createdAt' => '2020-09-16T13:19:20.041Z',
+                    'volumeId' => 'string',
+                ]
+            ]
+        ]));
+        app()->bind(KingpinService::class, function () use ($mockKingpinService) {
+            return $mockKingpinService;
+        });
+    }
+
+    public function testGetPowerState()
+    {
+        $this->assertNotEmpty($this->instance->power_state);
+
+        // Test power state is not returned on the collection
+        $this->get(
+            '/v2/instances',
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups'    => 'ecloud.read',
+            ]
+        )
+            ->dontSeeJson([
+                'power_state' => 'string',
+            ])
+            ->assertResponseStatus(200);
+
+        // Test power state is returned on the model
+        $this->get(
+            '/v2/instances/' . $this->instance->getKey(),
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups'    => 'ecloud.read',
+            ]
+        )
+            ->seeJson([
+                'power_state' => 'string',
+            ])
+            ->assertResponseStatus(200);
+    }
+}
