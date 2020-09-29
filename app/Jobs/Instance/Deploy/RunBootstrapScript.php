fd94f3a236dcf0eb63e4aeb655d8a73d5c5e5ffe
Umair Majeed <umairbmajeed@gmail.com>
2020-09-29 15:25:19 +0100

    WIP

--- app/Jobs/Instance/Deploy/RunBootstrapScript.php
+++ app/Jobs/Instance/Deploy/RunBootstrapScript.php
@@ -3,6 +3,11 @@
 namespace App\Jobs\Instance\Deploy;
 
 use App\Jobs\Job;
+use App\Models\V2\Instance;
+use App\Models\V2\Vpc;
+use App\Services\V2\KingpinService;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
 
 class RunBootstrapScript extends Job
@@ -19,6 +24,41 @@ class RunBootstrapScript extends Job
      */
     public function handle()
     {
-        Log::info('RunBootstrapScript');
+        Log::info('Starting RunBootstrapScript for instance '.$this->data['instance_id']);
+        $instance = Instance::findOrFail($this->data['instance_id']);
+        $vpc = Vpc::findOrFail($this->data['vpc_id']);
+        $credential = $instance->credentials()
+            ->where('user', ($instance->platform == 'Linux') ? 'root' : 'administrator')
+            ->firstOrFail();
+        if (!$credential) {
+            $this->fail(new \Exception('RunBootstrapScript failed for '.$instance->id.', no credentials found'));
+            return;
+        }
+
+        $endpoint = ($instance->platform == 'Linux') ? 'linux/script' : 'windows/script';
+        try {
+            $kingpinService = app()->make(KingpinService::class, [$instance->availabilityZone]);
+            /** @var Response $response */
+            $response = $kingpinService->put('/api/v2/vpc/'.$vpc->id.'/instance/'.$instance->id.'/guest/'.$endpoint, [
+                'json' => [
+                    'encodedScript' => $script,
+                    'username' => $credential->username,
+                    'password' => $credential->password,
+                ],
+            ]);
+            if ($response->getStatusCode() == 200) {
+                Log::info('RunBootstrapScript finished successfully for instance '.$instance->id);
+                return;
+            }
+            $this->fail(new \Exception(
+                'Failed RunBootstrapScript for '.$instance->id.', Kingpin status was '.$response->getStatusCode()
+            ));
+            return;
+        } catch (GuzzleException $exception) {
+            $this->fail(new \Exception(
+                'Failed RunBootstrapScript for '.$instance->id.' : '.$exception->getResponse()->getBody()->getContents()
+            ));
+            return;
+        }
     }
 }
