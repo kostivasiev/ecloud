303239945ab5f726e3575a37cbe7ea1d34d38fe1
Umair Majeed <umairbmajeed@gmail.com>
2020-09-30 11:29:55 +0100

    Aadded appliance and appliance_version setup as well as disabled Instance events.
    Relates to #465

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,15 +4,21 @@ namespace Tests;
 
 use App\Listeners\V2\DhcpCreate;
 use App\Models\V1\Datastore;
+use App\Models\V2\Appliance;
+use App\Models\V2\ApplianceVersion;
 use App\Models\V2\AvailabilityZone;
 use App\Models\V2\Dhcp;
 use App\Models\V2\FirewallRule;
+use App\Models\V2\Instance;
 use App\Models\V2\Router;
 use App\Models\V2\Vpc;
 use App\Models\V2\Network;
 
 abstract class TestCase extends \Laravel\Lumen\Testing\TestCase
 {
+
+    protected $appliance;
+    protected $appliance_version;
 
     public $validReadHeaders = [
         'X-consumer-custom-id' => '1-1',
@@ -35,6 +41,13 @@ abstract class TestCase extends \Laravel\Lumen\Testing\TestCase
         FirewallRule::flushEventListeners();
         Vpc::flushEventListeners();
         Network::flushEventListeners();
+        $this->appliance = factory(Appliance::class)->create([
+            'appliance_name' => 'Test Appliance',
+        ])->refresh();
+        $this->appliance_version = factory(ApplianceVersion::class)->create([
+            'appliance_version_appliance_id' => $this->appliance->appliance_id,
+        ])->refresh();
+        Instance::flushEventListeners();
     }
 
     /**
