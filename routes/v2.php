62c28d18f551b0ebc7c524eaaa1e47c96fd3f7d2
Umair Majeed <umairbmajeed@gmail.com>
2020-10-01 14:50:06 +0100

    add and expose gueset restart job

--- routes/v2.php
+++ routes/v2.php
@@ -85,6 +85,7 @@ $router->group($baseRouteParameters, function () use ($router) {
         $router->post('instances/{instanceId}/deploy', 'InstanceController@deploy');
         $router->put('instances/{instanceId}/power-on', 'InstanceController@powerOn');
         $router->put('instances/{instanceId}/power-off', 'InstanceController@powerOff');
+        $router->put('instances/{instanceId}/power-restart', 'InstanceController@guestRestart');
     });
 
     /** Floating Ips */
