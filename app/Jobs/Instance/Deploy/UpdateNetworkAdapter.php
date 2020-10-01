7e0c6f5a4c0a61b31f4e94643f769eb1d587e2bd
Umair Majeed <umairbmajeed@gmail.com>
2020-10-01 16:09:07 +0100

    With the change in the Instance::nics() relationship have added an iteration to perform the updates to all the Nics in the collection.
    Relates to #327

--- app/Jobs/Instance/Deploy/UpdateNetworkAdapter.php
+++ app/Jobs/Instance/Deploy/UpdateNetworkAdapter.php
@@ -28,32 +28,34 @@ class UpdateNetworkAdapter extends Job
         if (!empty($instance->applianceVersion->appliance_version_vm_template)) {
             Log::info('UpdateNetworkAdapter');
             $vpc = Vpc::findOrFail($this->data['vpc_id']);
-            $nic = $instance->nic;
-            try {
-                /** @var Response $response */
-                $response = $instance->availabilityZone->kingpinService()->put(
-                    '/api/v2/vpc/'.$vpc->id.'/instance/'.$instance->id.'/nic/'.$nic->mac_address.'/connect',
-                    [
-                        'json' => [
-                            'networkId' => $nic->network_id,
-                        ],
-                    ]
-                );
-                if ($response->getStatusCode() == 200) {
-                    Log::info('UpdateNetworkAdapter finished successfully for instance '.$instance->id);
+            $nics = $instance->nics();
+            foreach ($nics as $nic) {
+                try {
+                    /** @var Response $response */
+                    $response = $instance->availabilityZone->kingpinService()->put(
+                        '/api/v2/vpc/'.$vpc->id.'/instance/'.$instance->id.'/nic/'.$nic->mac_address.'/connect',
+                        [
+                            'json' => [
+                                'networkId' => $nic->network_id,
+                            ],
+                        ]
+                    );
+                    if ($response->getStatusCode() == 200) {
+                        Log::info('UpdateNetworkAdapter finished successfully for instance '.$instance->id);
+                        return;
+                    }
+                    $this->fail(new \Exception(
+                        'Failed UpdateNetworkAdapter for '.$instance->id.', Kingpin status was '.
+                        $response->getStatusCode()
+                    ));
+                    return;
+                } catch (GuzzleException $exception) {
+                    $this->fail(new \Exception(
+                        'Failed UpdateNetworkAdapter for '.$instance->id.' : '.
+                        $exception->getResponse()->getBody()->getContents()
+                    ));
                     return;
                 }
-                $this->fail(new \Exception(
-                    'Failed UpdateNetworkAdapter for '.$instance->id.', Kingpin status was '.
-                    $response->getStatusCode()
-                ));
-                return;
-            } catch (GuzzleException $exception) {
-                $this->fail(new \Exception(
-                    'Failed UpdateNetworkAdapter for '.$instance->id.' : '.
-                    $exception->getResponse()->getBody()->getContents()
-                ));
-                return;
             }
         }
     }
