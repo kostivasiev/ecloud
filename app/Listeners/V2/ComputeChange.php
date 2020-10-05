4f2d9b005d5d0f735893b7559e0c1af1c7c9c301
Umair Majeed <umairbmajeed@gmail.com>
2020-10-05 15:10:05 +0100

    Refactored listener to be called ComputeChange and to process the reboot flag.
    Relates to #444

--- app/Listeners/V2/ComputeChange.php
+++ app/Listeners/V2/ComputeChange.php
@@ -2,29 +2,30 @@
 
 namespace App\Listeners\V2;
 
-use App\Events\V2\MemoryChanged;
+use App\Events\V2\ComputeChanged;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class MemoryChange implements ShouldQueue
+class ComputeChange implements ShouldQueue
 {
     use InteractsWithQueue;
 
     /**
-     * @param  \App\Events\V2\MemoryChanged  $event
+     * @param  \App\Events\V2\ComputeChanged  $event
      * @return void
      * @throws \Exception
      */
-    public function handle(MemoryChanged $event)
+    public function handle(ComputeChanged $event)
     {
         $instance = $event->instance;
+        $reboot = $event->rebootRequired;
         $parameters = [];
 
         // Handle ram_capacity
         $parameters['ramGB'] = $instance->ram_capacity;
         $limit = ($instance->platform == "Windows") ? 16 : 3;
-        $reboot = (($instance->ram_capacity / 1024) <= $limit) ? false : true;
+        $reboot = ((!$reboot) && (($instance->ram_capacity / 1024) <= $limit)) ? false : true;
 
         // Handle vcpu_cores
         $parameters['numCpu'] = $instance->vcpu_cores;
