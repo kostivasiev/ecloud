99aa9174db39233a2eeae05189531a515e9ffaa5
Umair Majeed <umairbmajeed@gmail.com>
2020-10-06 12:45:23 +0100

    Added instance relationship
    Relates to #461

--- app/Models/V2/Credential.php
+++ app/Models/V2/Credential.php
@@ -58,6 +58,11 @@ class Credential extends Model implements Filterable, Sortable
         return $this->belongsTo(AvailabilityZone::class, 'id', 'resource_id');
     }
 
+    public function instance()
+    {
+        return $this->belongsTo(Instance::class, 'id', 'resource_id');
+    }
+
     /**
      * @param FilterFactory $factory
      * @return array|Filter[]
