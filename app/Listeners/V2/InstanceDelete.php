a1007b38c8056491a0ca6703d80b6769aeb5ca5f
Umair Majeed <umairbmajeed@gmail.com>
2020-10-07 08:04:33 +0100

    Created InstanceDelete Listener which contains refactored functionality from the InstanceDelete job.
    Relates to #445

---
+++ app/Listeners/V2/InstanceDelete.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners\V2;
+
+use App\Events\V2\InstanceDeleteEvent;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class InstanceDelete implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * @param  InstanceDeleteEvent  $event
+     * @return void
+     * @throws \Exception
+     */
+    public function handle(InstanceDeleteEvent $event)
+    {
+        $instance = $event->instance;
+        Log::info('Attempting to Delete instance '.$instance->getKey());
+        try {
+            /** @var Response $response */
+            $response = $instance->availabilityZone
+                ->kingpinService()
+                ->delete('/api/v2/vpc/'.$instance->vpc_id.'/instance/'.$instance->getKey());
+            if ($response->getStatusCode() == 200) {
+                Log::info('Delete finished successfully for instance '.$instance->id);
+                return;
+            }
+            $this->fail(new \Exception(
+                'Failed to Delete '.$instance->getKey().', Kingpin status was '.$response->getStatusCode()
+            ));
+            return;
+        } catch (GuzzleException $exception) {
+            $this->fail(new \Exception(
+                'Failed to Delete '.$instance->getKey().' : '.
+                $exception->getResponse()->getBody()->getContents()
+            ));
+            return;
+        }
+    }
+
+}
