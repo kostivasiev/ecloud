db17f82884973126f013a1968bf32b6c74b5df38
Umair Majeed <umairbmajeed@gmail.com>
2020-10-07 16:34:54 +0100

    Job dev

--- app/Jobs/Instance/Deploy/PrepareOsUsers.php
+++ app/Jobs/Instance/Deploy/PrepareOsUsers.php
@@ -3,6 +3,9 @@
 namespace App\Jobs\Instance\Deploy;
 
 use App\Jobs\Job;
+use App\Models\V2\Instance;
+use App\Models\V2\Vpc;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
 
 class PrepareOsUsers extends Job
@@ -19,6 +22,42 @@ class PrepareOsUsers extends Job
      */
     public function handle()
     {
-        Log::info('PrepareOsUsers');
+        Log::info('Starting PrepareOsUsers for instance ' . $this->data['instance_id']);
+        $instance = Instance::findOrFail($this->data['instance_id']);
+        $vpc = Vpc::findOrFail($this->data['vpc_id']);
+        $credential = $instance->credentials()
+            ->where('user', ($instance->platform == 'Linux') ? 'root' : 'administrator')
+            ->firstOrFail();
+        if (!$credential) {
+            $this->fail(new \Exception('PrepareOsUsers failed for ' . $instance->id . ', no credentials found'));
+            return;
+        }
+
+        try {
+            /** @var Response $response */
+            $response = $instance->availabilityZone->kingpinService()->put(
+                '/api/v2/vpc/' . $vpc->id . '/instance/' . $instance->id . '/TODO',
+                [
+                    'json' => [
+                        'platform' => $instance->platform,
+                        'password' => $credential->password,
+                        'hostname' => $instance->id,
+                    ],
+                ]
+            );
+            if ($response->getStatusCode() == 200) {
+                Log::info('PrepareOsUsers finished successfully for instance ' . $instance->id);
+                return;
+            }
+            $this->fail(new \Exception(
+                'Failed PrepareOsUsers for ' . $instance->id . ', Kingpin status was ' . $response->getStatusCode()
+            ));
+            return;
+        } catch (GuzzleException $exception) {
+            $this->fail(new \Exception(
+                'Failed PrepareOsUsers for ' . $instance->id . ' : ' . $exception->getResponse()->getBody()->getContents()
+            ));
+            return;
+        }
     }
 }
