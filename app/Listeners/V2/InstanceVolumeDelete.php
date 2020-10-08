f825a24d60f38177e039ade128aeef9f7b99ea52
Umair Majeed <umairbmajeed@gmail.com>
2020-10-08 15:15:26 +0100

    Created a separate listener for volume delete. Have implemented 3 retries with this and a delay of 10 seconds between retries in case the listener is fired before an instance has deleted. Probably overkill but wanted to avoid a race condition.
    Relates to #445

---
+++ app/Listeners/V2/InstanceVolumeDelete.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Listeners\V2;
+
+use App\Events\V2\InstanceDeleteEvent;
+use App\Models\V2\Instance;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class InstanceVolumeDelete implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    // Allows for 3 attempts with 10 seconds between
+    public $tries = 3;
+    public $delay = 10;
+
+    public function handle(InstanceDeleteEvent $event)
+    {
+        dd("HERE");
+        $instance = $event->instance;
+        Log::info('Attempting to Delete volumes for instance '.$instance->getKey());
+        if ($this->attempts() <= $this->tries) {
+            if (!is_null(Instance::withTrashed()->findOrFail($instance->id)->deleted_at)) {
+                $instance->volumes()->each(function ($volume) use ($instance) {
+                    // If volume is only used in this instance then delete
+                    if ($volume->instances()->count() == 0) {
+                        Log::info('Deleting volume: '.$volume->getKey());
+                        $volume->delete();
+                        $volume->instances()->detach($instance);
+                    }
+                });
+            } else {
+                Log::info(
+                    'Instance '.$instance->getKey().' not yet deleted. Deferring volume deletion for '.
+                    $this->delay.' seconds'
+                );
+                $this->release($this->delay);
+            }
+        } else {
+            $this->fail(new \Exception(
+                'Failed to Delete volumes for Instance '.$instance->getKey().' : '.
+                'maximum number of retries exceeded'
+            ));
+            $this->delete(); // releases the job if the number of retries has been exceeded
+        }
+    }
+}
