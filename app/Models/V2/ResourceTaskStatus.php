0678f2908751dbe09bbbfda2e0e5e7f56b0a019d
Umair Majeed <umairbmajeed@gmail.com>
2020-10-13 14:25:05 +0100

    formatting, overide doc block for string type id

--- app/Models/V2/ResourceTaskStatus.php
+++ app/Models/V2/ResourceTaskStatus.php
@@ -3,17 +3,21 @@
 namespace App\Models\V2;
 
 use App\Traits\V2\CustomKey;
+use Imtigger\LaravelJobStatus\JobStatus;
+
 /**
  * App\Models\V2\ResourceTaskStatus.
  *
- * @property string    $resource_id
-*/
-class ResourceTaskStatus extends \Imtigger\LaravelJobStatus\JobStatus {
+ * @property string $id
+ * @property string $resource_id
+ */
+class ResourceTaskStatus extends JobStatus
+{
     use CustomKey;
 
     public $keyPrefix = 'task';
+    public $incrementing = false;
     protected $keyType = 'string';
     protected $connection = 'ecloud';
     protected $table = 'resource_task_statuses';
-    public $incrementing = false;
 }
