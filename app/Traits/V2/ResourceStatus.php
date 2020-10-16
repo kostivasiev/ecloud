43229d10470d361cb3781e6d4fdb90dabc23e1be
Umair Majeed <umairbmajeed@gmail.com>
2020-10-16 16:36:24 +0100

    tidy

--- app/Traits/V2/ResourceStatus.php
+++ app/Traits/V2/ResourceStatus.php
@@ -8,19 +8,21 @@ trait ResourceStatus
 {
     // Until tasks are exposed, for now we'll compute status based on tasks associated
     // with this resource.
-    // If an resource only has 1 task and that task is failed, the status will be STATUS_FAILED
-    // If an resource only has 1 task and that task is not completed, the status will be STATUS_CREATING
+    // If a resource only has 1 task and that task is failed, the status will be STATUS_FAILED
+    // If a resource only has 1 task and that task is not completed, the status will be STATUS_CREATING
+    // If a resource has one or more tasks that are running, the status will be STATUS_UPDATING
+    // else status will be STATUS_READY
     public function getStatusAttribute()
     {
-        if ($this->tasks()->count() == 1 && $this->tasks()->latest()->first()->status == Task::STATUS_FAILED) {
+        if ($this->tasks()->count() == 1 && $this->tasks()->latest()->first()->is_failed) {
             return self::STATUS_FAILED;
         }
 
-        if ($this->tasks()->count() == 1 && !$this->tasks()->latest()->first()->getIsEndedAttribute()) {
+        if ($this->tasks()->count() == 1 && !$this->tasks()->latest()->first()->is_ended) {
             return self::STATUS_CREATING;
         }
 
-        if ($this->tasks()->count() > 1 && !$this->tasks()->latest()->first()->getIsEndedAttribute()) {
+        if ($this->task_running) {
             return self::STATUS_UPDATING;
         }
 
