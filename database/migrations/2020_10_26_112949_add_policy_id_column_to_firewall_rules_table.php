255709c888ef9d7533db64c2128f76bcf3c4c0ae
Umair Majeed <umairbmajeed@gmail.com>
2020-10-26 11:37:04 +0000

    added policy_id column to firewall rules table
    Relates to #384

---
+++ database/migrations/2020_10_26_112949_add_policy_id_column_to_firewall_rules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPolicyIdColumnToFirewallRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('firewall_rules', function (Blueprint $table) {
+            $table->string('policy_id')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('firewall_rules', function (Blueprint $table) {
+            $table->dropColumn('policy_id');
+        });
+    }
+}
