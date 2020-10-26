051f95614f85f9ee837020399bf7882b842b6839
Umair Majeed <umairbmajeed@gmail.com>
2020-10-26 11:13:57 +0000

    Created Firewall Policy Resource
    Relates to #384

---
+++ app/Resources/V2/FirewallPolicyResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Resources\V2;
+
+use Illuminate\Support\Carbon;
+use UKFast\Responses\UKFastResource;
+
+/**
+ * Class FirewallPolicyResource
+ * @package App\Resources\V2
+ * @property string id
+ * @property string name
+ * @property string sequence
+ * @property string created_at
+ * @property string updated_at
+ */
+class FirewallPolicyResource extends UKFastResource
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->id,
+            'name'       => $this->name,
+            'sequence'   => $this->sequence,
+            'created_at' => Carbon::parse(
+                $this->created_at,
+                new \DateTimeZone(config('app.timezone'))
+            )->toIso8601String(),
+            'updated_at' => Carbon::parse(
+                $this->updated_at,
+                new \DateTimeZone(config('app.timezone'))
+            )->toIso8601String(),
+        ];
+    }
+}
