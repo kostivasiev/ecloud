83861ddab7f2347186e7b6fc0f1960c78c2b56a7
Umair Majeed <umairbmajeed@gmail.com>
2020-10-28 16:14:14 +0000

    Updated validations with additional fields
    Relates to #500

--- app/Http/Requests/V2/CreateFirewallRuleRequest.php
+++ app/Http/Requests/V2/CreateFirewallRuleRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests\V2;
 
+use App\Models\V2\FirewallPolicy;
 use App\Models\V2\Router;
 use App\Rules\V2\ExistsForUser;
 use App\Rules\V2\ValidCidrSubnetArray;
+use App\Rules\V2\ValidPortReference;
 use UKFast\FormRequests\FormRequest;
 
 class CreateFirewallRuleRequest extends FormRequest
@@ -32,16 +34,32 @@ class CreateFirewallRuleRequest extends FormRequest
                 'exists:ecloud.routers,id,deleted_at,NULL',
                 new ExistsForUser(Router::class)
             ],
-            'firewall_policy_id' => 'required|string|exists:ecloud.firewall_policies,id,deleted_at,NULL',
+            'firewall_policy_id' => [
+                'required',
+                'string',
+                'exists:ecloud.firewall_policies,id,deleted_at,NULL',
+                new ExistsForUser(FirewallPolicy::class)
+            ],
+            'service_type' => 'required|string|in:IP,IGMP,ICMPv4,ALG,TCP,UDP,ICMPv6',
             'source' => [
                 'required',
                 'string',
                 new ValidCidrSubnetArray()
             ],
+            'source_ports' => [
+                'required',
+                'string',
+                new ValidPortReference()
+            ],
             'destination' => [
                 'required',
                 'string',
                 new ValidCidrSubnetArray()
+            ],
+            'destination_ports' => [
+                'required',
+                'string',
+                new ValidPortReference()
             ],
             'action' => 'required|string|in:ALLOW,DROP,REJECT',
             'direction' => 'required|string|in:IN,OUT,IN_OUT',
@@ -63,8 +81,15 @@ class CreateFirewallRuleRequest extends FormRequest
             'firewall_policy_id.exists' => 'The specified :attribute was not found',
             'source.required' => 'The :attribute field is required',
             'source.string' => 'The :attribute field must contain a string',
+            'source_ports.required' => 'The :attribute field is required',
+            'source_ports.string' => 'The :attribute field must contain a string',
+            'service_type.required' => 'The :attribute field is required',
+            'service_type.string' => 'The :attribute field must contain a string',
+            'service_type.in' => 'The :attribute field must contain one of IP,IGMP,ICMPv4,ALG,TCP,UDP or ICMPv6',
             'destination.required' => 'The :attribute field is required',
             'destination.string' => 'The :attribute field must contain a string',
+            'destination_ports.required' => 'The :attribute field is required',
+            'destination_ports.string' => 'The :attribute field must contain a string',
             'action.required' => 'The :attribute field is required',
             'action.string' => 'The :attribute field must contain a string',
             'action.in' => 'The :attribute field contains an invalid option',
--- app/Http/Requests/V2/UpdateFirewallRuleRequest.php
+++ app/Http/Requests/V2/UpdateFirewallRuleRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests\V2;
 
+use App\Models\V2\FirewallPolicy;
 use App\Models\V2\Router;
 use App\Rules\V2\ExistsForUser;
 use App\Rules\V2\ValidCidrSubnetArray;
+use App\Rules\V2\ValidPortReference;
 use UKFast\FormRequests\FormRequest;
 
 class UpdateFirewallRuleRequest extends FormRequest
@@ -23,18 +25,37 @@ class UpdateFirewallRuleRequest extends FormRequest
                 'exists:ecloud.routers,id,deleted_at,NULL',
                 new ExistsForUser(Router::class)
             ],
-            'firewall_policy_id' => 'sometimes|required|string|exists:ecloud.firewall_policies,id,deleted_at,NULL',
+            'firewall_policy_id' => [
+                'sometimes',
+                'required',
+                'string',
+                'exists:ecloud.firewall_policies,id,deleted_at,NULL',
+                new ExistsForUser(FirewallPolicy::class)
+            ],
+            'service_type' => 'sometimes|required|string|in:IP,IGMP,ICMPv4,ALG,TCP,UDP,ICMPv6',
             'source' => [
                 'sometimes',
                 'required',
                 'string',
                 new ValidCidrSubnetArray()
             ],
+            'source_ports' => [
+                'sometimes',
+                'required',
+                'string',
+                new ValidPortReference()
+            ],
             'destination' => [
                 'sometimes',
                 'required',
                 'string',
                 new ValidCidrSubnetArray()
+            ],
+            'destination_ports' => [
+                'sometimes',
+                'required',
+                'string',
+                new ValidPortReference()
             ],
             'action' => 'sometimes|required|string|in:ALLOW,DROP,REJECT',
             'direction' => 'sometimes|required|string|in:IN,OUT,IN_OUT',
@@ -48,11 +69,31 @@ class UpdateFirewallRuleRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.required' => 'The :attribute field, when specified, cannot be null',
             'name.string' => 'The :attribute field must contain a string',
             'name.max' => 'The :attribute field must be less than 50 characters',
-            'router_id.required' => 'The :attribute field, when specified, cannot be null',
+            'router_id.required' => 'The :attribute field is required',
             'router_id.exists' => 'The specified :attribute was not found',
+            'firewall_policy_id.required' => 'The :attribute field is required',
+            'firewall_policy_id.exists' => 'The specified :attribute was not found',
+            'source.required' => 'The :attribute field is required',
+            'source.string' => 'The :attribute field must contain a string',
+            'source_ports.required' => 'The :attribute field is required',
+            'source_ports.string' => 'The :attribute field must contain a string',
+            'service_type.required' => 'The :attribute field is required',
+            'service_type.string' => 'The :attribute field must contain a string',
+            'service_type.in' => 'The :attribute field must contain one of IP,IGMP,ICMPv4,ALG,TCP,UDP or ICMPv6',
+            'destination.required' => 'The :attribute field is required',
+            'destination.string' => 'The :attribute field must contain a string',
+            'destination_ports.required' => 'The :attribute field is required',
+            'destination_ports.string' => 'The :attribute field must contain a string',
+            'action.required' => 'The :attribute field is required',
+            'action.string' => 'The :attribute field must contain a string',
+            'action.in' => 'The :attribute field contains an invalid option',
+            'direction.required' => 'The :attribute field is required',
+            'direction.string' => 'The :attribute field must contain a string',
+            'direction.in' => 'The :attribute field contains an invalid option',
+            'enabled.required' => 'The :attribute field is required',
+            'enabled.boolean' => 'The :attribute field is not a valid boolean value',
         ];
     }
 }
