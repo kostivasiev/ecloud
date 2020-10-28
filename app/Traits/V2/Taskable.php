3bca0539ffbf30470399c0ac0b1c57c80178b5e2
Umair Majeed <umairbmajeed@gmail.com>
2020-10-28 12:28:32 +0000

    Removing Golang documentation.

--- app/Traits/V2/Taskable.php
+++ app/Traits/V2/Taskable.php
@@ -17,8 +17,8 @@ trait Taskable
     public function getTaskRunningAttribute()
     {
         return $this->tasks()->get()->filter(function ($task) {
-            return !$task->is_ended;
-        })->count() > 0;
+                return !$task->is_ended;
+            })->count() > 0;
     }
 
     public function tasks()
@@ -26,11 +26,13 @@ trait Taskable
         return $this->hasMany(Task::class, 'resource_id', 'id');
     }
 
-    // Until tasks are exposed, for now we'll compute status based on tasks associated
-    // with this resource.
-    // If a resource has at least one task, and the latested task failed, the status will be STATUS_FAILED
-    // If a resource has at least one task running, the status will be STATUS_PROVISIONING
-    // else status will be STATUS_READY
+    /**
+     * Until tasks are exposed, for now we'll compute status based on tasks associated
+     * with this resource.
+     * If a resource has at least one task, and the latest task failed, the status will be STATUS_FAILED
+     * If a resource has at least one task running, the status will be STATUS_PROVISIONING
+     * else status will be STATUS_READY
+     */
     public function getStatusAttribute()
     {
         if ($this->tasks()->count() > 0 && $this->tasks()->latest()->first()->is_failed) {
