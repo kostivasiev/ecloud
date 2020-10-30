8a1694491968423d215902b0e597b963901260d2
Umair Majeed <umairbmajeed@gmail.com>
2020-10-30 11:43:07 +0000

    Simplified error messages in request validation
    Relates to #500

--- app/Http/Requests/V2/CreateFirewallRuleRequest.php
+++ app/Http/Requests/V2/CreateFirewallRuleRequest.php
@@ -67,28 +67,13 @@ class CreateFirewallRuleRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.string' => 'The :attribute field must contain a string',
+            'required' => 'The :attribute field is required',
+            'string' => 'The :attribute field must contain a string',
             'name.max' => 'The :attribute field must be less than 50 characters',
-            'firewall_policy_id.required' => 'The :attribute field is required',
             'firewall_policy_id.exists' => 'The specified :attribute was not found',
-            'source.required' => 'The :attribute field is required',
-            'source.string' => 'The :attribute field must contain a string',
-            'source_ports.required' => 'The :attribute field is required',
-            'source_ports.string' => 'The :attribute field must contain a string',
-            'service_type.required' => 'The :attribute field is required',
-            'service_type.string' => 'The :attribute field must contain a string',
             'service_type.in' => 'The :attribute field must contain one of IP,IGMP,ICMPv4,ALG,TCP,UDP or ICMPv6',
-            'destination.required' => 'The :attribute field is required',
-            'destination.string' => 'The :attribute field must contain a string',
-            'destination_ports.required' => 'The :attribute field is required',
-            'destination_ports.string' => 'The :attribute field must contain a string',
-            'action.required' => 'The :attribute field is required',
-            'action.string' => 'The :attribute field must contain a string',
             'action.in' => 'The :attribute field contains an invalid option',
-            'direction.required' => 'The :attribute field is required',
-            'direction.string' => 'The :attribute field must contain a string',
             'direction.in' => 'The :attribute field contains an invalid option',
-            'enabled.required' => 'The :attribute field is required',
             'enabled.boolean' => 'The :attribute field is not a valid boolean value',
         ];
     }
