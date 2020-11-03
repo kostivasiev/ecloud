910a16ccca34fb9efe24b682a9d52922da732800
Umair Majeed <umairbmajeed@gmail.com>
2020-11-03 12:23:11 +0000

    tests

--- tests/V2/Vpc/DeleteTest.php
+++ tests/V2/Vpc/DeleteTest.php
@@ -3,6 +3,7 @@
 namespace Tests\V2\Vpc;
 
 use App\Models\V2\Region;
+use App\Models\V2\Router;
 use App\Models\V2\Vpc;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -59,6 +60,23 @@ class DeleteTest extends TestCase
             'detail' => 'No Vpc with that ID was found',
             'status' => 404,
         ])->assertResponseStatus(404);
+    }
+
+    public function testDeleteVpcWithResourcesFails()
+    {
+       factory(Router::class)->create([
+            'vpc_id' => $this->vpc->getKey()
+        ]);
+
+        $this->delete('/v2/vpcs/' . $this->vpc->getKey(), [], [
+            'X-consumer-custom-id' => '0-0',
+            'X-consumer-groups' => 'ecloud.write',
+        ])->seeJson([
+            'title' => 'Validation Error',
+            'detail' => 'Can not delete VPC with active resources',
+            'status' => 422,
+            'source' => 'vpc_id'
+        ])->assertResponseStatus(422);
     }
 
     public function testSuccessfulDelete()
---
+++ tests/V2/Vpc/VpcHasResourcesValidationTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\V2\Vpc;
+
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Region;
+use App\Models\V2\Router;
+use App\Models\V2\Vpc;
+use App\Rules\V2\VpcHasResources;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class VpcHasResourcesValidationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @var Region */
+    private $region;
+
+    /** @var Vpc */
+    private $vpc;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->region = factory(Region::class)->create();
+        $this->availability_zone = factory(AvailabilityZone::class)->create([
+            'region_id' => $this->region->getKey()
+        ]);
+        $this->vpc = factory(Vpc::class)->create([
+            'region_id' => $this->region->getKey(),
+        ]);
+
+        $this->validator = new VpcHasResources();
+    }
+
+    public function testNoResourcesPasses()
+    {
+        $this->assertTrue($this->validator->passes('', $this->vpc->getKey()));
+    }
+
+    public function testAssignedresourcesFails()
+    {
+        factory(Router::class)->create([
+            'vpc_id' => $this->vpc->getKey(),
+            'availability_zone_id' => $this->availability_zone->getKey()
+        ]);
+
+        $this->assertFalse($this->validator->passes('', $this->vpc->getKey()));
+    }
+}
