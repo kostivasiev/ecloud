686329d665bc36dd49ba4f64321246851239052e
Umair Majeed <umairbmajeed@gmail.com>
2020-11-03 16:09:36 +0000

    WIP

--- app/Listeners/V2/Nsx/Dhcp/Create.php
+++ app/Listeners/V2/Nsx/Dhcp/Create.php
@@ -3,24 +3,20 @@
 namespace App\Listeners\V2\Nsx\Dhcp;
 
 use App\Events\V2\Dhcp\Created;
-use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class Create implements ShouldQueue
 {
     use InteractsWithQueue;
 
-    /**
-     * @param Created $event
-     * @return void
-     * @throws \Exception
-     */
     public function handle(Created $event)
     {
         $dhcp = $event->model;
+        Log::info('NSX Dhcp Create ' . $dhcp->id . ' : Started');
         try {
-            $dhcp->availabilityZone->nsxService()->put('/policy/api/v1/infra/dhcp-server-configs/' . $dhcp->getKey(), [
+            $response = $dhcp->availabilityZone->nsxService()->put('/policy/api/v1/infra/dhcp-server-configs/' . $dhcp->id, [
                 'json' => [
                     'lease_time' => config('defaults.dhcp.lease_time'),
                     'edge_cluster_path' => '/infra/sites/default/enforcement-points/default/edge-clusters/'
@@ -29,13 +25,24 @@ class Create implements ShouldQueue
                     'tags' => [
                         [
                             'scope' => config('defaults.tag.scope'),
-                            'tag' => $dhcp->vpc->getKey()
+                            'tag' => $dhcp->vpc->id
                         ]
                     ]
                 ]
             ]);
-        } catch (GuzzleException $exception) {
-            throw new \Exception($exception->getResponse()->getBody()->getContents());
+            if ($response->getStatusCode() !== 200) {
+                $message = 'NSX Dhcp Create ' . $dhcp->id . ' : Failed';
+                Log::error($message, ['response' => $response]);
+                $this->fail(new \Exception($message));
+                return;
+            }
+        } catch (\Exception $exception) {
+            $message = 'NSX Dhcp Create ' . $dhcp->id . ' : Exception';
+            Log::error($message, ['exception' => $exception]);
+            $this->fail(new \Exception($message));
+            return;
         }
+
+        Log::info('NSX Dhcp Create ' . $dhcp->id . ' : Finished');
     }
 }
