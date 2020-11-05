230c5578b9690c371558e04e0739303641795e66
Umair Majeed <umairbmajeed@gmail.com>
2020-11-05 16:35:10 +0000

    Added test to prove solution on AvailabilityZones
    Relates to #507

---
+++ tests/V2/AvailabilityZone/DeletionRulesTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\V2\AvailabilityZone;
+
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Region;
+use App\Models\V2\Router;
+use App\Models\V2\Vpc;
+use Faker\Factory as Faker;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class DeletionRulesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $faker;
+    protected $availabilityZone;
+    protected $region;
+    protected $router;
+    protected $vpc;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+
+        $this->region = factory(Region::class)->create();
+        $this->availabilityZone = factory(AvailabilityZone::class)->create([
+            'region_id' => $this->region->getKey()
+        ]);
+        $this->vpc = factory(Vpc::class)->create([
+            'region_id' => $this->region->getKey()
+        ]);
+        $this->router = factory(Router::class)->create([
+            'name' => 'Manchester Router 1',
+            'vpc_id' => $this->vpc->getKey(),
+            'availability_zone_id' => $this->availabilityZone->getKey(),
+        ]);
+    }
+
+    public function testFailedDeletion()
+    {
+        $this->delete(
+            '/v2/availability-zones/'.$this->availabilityZone->id,
+            [],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->seeJson([
+            'detail' => 'Child records exist for this item',
+        ])->assertResponseStatus(412);
+        $availabilityZone = AvailabilityZone::withTrashed()->findOrFail($this->availabilityZone->id);
+        $this->assertNull($availabilityZone->deleted_at);
+    }
+}
