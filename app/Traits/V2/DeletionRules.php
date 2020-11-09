06ec380256dada50cc70143e9a2e8027af2222cb
Umair Majeed <umairbmajeed@gmail.com>
2020-11-09 09:41:41 +0000

    Tweaked the trait to include a scope (withAll) for loading the relationships based on the $children property of the model. Additionally includes a getRelatableInstance method.
    Relates to #507

--- app/Traits/V2/DeletionRules.php
+++ app/Traits/V2/DeletionRules.php
@@ -2,21 +2,47 @@
 
 namespace App\Traits\V2;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Log;
 
 trait DeletionRules
 {
+    /**
+     * @throws \Exception
+     */
     public static function bootDeletionRules()
     {
         static::deleting(function ($model) {
-            if (property_exists($model, 'children')) {
-                foreach ($model->children as $child) {
-                    if ($model->$child()->count() > 0) {
-                        throw new \Exception('Active resources exist for this item', 412);
+            if (method_exists($model, 'getRelatableInstance')) {
+                $model = $model->getRelatableInstance()
+                    ->findOrFail($model->getKey());
+                foreach ($model->getRelations() as $relation) {
+                    if ($relation->count() > 0) {
+                        throw new \Exception(
+                            'Active resources exist for this item',
+                            412
+                        );
                     }
                 }
             }
         });
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeWithAll(Builder $query): Builder
+    {
+        return $query->with($this->children);
+    }
+
+    /**
+     * @return Builder
+     */
+    public function getRelatableInstance(): Builder
+    {
+        return (new self)::withAll();
     }
 
     /**
