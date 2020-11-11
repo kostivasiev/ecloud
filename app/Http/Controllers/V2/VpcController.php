fd2bcf643c5ca1b2beb7ac9fd343325a80c5acf2
Umair Majeed <umairbmajeed@gmail.com>
2020-11-11 11:15:36 +0000

    fix default subnet on network for rincewind deploy

--- app/Http/Controllers/V2/VpcController.php
+++ app/Http/Controllers/V2/VpcController.php
@@ -132,7 +132,6 @@ class VpcController extends BaseController
      * @param \Illuminate\Http\Request $request
      * @param QueryTransformer $queryTransformer
      * @param string $vpcId
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection|\Illuminate\Support\HigherOrderTapProxy|mixed
      */
     public function lbcs(Request $request, QueryTransformer $queryTransformer, string $vpcId)
     {
@@ -148,7 +147,7 @@ class VpcController extends BaseController
     /**
      * @param Request $request
      * @param string $vpcId
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
      * @throws \Exception
      */
     public function deployDefaults(Request $request, string $vpcId)
@@ -174,6 +173,6 @@ class VpcController extends BaseController
         // Deploy router and network
         //event(new RouterAvailabilityZoneAttach($router, $availabilityZone));
 
-        return response()->json([], 202);
+        return response(null, 202);
     }
 }
