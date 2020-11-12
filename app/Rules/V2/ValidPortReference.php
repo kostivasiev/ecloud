dd7f18f4fe6075d391633d164e6f2e083ac33a34
Umair Majeed <umairbmajeed@gmail.com>
2020-11-12 15:57:11 +0000

    Update custom validation rule to compensate for ICMPv4 value, and if source and destination values are provided with an ICMPv4 protocol then the rule returns false, as these values are not required.
    Relates to #523

--- app/Rules/V2/ValidPortReference.php
+++ app/Rules/V2/ValidPortReference.php
@@ -6,8 +6,24 @@ use Illuminate\Contracts\Validation\Rule;
 
 class ValidPortReference implements Rule
 {
+    protected $isIcmp = false;
+
+    public function __construct()
+    {
+        $request = app('request');
+        if ($request->has('protocol') && $request->input('protocol') == 'ICMPv4') {
+            $this->isIcmp = true;
+        }
+    }
+
     public function passes($attribute, $value)
     {
+        if ($this->isIcmp) {
+            if (empty($value)) {
+                return true;
+            }
+            return false;
+        }
         foreach (explode(",", $value) as $port) {
             if (strpos($port, '-')) {
                 if (!preg_match('/\d+\-\d+/', $port)) {
@@ -23,6 +39,9 @@ class ValidPortReference implements Rule
 
     public function message()
     {
+        if ($this->isIcmp) {
+            return 'When using ICMPv4 protocol :attribute must be null';
+        }
         return 'The :attribute must be a valid port or port range';
     }
 }
