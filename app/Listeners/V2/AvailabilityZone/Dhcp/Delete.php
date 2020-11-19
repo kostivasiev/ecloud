f41f28680b40a74f51e30bb9a87ccde4e08eb9d6
Umair Majeed <umairbmajeed@gmail.com>
2020-11-19 16:05:54 +0000

    Converted Listener to a Queued Listener and incorporated the job code into the Listener.
    Relates to #527

--- app/Listeners/V2/AvailabilityZone/Dhcp/Delete.php
+++ app/Listeners/V2/AvailabilityZone/Dhcp/Delete.php
@@ -2,14 +2,22 @@
 namespace App\Listeners\V2\AvailabilityZone\Dhcp;
 
 use App\Events\V2\AvailabilityZone\Deleted;
-use App\Jobs\AvailabilityZone\DeleteDhcps;
+use App\Models\V2\AvailabilityZone;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
-class Delete
+class Delete implements ShouldQueue
 {
+    use InteractsWithQueue;
+
     public function handle(Deleted $event)
     {
-        dispatch(new DeleteDhcps([
-            'availability_zone_id' => $event->availabilityZoneId,
-        ]));
+        Log::info(get_class($this) . ' : Started', ['data' => $event->model]);
+        $availabilityZone = AvailabilityZone::withTrashed()->findOrFail($event->model->getKey());
+        $availabilityZone->dhcps()->each(function ($dhcp) {
+            $dhcp->delete();
+        });
+        Log::info(get_class($this) . ' : Finished', ['data' => $event->model]);
     }
 }
