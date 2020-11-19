4f60a004e6dadb66ceaa93904dcc6ace9b3c19b3
Umair Majeed <umairbmajeed@gmail.com>
2020-11-19 16:09:44 +0000

    Incorporated job logic into listener
    Relates to #527

--- app/Listeners/V2/AvailabilityZone/Credential/Delete.php
+++ app/Listeners/V2/AvailabilityZone/Credential/Delete.php
@@ -3,13 +3,22 @@ namespace App\Listeners\V2\AvailabilityZone\Credential;
 
 use App\Events\V2\AvailabilityZone\Deleted;
 use App\Jobs\AvailabilityZone\DeleteCredentials;
+use App\Models\V2\AvailabilityZone;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
-class Delete
+class Delete implements ShouldQueue
 {
+    use InteractsWithQueue;
+
     public function handle(Deleted $event)
     {
-        dispatch(new DeleteCredentials([
-            'availability_zone_id' => $event->availabilityZoneId,
-        ]));
+        Log::info(get_class($this) . ' : Started', ['data' => $event->model]);
+        $availabilityZone = AvailabilityZone::withTrashed()->findOrFail($event->model->getKey());
+        $availabilityZone->credentials()->each(function ($credential) {
+            $credential->delete();
+        });
+        Log::info(get_class($this) . ' : Finished', ['data' => $event->model]);
     }
 }
