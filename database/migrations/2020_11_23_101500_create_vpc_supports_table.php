7e0e9656933e402dbb607a14ae953eedf1047f18
Umair Majeed <umairbmajeed@gmail.com>
2020-11-24 16:26:20 +0000

    support crud, prop on vpc, docs & access control

---
+++ database/migrations/2020_11_23_101500_create_vpc_supports_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVpcSupporstTable extends Migration
+{
+    public function up()
+    {
+        Schema::connection('ecloud')->create('vpc_supports', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('vpc_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::connection('ecloud')->dropIfExists('vpc_supports');
+    }
+}
