17c518382db7124ada9a3401bba71562922ca84f
Umair Majeed <umairbmajeed@gmail.com>
2020-11-25 16:06:52 +0000

    Update start.sh - handle sigterm with scheduler

--- .docker/start.sh
+++ .docker/start.sh
@@ -6,6 +6,15 @@ if [ "$ROLE" = "app" ]; then
 elif [ "$ROLE" = "queue" ]; then
     php /var/www/html/artisan queue:work --verbose --tries 3 --timeout=900
 elif [ "$ROLE" = "scheduler" ]; then
+    function signal_exit
+    {
+        echo  "Caught signal, waiting for background processes to finish.."
+        wait $(jobs -pr)
+        exit 0
+    }
+
+    trap signal_exit SIGINT SIGTERM
+
     while [ true ]
     do
       php /var/www/html/artisan schedule:run --verbose &
