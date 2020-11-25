9bb3c4f24509e502bf3d35ebeb840042f1401204
Umair Majeed <umairbmajeed@gmail.com>
2020-11-25 14:10:53 +0000

    Added kingpin request to delete volume, which was missing at this point.
    Relates to #543

--- app/Jobs/Instance/Undeploy/DeleteVolumes.php
+++ app/Jobs/Instance/Undeploy/DeleteVolumes.php
@@ -4,6 +4,7 @@ namespace App\Jobs\Instance\Undeploy;
 
 use App\Jobs\Job;
 use App\Models\V2\Instance;
+use App\Models\V2\Vpc;
 use Illuminate\Support\Facades\Log;
 
 class DeleteVolumes extends Job
@@ -20,12 +21,18 @@ class DeleteVolumes extends Job
         Log::info(get_class($this) . ' : Started', ['data' => $this->data]);
 
         $instance = Instance::withTrashed()->findOrFail($this->data['instance_id']);
+        $vpc = Vpc::findOrFail($this->data['vpc_id']);
         $logMessage = 'DeleteVolumes for instance ' . $instance->getKey() . ': ';
 
-        $instance->volumes()->each(function ($volume) use ($instance) {
+        $instance->volumes()->each(function ($volume) use ($instance, $vpc) {
             // If volume is only used in this instance then delete
             if ($volume->instances()->count() == 0) {
                 Log::info('Deleting volume: ' . $volume->getKey());
+                // Send delete request to kingpin
+                $instance->availabilityZone->kingpinService()->delete(
+                    '/api/v1/vpc/' . $vpc->id . '/instance/' . $instance->id . '/volume/' . $volume->vmware_uuid
+                );
+                Log::info('Volume ' . $volume->getKey() . ' (' . $volume->vmware_uuid . ') deleted.');
                 $volume->instances()->detach($instance);
                 $volume->delete();
             }
