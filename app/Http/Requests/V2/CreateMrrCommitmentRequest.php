e7972c5b9b8511aec0e476fa1646e38146f9bb68
Umair Majeed <umairbmajeed@gmail.com>
2020-12-03 07:39:22 +0000

    Tweaked validation. Temporarily removed validation for existing in reseller_contact and employee tables, and tweaked term_length validation to use integer instead of numeric and regex to validate whole integer numbers.
    Relates to #555

--- app/Http/Requests/V2/CreateMrrCommitmentRequest.php
+++ app/Http/Requests/V2/CreateMrrCommitmentRequest.php
@@ -18,14 +18,16 @@ class CreateMrrCommitmentRequest extends FormRequest
     public function rules()
     {
         return [
-            'contact_id' => 'sometimes|required_without:employee_id|exists:reseller.reseller_contact,reseller_contact_id',
-            'employee_id' => 'sometimes|required_without:contact_id|exists:holiday.employee,employee_id',
+//            'contact_id' => 'sometimes|required_without:employee_id|exists:reseller.reseller_contact,reseller_contact_id',
+//            'employee_id' => 'sometimes|required_without:contact_id|exists:holiday.employee,employee_id',
+            'contact_id' => 'sometimes|required_without:employee_id|integer',
+            'employee_id' => 'sometimes|required_without:contact_id|integer',
             'name' => 'required|string|max:255',
             'commitment_amount' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'commitment_before_discount' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'discount_rate' => 'required|numeric|min:0|max:100',
-            'term_length' => 'required|numeric|regex:/^\d+$/|min:1',
-            'term_start_date' => 'required|date|after_or_equals:today',
+            'term_length' => 'required|integer|min:1',
+            'term_start_date' => 'required|date|after_or_equal:today',
             'term_end_date' => 'required|date|after:today',
         ];
     }
@@ -36,8 +38,8 @@ class CreateMrrCommitmentRequest extends FormRequest
             'required' => 'The :attribute field is required',
             'date' => 'The :attribute field is not a valid date',
             'numeric' => 'The :attribute field is not a numeric value',
+            'integer' => 'The :attribute field is not an integer value',
             'commitment_amount.regex' => 'The :attribute field is not a valid monetary value',
-            'term_length.regex' => 'The :attribute field is not a whole integer value',
             'term_start_date.after_or_equals' => 'The :attribute field cannot be a date in the past',
             'term_end_date.after' => 'The :attribute field must be a date after today',
         ];
