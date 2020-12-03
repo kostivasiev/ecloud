d32a253de11c2422402a03309d413f9b568a199f
Umair Majeed <umairbmajeed@gmail.com>
2020-12-03 11:20:25 +0000

    Changed key prefix following refactoring
    Relates to #555

--- app/Models/V2/DiscountPlan.php
+++ app/Models/V2/DiscountPlan.php
@@ -22,7 +22,7 @@ class DiscountPlan extends Model implements Filterable, Sortable
 {
     use CustomKey, SoftDeletes;
 
-    public $keyPrefix = 'mrrc';
+    public $keyPrefix = 'disc';
     public $incrementing = false;
     public $timestamps = true;
     protected $keyType = 'string';
