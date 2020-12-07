752a4012e0aaebf398c43534a7a828e29b22b867
Umair Majeed <umairbmajeed@gmail.com>
2020-12-07 14:49:50 +0000

    Updated EventServiceProvider to include deleting, created and deleted events
    Relates to #563

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -62,6 +62,10 @@ class EventServiceProvider extends ServiceProvider
         ],
         \App\Events\V2\Dhcp\Created::class => [
             \App\Listeners\V2\Nsx\Dhcp\Create::class,
+            \App\Listeners\V2\ResourceSync::class,
+        ],
+        \App\Events\V2\Dhcp\Deleting::class => [
+            \App\Listeners\V2\ResourceSync::class,
         ],
         \App\Events\V2\Dhcp\Deleted::class => [
             \App\Listeners\V2\Nsx\Dhcp\Delete::class,
