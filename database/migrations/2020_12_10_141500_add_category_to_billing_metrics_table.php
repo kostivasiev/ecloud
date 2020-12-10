19655f1431be08678d1276360c58eb83d69ceb60
Umair Majeed <umairbmajeed@gmail.com>
2020-12-10 14:43:00 +0000

    price and category migrations on billing_metrics table

--- database/migrations/2020_12_10_141500_add_category_to_billing_metrics_table.php
+++ database/migrations/2020_12_10_141500_add_category_to_billing_metrics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPriceToBillingMetricsTable extends Migration
+class AddCategoryToBillingMetricsTable extends Migration
 {
     public function up()
     {
