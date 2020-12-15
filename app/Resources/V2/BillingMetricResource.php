b4056e119fb2e05ac3810846ca23bdfcd117b115
Umair Majeed <umairbmajeed@gmail.com>
2020-12-15 07:49:29 +0000

    Removed number formatting
    Relates to #577

--- app/Resources/V2/BillingMetricResource.php
+++ app/Resources/V2/BillingMetricResource.php
@@ -16,7 +16,7 @@ class BillingMetricResource extends UKFastResource
             'key' => $this->key,
             'value' => $this->value,
             'category' => $this->category,
-            'price' => (float) number_format($this->price, 2),
+            'price' => (float) $this->price,
             'start' => $this->start === null ? null : Carbon::parse(
                 $this->start,
                 new \DateTimeZone(config('app.timezone'))
