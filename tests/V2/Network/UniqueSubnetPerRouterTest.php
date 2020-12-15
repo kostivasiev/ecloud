458b0bff5fc6b0ebc826e8e5c7bf6ab4cc762021
Umair Majeed <umairbmajeed@gmail.com>
2020-12-15 13:38:26 +0000

    Added tests to prove that the removal of the unique rule was valid. An additional test shows that you can update the same record with the same data without triggering the overlapping subnet validation rule.
    Relates to #581

---
+++ tests/V2/Network/UniqueSubnetPerRouterTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\V2\Network;
+
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Network;
+use App\Models\V2\Region;
+use App\Models\V2\Router;
+use App\Models\V2\Vpc;
+use App\Rules\V2\IsNotOverlappingSubnet;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class UniqueSubnetPerRouterTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected AvailabilityZone $availabilityZone;
+    protected Network $network;
+    protected Network $network2;
+    protected Region $region;
+    protected Router $router;
+    protected Router $router2;
+    protected Vpc $vpc;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->region = factory(Region::class)->create([
+            'name' => 'testregion',
+        ]);
+        $this->availabilityZone = factory(AvailabilityZone::class)->create([
+            'region_id' => $this->region->getKey(),
+        ]);
+        $this->vpc = factory(Vpc::class)->create([
+            'region_id' => $this->region->getKey(),
+        ]);
+        $this->router = factory(Router::class)->create([
+            'vpc_id' => $this->vpc->getKey(),
+            'availability_zone_id' => $this->availabilityZone->getKey(),
+        ]);
+        $this->router2 = factory(Router::class)->create([
+            'vpc_id' => $this->vpc->getKey(),
+            'availability_zone_id' => $this->availabilityZone->getKey(),
+        ]);
+        $this->network = factory(Network::class)->create([
+            'router_id' => $this->router->getKey(),
+            'subnet' => '10.0.0.1/30',
+        ]);
+        $this->network2 = factory(Network::class)->create([
+            'router_id' => $this->router->getKey(),
+            'subnet' => '10.0.0.2/30',
+        ]);
+    }
+
+    public function testOverlapDetectionWorks()
+    {
+        $this->post(
+            '/v2/networks',
+            [
+                'name' => 'Manchester Network',
+                'router_id' => $this->router->getKey(),
+                'subnet' => '10.0.0.1/22'
+            ],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->seeJson([
+            'title' => 'Validation Error',
+            'detail' => 'The subnet must not overlap an existing CIDR range',
+        ])->assertResponseStatus(422);
+    }
+
+    public function testSuccessfulCreation()
+    {
+        $this->post(
+            '/v2/networks',
+            [
+                'name' => 'Manchester Network',
+                'router_id' => $this->router2->getKey(),
+                'subnet' => '10.0.0.1/22'
+            ],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->assertResponseStatus(201);
+    }
+
+    public function testPatchIsSuccessful()
+    {
+        $this->post(
+            '/v2/networks',
+            [
+                'name' => 'Manchester Network',
+                'router_id' => $this->router2->getKey(),
+                'subnet' => '10.0.0.1/22'
+            ],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->assertResponseStatus(201);
+
+        $networkId = (json_decode($this->response->getContent()))->data->id;
+
+        // update the record using the same data. Before the fix the same subnet for the same
+        // record would result in an overlapping subnet error.
+        $this->patch(
+            '/v2/networks/'.$networkId,
+            [
+                'name' => 'Updated Network',
+                'router_id' => $this->router2->getKey(),
+                'subnet' => '10.0.0.1/22'
+            ],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->assertResponseStatus(200);
+    }
+
+}
