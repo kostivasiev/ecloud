1b498873b36adcddbe663f7403e9a34d9d4297cc
Umair Majeed <umairbmajeed@gmail.com>
2020-12-17 11:17:57 +0000

    Tweaked validation rules and added additional validations on term_end_date for both Create and Update requests, incorporating those into tests too.
    Relates to #555

--- app/Http/Requests/V2/DiscountPlan/Create.php
+++ app/Http/Requests/V2/DiscountPlan/Create.php
@@ -22,13 +22,23 @@ class Create extends FormRequest
 //            'employee_id' => 'sometimes|required_without:contact_id|exists:holiday.employee,employee_id',
             'contact_id' => 'sometimes|required_without:employee_id|integer',
             'employee_id' => 'sometimes|required_without:contact_id|integer',
-            'name' => 'required|string|max:255',
+            'reseller_id' => 'required|integer',
+            'name' => 'sometimes|required|string|max:255',
             'commitment_amount' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'commitment_before_discount' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'discount_rate' => 'required|numeric|min:0|max:100',
             'term_length' => 'required|integer|min:1',
             'term_start_date' => 'required|date|after_or_equal:today',
-            'term_end_date' => 'required|date|after:today',
+            'term_end_date' => [
+                'required',
+                'date',
+                'after:today',
+                function ($attribute, $value, $fail) {
+                    if (strtotime($value) <= strtotime($this->request->get('term_start_date'))) {
+                        $fail('The '.$attribute.' must be greater than the term_start_date');
+                    }
+                }
+            ],
         ];
     }
 
