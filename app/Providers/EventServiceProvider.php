4a596d9bb50968f76d13e0063ec17f66d165b9a5
Umair Majeed <umairbmajeed@gmail.com>
2020-12-18 09:55:14 +0000

    Changed FirewallPolicy\Deploy call to FirewallPolicy\Undeploy
    Relates to #527

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -97,7 +97,7 @@ class EventServiceProvider extends ServiceProvider
             \App\Listeners\V2\FirewallPolicy\Deploy::class,
         ],
         \App\Events\V2\FirewallRule\Deleted::class => [
-            \App\Listeners\V2\FirewallPolicy\Deploy::class,
+            \App\Listeners\V2\FirewallPolicy\Undeploy::class,
             \App\Listeners\V2\FirewallPolicy\FirewallRule\FirewallRulePort\Delete::class,
             \App\Listeners\V2\FirewallRule\Undeploy::class,
             \App\Listeners\V2\BillingMetric\End::class,
