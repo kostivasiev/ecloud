3c2cf062ea5bef4e95a9573b5521edc41c25f4f2
Umair Majeed <umairbmajeed@gmail.com>
2021-01-19 10:59:30 +0000

    Added Undeploy and UndeployCheck code for routers
    #584 Routers not removed from NSX when deleted

--- app/Jobs/Nsx/Router/UndeployCheck.php
+++ app/Jobs/Nsx/Router/UndeployCheck.php
@@ -21,13 +21,26 @@ class UndeployCheck extends Job
 
     public function handle()
     {
-        Log::info(get_class($this) . ' : Started', ['model' => $this->model]);
+        Log::info(get_class($this) . ' : Started', ['model' => ['id' => $this->model->id]]);
 
         // TODO :- Undeploy Router
+        $response = $this->model->availabilityZone->nsxService()->get(
+            'policy/api/v1/infra/tier-1s/' . $this->model->id . '?include_mark_for_delete_objects=true'
+        );
+        $response = json_decode($response->getBody()->getContents());
+        foreach ($response->results as $segment) {
+            if ($this->model->id === $segment->id) {
+                $this->release(static::RETRY_DELAY);
+                Log::info(
+                    'Waiting for segment ' . $this->model->id . ' being deleted, retrying in ' . static::RETRY_DELAY . ' seconds'
+                );
+                return;
+            }
+        }
 
         $this->model->setSyncCompleted();
         $this->model->syncDelete();
 
-        Log::info(get_class($this) . ' : Finished', ['model' => $this->model]);
+        Log::info(get_class($this) . ' : Finished', ['model' => ['id' => $this->model->id]]);
     }
 }
