39b7eaaa9e57f6a90b03d142a8a37abe3c6976be
Umair Majeed <umairbmajeed@gmail.com>
2021-01-21 10:58:29 +0000

    WIP

--- app/Console/Commands/Nsx/UndeployDeletedRouters.php
+++ app/Console/Commands/Nsx/UndeployDeletedRouters.php
@@ -28,20 +28,55 @@ class UndeployDeletedRouters extends Command
                     'policy/api/v1/infra/tier-1s/' . $router->id
                 );
             } catch (ClientException $exception) {
-                if ($exception->getCode() == 404) {
+                $response = $exception->getResponse();
+                if ($response->getStatusCode() == 404) {
                     return true;
                 }
             }
 
-            $router->availabilityZone->nsxService()->delete(
-                'policy/api/v1/infra/tier-1s/'.$router->id.'/locale-services/'.$router->id
-            );
-
-            $router->availabilityZone->nsxService()->delete(
-                'policy/api/v1/infra/tier-1s/' . $router->id
-            );
+            $this->drillDelete($router->availabilityZone->nsxService(), 'policy/api/v1/infra/tier-1s/' . $router->id);
 
             $this->info('Network ' . $router->id . ' Undeployed.');
         });
+    }
+
+    private function drillDelete($nsxService, $resource)
+    {
+        $deleted = false;
+        do {
+            $this->info('Deleting "' . $resource . '"');
+            try {
+                $nsxService->delete($resource);
+                $deleted = true;
+            } catch (ClientException $exception) {
+                $response = $exception->getResponse();
+                if ($response->getStatusCode() !== 400) {
+                    throw $exception;
+                }
+
+                $json = json_decode($response->getBody()->getContents());
+                if (!preg_match(
+                    '/The object path=\[[^\]]+\] cannot be deleted as either it has children or it is being referenced by other objects path=\[[^\]]+\]/',
+                    $json->error_message
+                )) {
+                    throw $exception;
+                }
+
+                $childPath = preg_replace(
+                    '/The object path=\[[^\]]+\] cannot be deleted as either it has children or it is being referenced by other objects path=\[([^\]]+)\]/',
+                    '$1',
+                    $json->error_message
+                );
+
+                $childPath = 'policy/api/v1' . $childPath;
+
+                $this->warn('Failed to delete due to dependant "' . $childPath . '"');
+
+                $this->drillDelete($nsxService, $childPath);
+            }
+            sleep(1);
+        } while(!$deleted);
+
+        $this->info('Deleted "' . $resource . '"');
     }
 }
