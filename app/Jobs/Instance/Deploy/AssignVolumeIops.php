5e62703f83e8a2151ed0c2a3f4e56316008fe416
Umair Majeed <umairbmajeed@gmail.com>
2021-01-26 13:20:28 +0000

    Assign iops to volume at end of deployment process. This will also trigger the billing metrics insertion
    #651 NSX | Volumes - Update Instance creation endpoint to accept IOPS

---
+++ app/Jobs/Instance/Deploy/AssignVolumeIops.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs\Instance\Deploy;
+
+use App\Jobs\Job;
+use App\Models\V2\Instance;
+use Illuminate\Support\Facades\Log;
+
+class AssignVolumeIops extends Job
+{
+    private $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function handle()
+    {
+        Log::info(get_class($this) . ' : Started', ['data' => $this->data]);
+
+        // Finally set the iops, we can rely on the update events to handle the billing lines
+        $instance = Instance::findOrFail($this->data['instance_id']);
+        $volume = $instance->volumes->first();
+        $volume->iops = $this->data['iops'];
+        $volume->save();
+        Log::info('Volume ' . $volume->getKey() . ' iops set to ' . $volume->iops);
+
+        Log::info(get_class($this) . ' : Finished', ['data' => $this->data]);
+    }
+
+}
