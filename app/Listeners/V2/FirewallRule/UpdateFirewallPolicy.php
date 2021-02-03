fd1ceb050a9146823e1837833f0570e641d4c899
Umair Majeed <umairbmajeed@gmail.com>
2021-02-03 15:57:51 +0000

    Quick fix for FirewallRule(Port) not triggering FirewallPolicy sync.

--- app/Listeners/V2/FirewallRule/UpdateFirewallPolicy.php
+++ app/Listeners/V2/FirewallRule/UpdateFirewallPolicy.php
@@ -2,13 +2,11 @@
 
 namespace App\Listeners\V2\FirewallRule;
 
-use App\Events\V2\FirewallRule\Saved;
-use App\Jobs\Sync\FirewallPolicy\Save;
 use Illuminate\Support\Facades\Log;
 
 class UpdateFirewallPolicy
 {
-    public function handle(Saved $event)
+    public function handle($event)
     {
         Log::info(get_class($this) . ' : Started', ['event' => $event]);
 
--- app/Listeners/V2/FirewallRulePort/UpdateFirewallPolicy.php
+++ app/Listeners/V2/FirewallRulePort/UpdateFirewallPolicy.php
@@ -2,13 +2,11 @@
 
 namespace App\Listeners\V2\FirewallRulePort;
 
-use App\Events\V2\FirewallRulePort\Saved;
-use App\Jobs\Sync\FirewallPolicy\Save;
 use Illuminate\Support\Facades\Log;
 
 class UpdateFirewallPolicy
 {
-    public function handle(Saved $event)
+    public function handle($event)
     {
         Log::info(get_class($this) . ' : Started', ['event' => $event]);
 
