5a4acbacf4304410a152665e7899d2fd905eea00
Umair Majeed <umairbmajeed@gmail.com>
2021-02-10 12:09:51 +0000

    WIP

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -242,10 +242,6 @@ class EventServiceProvider extends ServiceProvider
             \App\Listeners\V2\Instance\UpdateVcpuBilling::class,
             \App\Listeners\V2\Instance\UpdateLicenseBilling::class,
             \App\Listeners\V2\Instance\UpdateBackupBilling::class,
-        ],
-
-//        \Illuminate\Queue\Events\JobExceptionOccurred::class => [
-//            \App\Listeners\V2\JobExceptionHandler::class,
-//        ]
+        ]
     ];
 }
