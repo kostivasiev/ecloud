c2dec2528ef3fac5d427849c3fc5556efcd97654
Umair Majeed <umairbmajeed@gmail.com>
2021-02-11 13:51:45 +0000

    Added Delete, Undeploy and UndeployCheck jobs
    #643-nsx-network-acl-rules-crud

---
+++ app/Jobs/Nsx/NetworkAclRule/Undeploy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs\Nsx\NetworkAclRule;
+
+use App\Jobs\Job;
+use App\Models\V2\NetworkAclRule;
+use Illuminate\Support\Facades\Log;
+
+class Undeploy extends Job
+{
+    const RETRY_DELAY = 5;
+
+    public $tries = 500;
+
+    private $model;
+
+    public function __construct(NetworkAclRule $model)
+    {
+        $this->model = $model;
+    }
+
+    public function handle()
+    {
+        Log::info(get_class($this) . ' : Started', ['id' => $this->model->id]);
+
+        // @todo NSX undeploy to be added here
+
+        Log::info(get_class($this) . ' : Finished', ['id' => $this->model->id]);
+    }
+}
