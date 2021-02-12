b3fed84a06779e2ba861b6372433c75ce815c44a
Umair Majeed <umairbmajeed@gmail.com>
2021-02-12 14:56:53 +0000

    Removed ACL reference
    #642-nsx-network-acl-policy-crud

--- app/Models/V2/Vpc.php
+++ app/Models/V2/Vpc.php
@@ -84,7 +84,7 @@ class Vpc extends Model implements Filterable, Sortable
         return $this->hasMany(VpcSupport::class);
     }
 
-    public function networkAcls()
+    public function networkPolicies()
     {
         return $this->hasMany(NetworkPolicy::class);
     }
