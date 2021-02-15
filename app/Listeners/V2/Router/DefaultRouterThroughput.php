53c3e0047d61c1950ce3e5823822274e3fd2cafa
Umair Majeed <umairbmajeed@gmail.com>
2021-02-15 09:33:14 +0000

    WIP

--- app/Listeners/V2/Router/DefaultRouterThroughput.php
+++ app/Listeners/V2/Router/DefaultRouterThroughput.php
@@ -14,37 +14,39 @@ class DefaultRouterThroughput
         /** @var Router $router */
         $router = $event->model;
 
-        if (empty($router->router_throughput_id)) {
-            if (empty($router->availability_zone_id)) {
-                $availabilityZone = Vpc::forUser(app('request')->user)
-                    ->findOrFail($router->vpc_id)
-                    ->region
-                    ->availabilityZones
-                    ->first();
-            } else {
-                $availabilityZone = $router->availabilityZone;
-            }
-
-            if (empty($availabilityZone)) {
-                Log::error('Failed to set default router throughput for router ' . $router->id
-                    . ', unable to determine the router\s availability zone');
-                return;
-            }
-
-            // Default router throughput is 20Mbps
-            $routerThroughput = $availabilityZone->routerThroughputs
-                ->where('committed_bandwidth', config('router.throughput.default.bandwidth'))
-                ->first();
-
-            if (empty($routerThroughput)) {
-                Log::error('Failed to set default router throughput for router ' . $router->id
-                    . ', the default valued router throughput record for availability zone ' . $availabilityZone->id . ' was not found');
-                return;
-            }
-
-            $router->router_throughput_id = $routerThroughput->id;
-
-            Log::info(get_class($this) . ' : Throughput was not specified whilst creating router, set to default for availability zone ' . $availabilityZone->id . ': ' . $routerThroughput->id . ' (' . $routerThroughput->name . ')');
+        if (!empty($router->router_throughput_id)) {
+            return;
         }
+
+        if (empty($router->availability_zone_id)) {
+            $availabilityZone = Vpc::forUser(app('request')->user)
+                ->findOrFail($router->vpc_id)
+                ->region
+                ->availabilityZones
+                ->first();
+        } else {
+            $availabilityZone = $router->availabilityZone;
+        }
+
+        if (empty($availabilityZone)) {
+            Log::error('Failed to set default router throughput for router ' . $router->id
+                . ', unable to determine the router\s availability zone');
+            return;
+        }
+
+        // Default router throughput is 20Mbps
+        $routerThroughput = $availabilityZone->routerThroughputs
+            ->where('committed_bandwidth', config('router.throughput.default.bandwidth'))
+            ->first();
+
+        if (empty($routerThroughput)) {
+            Log::error('Failed to set default router throughput for router ' . $router->id
+                . ', the default valued router throughput record for availability zone ' . $availabilityZone->id . ' was not found');
+            return;
+        }
+
+        $router->router_throughput_id = $routerThroughput->id;
+
+        Log::info(get_class($this) . ' : Throughput was not specified whilst creating router, set to default for availability zone ' . $availabilityZone->id . ': ' . $routerThroughput->id . ' (' . $routerThroughput->name . ')');
     }
 }
