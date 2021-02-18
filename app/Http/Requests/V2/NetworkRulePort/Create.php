867922a0e133a8d30fb0b5c5a71cae490a228a1e
Umair Majeed <umairbmajeed@gmail.com>
2021-02-18 07:45:32 +0000

    Removing methods that are not used

--- app/Http/Requests/V2/NetworkRulePort/Create.php
+++ app/Http/Requests/V2/NetworkRulePort/Create.php
@@ -9,21 +9,6 @@ use UKFast\FormRequests\FormRequest;
 
 class Create extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
@@ -50,15 +35,5 @@ class Create extends FormRequest
                 new ValidPortReference()
             ],
         ];
-    }
-
-    /**
-     * Get the validation messages that apply to the request.
-     *
-     * @return array|string[]
-     */
-    public function messages()
-    {
-        return [];
     }
 }
--- app/Http/Requests/V2/NetworkRulePort/Update.php
+++ app/Http/Requests/V2/NetworkRulePort/Update.php
@@ -9,21 +9,6 @@ use UKFast\FormRequests\FormRequest;
 
 class Update extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
@@ -52,15 +37,5 @@ class Update extends FormRequest
                 new ValidPortReference()
             ],
         ];
-    }
-
-    /**
-     * Get the validation messages that apply to the request.
-     *
-     * @return array|string[]
-     */
-    public function messages()
-    {
-        return [];
     }
 }
