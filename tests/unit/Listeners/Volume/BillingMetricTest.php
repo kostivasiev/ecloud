66e569386cc827ae82ddafcf36271489dec2366e
Umair Majeed <umairbmajeed@gmail.com>
2021-02-24 17:22:43 +0000

    WIP

--- tests/unit/Listeners/Volume/BillingMetricTest.php
+++ tests/unit/Listeners/Volume/BillingMetricTest.php
@@ -34,19 +34,6 @@ class BillingMetricTest extends TestCase
                 return new Response(200, [], json_encode(['uuid' => 'uuid-test-uuid-test-uuid-test']));
             });
 
-        $this->kingpinServiceMock()->expects('put')
-            ->withArgs([
-                '/api/v1/vpc/vpc-test/volume/uuid-test-uuid-test-uuid-test/size',
-                [
-                    'json' => [
-                        'sizeGiB' => '10',
-                    ]
-                ]
-            ])
-            ->andReturnUsing(function () {
-                return new Response(200);
-            });
-
         $this->volume = factory(Volume::class)->create([
             'id' => 'vol-test',
             'vpc_id' => $this->vpc()->id,
