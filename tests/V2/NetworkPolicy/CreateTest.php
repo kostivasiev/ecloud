959c9673534fbe258a8093559fe6c64c897c2958
Umair Majeed <umairbmajeed@gmail.com>
2021-02-24 11:50:34 +0000

    Update network policy create tests

--- tests/V2/NetworkPolicy/CreateTest.php
+++ tests/V2/NetworkPolicy/CreateTest.php
@@ -2,7 +2,6 @@
 namespace Tests\V2\NetworkPolicy;
 
 use App\Models\V2\NetworkPolicy;
-use App\Models\V2\Network;
 use GuzzleHttp\Psr7\Response;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -11,41 +10,43 @@ class CreateTest extends TestCase
 {
     use DatabaseMigrations;
 
-    protected Network $network;
-
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
-        $this->vpc();
-        $this->availabilityZone();
+        $this->network();
 
         // bind data so we can use actual NSX mocks
         app()->bind(NetworkPolicy::class, function () {
             return factory(NetworkPolicy::class)->make([
                 'id' => 'np-abc123xyz',
-                'network_id' => 'net-test',
+                'network_id' => $this->network()->id,
                 'name' => 'Test Policy',
             ]);
         });
 
-        $mockIds = ['np-abc123xyz', 'np-zzzxxxyyy'];
-        foreach ($mockIds as $mockId) {
+        $mockNetworkPolicyIds = ['np-abc123xyz', 'np-zzzxxxyyy'];
+        foreach ($mockNetworkPolicyIds as $mockNetworkPolicyId) {
             $this->nsxServiceMock()->shouldReceive('patch')
-                ->withSomeOfArgs('/policy/api/v1/infra/domains/default/security-policies/'.$mockId)
+                ->withSomeOfArgs('/policy/api/v1/infra/domains/default/groups/' . $mockNetworkPolicyId)
                 ->andReturnUsing(function () {
                     return new Response(200, [], '');
                 });
+            $this->nsxServiceMock()->shouldReceive('get')
+                ->withArgs(['policy/api/v1/infra/realized-state/status?intent_path=/infra/domains/default/groups/' . $mockNetworkPolicyId])
+                ->andReturnUsing(function () {
+                    return new Response(200, [], json_encode(['publish_status' => 'REALIZED']));
+                });
             $this->nsxServiceMock()->shouldReceive('patch')
-                ->withSomeOfArgs('/policy/api/v1/infra/domains/default/groups/'.$mockId)
+                ->withSomeOfArgs('/policy/api/v1/infra/domains/default/security-policies/' . $mockNetworkPolicyId)
                 ->andReturnUsing(function () {
                     return new Response(200, [], '');
+                });
+            $this->nsxServiceMock()->shouldReceive('get')
+                ->withArgs(['policy/api/v1/infra/realized-state/status?intent_path=/infra/domains/default/security-policies/' . $mockNetworkPolicyId])
+                ->andReturnUsing(function () {
+                    return new Response(200, [], json_encode(['publish_status' => 'REALIZED']));
                 });
         }
-
-        $this->network = factory(Network::class)->create([
-            'id' => 'net-test',
-            'router_id' => $this->router()->id,
-        ]);
     }
 
     public function testCreateResource()
@@ -75,7 +76,7 @@ class CreateTest extends TestCase
     {
         $data = [
             'name' => 'Test Policy',
-            'network_id' => $this->network->id,
+            'network_id' => $this->network()->id,
         ];
         factory(NetworkPolicy::class)->create(array_merge(['id' => 'np-zzzxxxyyy'], $data));
         $this->post(
