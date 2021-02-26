29360dbe29a5403c3f0467efb4bc40bd56c5aab7
Umair Majeed <umairbmajeed@gmail.com>
2021-02-26 11:06:27 +0000

    Reinstated PATCH endpoint and added name attribute to model, validations, documentation and included a migration to add name column.
    #724-dhcp-update-allows-vpc-and-availability-zone-to-be-specified

--- app/Http/Controllers/V2/DhcpController.php
+++ app/Http/Controllers/V2/DhcpController.php
@@ -50,10 +50,24 @@ class DhcpController extends BaseController
      */
     public function create(CreateDhcpRequest $request)
     {
-        $dhcps = new Dhcp($request->only(['vpc_id', 'availability_zone_id']));
+        $dhcps = new Dhcp($request->only(['name', 'vpc_id', 'availability_zone_id']));
         $dhcps->save();
         $dhcps->refresh();
         return $this->responseIdMeta($request, $dhcps->id, 201);
+    }
+
+    /**
+     * @param UpdateDhcpRequest $request
+     * @param string $dhcpId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateDhcpRequest $request, string $dhcpId)
+    {
+        $dhcp = Dhcp::findOrFail($dhcpId);
+        $dhcp->fill($request->only(['name']));
+        $dhcp->save();
+        $dhcp->setSyncCompleted();
+        return $this->responseIdMeta($request, $dhcp->id, 200);
     }
 
     public function destroy(string $dhcpId)
---
+++ tests/V2/Dhcp/UpdateTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\V2\Dhcp;
+
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Dhcp;
+use App\Models\V2\Region;
+use App\Models\V2\Vpc;
+use Faker\Factory as Faker;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected Dhcp $dhcp;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->dhcp = factory(Dhcp::class)->create([
+            'vpc_id' => $this->vpc()->id,
+            'availability_zone_id' => $this->availabilityZone()->id
+        ]);
+    }
+
+    public function testNoPermsIsDenied()
+    {
+        $this->patch(
+            '/v2/dhcps/' . $this->dhcp->id,
+            [
+                'name' => 'Updated Name',
+            ],
+            []
+        )
+            ->seeJson([
+                'title' => 'Unauthorized',
+                'detail' => 'Unauthorized',
+                'status' => 401,
+            ])
+            ->assertResponseStatus(401);
+    }
+
+    public function testNullNameIsDenied()
+    {
+        $this->patch(
+            '/v2/dhcps/' . $this->dhcp->id,
+            [
+                'name' => '',
+            ],
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->seeJson(
+            [
+                'title' => 'Validation Error',
+                'detail' => 'The name field is required',
+                'status' => 422,
+                'source' => 'name'
+            ]
+        )
+            ->assertResponseStatus(422);
+    }
+
+    public function testValidDataIsSuccessful()
+    {
+        $data = [
+            'name' => 'Updated Name',
+        ];
+        $this->patch(
+            '/v2/dhcps/' . $this->dhcp->id,
+            $data,
+            [
+                'X-consumer-custom-id' => '0-0',
+                'X-consumer-groups' => 'ecloud.write',
+            ]
+        )->assertResponseStatus(200);
+
+        $dhcp = Dhcp::findOrFail($this->dhcp->id);
+        $this->assertEquals($data['name'], $dhcp->name);
+    }
+}
