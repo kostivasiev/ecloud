986be2f683053a771819518e46c18ea61ed7aa23
Umair Majeed <umairbmajeed@gmail.com>
2021-03-03 13:33:00 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/Http/Controllers/V2/NetworkRuleController.php
+++ app/Http/Controllers/V2/NetworkRuleController.php
@@ -58,7 +58,7 @@ class NetworkRuleController extends BaseController
             'enabled',
         ]));
         $networkRule->save();
-        return $this->responseIdMeta($request, $networkRule->getKey(), 200);
+        return $this->responseIdMeta($request, $networkRule->id, 200);
     }
 
     public function destroy(Request $request, string $networkRuleId)
