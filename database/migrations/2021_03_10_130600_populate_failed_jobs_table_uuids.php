22e55ede4b55e0e8c889e1d0960dd1091798d042
Umair Majeed <umairbmajeed@gmail.com>
2021-03-10 13:48:24 +0000

    updated phpunit config

--- database/migrations/2021_03_10_130600_populate_failed_jobs_table_uuids.php
+++ database/migrations/2021_03_10_130600_populate_failed_jobs_table_uuids.php
@@ -3,12 +3,12 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class PopulateFailedJobsTableTableUuids extends Migration
+class PopulateFailedJobsTableUuids extends Migration
 {
     public function up()
     {
-        DB::table('failed_jobs')->whereNull('uuid')->cursor()->each(function ($job) {
-            DB::table('failed_jobs')
+        DB::connection('ecloud')->table('failed_jobs')->whereNull('uuid')->cursor()->each(function ($job) {
+            DB::connection('ecloud')->table('failed_jobs')
                 ->where('id', $job->id)
                 ->update(['uuid' => (string) Illuminate\Support\Str::uuid()]);
         });
