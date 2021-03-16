157d1dd55cac0420c16fc7c2308f9e16ba5ea022
Umair Majeed <umairbmajeed@gmail.com>
2021-03-16 07:59:14 +0000

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -310,6 +310,58 @@ abstract class TestCase extends \Laravel\Lumen\Testing\TestCase
             ->andReturnUsing(function () {
                 return new Response(200);
             });
+
+        // PrepareCluster Job
+        $this->nsxServiceMock()->expects('get')
+            ->with('/api/v1/transport-node-collections')
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'results' => [
+                        [
+                            'id' => 'TEST-TRANSPORT-NODE-COLLECTION-ID',
+                            'display_name' => 'TEST-TRANSPORT-NODE-COLLECTION-DISPLAY-NAME',
+                        ],
+                    ],
+                ]));
+            });
+        $this->nsxServiceMock()->expects('get')
+            ->with('/api/v1/search/query?query=resource_type:TransportNodeProfile%20AND%20display_name:tnp-hg-test')
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'results' => [
+                        [
+                            'id' => 'TEST-TRANSPORT-NODE-COLLECTION-ID',
+                        ],
+                    ],
+                ]));
+            });
+        $this->nsxServiceMock()->expects('get')
+            ->with('/api/v1/fabric/compute-collections?origin_type=VC_Cluster&display_name=hg-test')
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'results' => [
+                        [
+                            'external_id' => 'TEST-COMPUTE-COLLECTION-ID',
+                        ],
+                    ],
+                ]));
+            });
+        $this->nsxServiceMock()->expects('post')
+            ->withSomeOfArgs(
+                '/api/v1/transport-node-collections',
+                [
+                    'json' => [
+                        'resource_type' => 'TransportNodeCollection',
+                        'display_name' => 'hg-test-tnc',
+                        'description' => 'API created Transport Node Collection',
+                        'compute_collection_id' => 'TEST-COMPUTE-COLLECTION-ID',
+                        'transport_node_profile_id' => 'TEST-TRANSPORT-NODE-COLLECTION-ID',
+                    ]
+                ]
+            )
+            ->andReturnUsing(function () {
+                return new Response(200);
+            });
     }
 
     public function kingpinServiceMock()
