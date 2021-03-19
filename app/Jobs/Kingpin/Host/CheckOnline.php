f69f3c900f4b88bc433439305a5f011dfaa44d03
Umair Majeed <umairbmajeed@gmail.com>
2021-03-19 15:46:23 +0000

    WIP - Worked on this without realising the 3.x jobs had been assigned to Paul. Committed work I've done, but if you want to use it or dump it Paul it's up to you.
    #625-3-7-nsx-dedicated-hosts-add-host-listener-wait-for-vmware-job

--- app/Jobs/Kingpin/Host/CheckOnline.php
+++ app/Jobs/Kingpin/Host/CheckOnline.php
@@ -4,10 +4,15 @@ namespace App\Jobs\Kingpin\Host;
 
 use App\Jobs\Job;
 use App\Models\V2\Host;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ServerException;
 use Illuminate\Support\Facades\Log;
 
 class CheckOnline extends Job
 {
+    const RETRY_DELAY = 5;
+    public $tries = 60;
     private $model;
 
     public function __construct(Host $model)
@@ -19,13 +24,50 @@ class CheckOnline extends Job
     {
         Log::info(get_class($this) . ' : Started', ['id' => $this->model->id]);
 
-        // TODO :- https://gitlab.devops.ukfast.co.uk/ukfast/api.ukfast/ecloud/-/issues/625
+        $host = $this->model;
+        $availabilityZone = $host->hostGroup->availabilityZone;
+
+        // Get the host spec from Conjurer
+        $response = $availabilityZone->conjurerService()->get(
+            '/api/v2/compute/' . $availabilityZone->ucs_compute_name . '/vpc/' . $host->hostGroup->vpc->id .'/host/' . $host->id
+        );
+        $response = json_decode($response->getBody()->getContents());
+
+        $macAddress = collect($response->interfaces)->firstWhere('name', 'eth0')->address;
+        if (empty($macAddress)) {
+            $message = 'Failed to load eth0 address for host ' . $host->id;
+            Log::error($message);
+            $this->fail(new \Exception($message));
+            return false;
+        }
+
+        try {
+            $response = $host->hostGroup->availabilityZone->kingpinService()->get(
+                '/api/v2/vpc/' . $host->hostGroup->vpc_id . '/hostgroup/' . $host->hostGroup->id . '/host/' . $macAddress
+            );
+            $response = json_decode($response->getBody()->getContents());
+        } catch (ClientException|ServerException $e) { // handle 40x/50x response if host not found
+            Log::debug('Waiting for Host ' . $host->id . ' to come online...');
+            $this->release(static::RETRY_DELAY);
+            return false;
+        }
+
+        if ($response->powerState !== 'On') {
+            Log::debug('Waiting for Host ' . $host->id . ' power state...');
+            $this->release(static::RETRY_DELAY);
+            return false;
+        }
+
+        // Power on received - move to next step
 
         Log::info(get_class($this) . ' : Finished', ['id' => $this->model->id]);
     }
 
     public function failed($exception)
     {
-        $this->model->setSyncFailureReason($exception->getMessage());
+        $message = ($exception instanceof RequestException && $exception->hasResponse()) ?
+            $exception->getResponse()->getBody()->getContents() :
+            $exception->getMessage();
+        $this->model->setSyncFailureReason($message);
     }
 }
