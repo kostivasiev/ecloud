5a9f04a128a62c995c0b5a311bbfe8b3e5ff17da
Umair Majeed <umairbmajeed@gmail.com>
2021-03-22 19:53:12 +0000

    fix dhcp lease removal

--- app/Jobs/Nsx/Nic/RemoveDHCPLease.php
+++ app/Jobs/Nsx/Nic/RemoveDHCPLease.php
@@ -25,30 +25,13 @@ class RemoveDHCPLease extends Job
 
         $nsxService = $this->nic->instance->availabilityZone->nsxService();
 
-        /**
-         * Create dhcp lease for the ip to the nic's mac address on NSX
-         * @see https://185.197.63.88/policy/api_includes/method_CreateOrReplaceSegmentDhcpStaticBinding.html
-         */
-        try {
-            $nsxService->put(
-                '/policy/api/v1/infra/tier-1s/' . $this->nic->network->router->id . '/segments/' . $this->nic->network->id
-                . '/dhcp-static-binding-configs/' . $this->nic->id,
-                [
-                    'json' => [
-                        'resource_type' => 'DhcpV4StaticBindingConfig',
-                        'mac_address' => $this->nic->mac_address,
-                        'ip_address' => $this->nic->ip_address
-                    ]
-                ]
-            );
-        } catch (\Exception $exception) {
-            if ($exception->hasResponse()) {
-                Log::info(get_class($this), json_decode($exception->getResponse()->getBody()->getContents(), true));
-            }
-            throw $exception;
-        }
+        //Delete dhcp lease for the ip to the nic's mac address on NSX
+        $nsxService->delete(
+            '/policy/api/v1/infra/tier-1s/' . $this->nic->network->router->id . '/segments/' . $this->nic->network->id
+            . '/dhcp-static-binding-configs/' . $this->nic->id
+        );
 
-        Log::info('DHCP static binding created for ' . $this->nic->id . ' (' . $this->nic->mac_address . ') with IP ' . $this->nic->ip_address);
+        Log::info('DHCP static binding deleted for ' . $this->nic->id . ' (' . $this->nic->mac_address . ') with IP ' . $this->nic->ip_address);
 
         Log::info(get_class($this) . ' : Finished', ['id' => $this->nic->id]);
     }
