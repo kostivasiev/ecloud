d2153354327ac7f4e31fc8ccb1b117a30eb1a1c0
Umair Majeed <umairbmajeed@gmail.com>
2021-03-25 07:54:01 +0000

    Return a 403 error when a 404 received, and stop proxying other errors, and just throw them back up.
    #659-500-thrown-when-creating-support-as-scoped-reseller-user

--- app/Http/Middleware/CanEnableSupport.php
+++ app/Http/Middleware/CanEnableSupport.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class CanEnableSupport
@@ -17,6 +18,7 @@ class CanEnableSupport
      * @param $request
      * @param Closure $next
      * @return mixed
+     * @throws \Exception
      */
     public function handle($request, Closure $next)
     {
@@ -25,6 +27,10 @@ class CanEnableSupport
             try {
                 $customer = $accountAdminClient->customers()->getById($request->user()->resellerId());
             } catch (\Exception $e) {
+                if ($e->getResponse()->getStatusCode() !== 404) {
+                    Log::info($e);
+                    throw($e);
+                }
                 return response()->json([
                     'errors' => [
                         [
