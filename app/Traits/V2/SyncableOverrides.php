440bfa5a5017a7e21d821feb578b39b10ffe0ae4
Umair Majeed <umairbmajeed@gmail.com>
2021-03-29 09:39:58 +0100

    remove unused libs

--- app/Traits/V2/SyncableOverrides.php
+++ app/Traits/V2/SyncableOverrides.php
@@ -3,15 +3,11 @@
 namespace App\Traits\V2;
 
 use App\Models\V2\FirewallPolicy;
-use App\Models\V2\FirewallRule;
-use App\Models\V2\FirewallRulePort;
 use App\Models\V2\Host;
 use App\Models\V2\HostGroup;
 use App\Models\V2\NetworkPolicy;
 use App\Models\V2\Sync;
-use App\Models\V2\Volume;
 use Illuminate\Support\Facades\Log;
-use Symfony\Component\HttpFoundation\Response;
 
 trait SyncableOverrides
 {
