924ab0ab199d03101536ded473cd5bc231f2263b
Umair Majeed <umairbmajeed@gmail.com>
2021-03-31 14:08:49 +0100

    Modifications and additions for Delete, Undeploy and Update FloatingIp jobs.
    #689-allocating-fip-returns-failed-sync-status-immediately-before-returning-complete

--- app/Jobs/Nsx/FloatingIp/UndeployCheck.php
+++ app/Jobs/Nsx/FloatingIp/UndeployCheck.php
@@ -4,10 +4,13 @@ namespace App\Jobs\Nsx\FloatingIp;
 
 use App\Jobs\Job;
 use App\Models\V2\FloatingIp;
+use Illuminate\Bus\Batchable;
 use Illuminate\Support\Facades\Log;
 
 class UndeployCheck extends Job
 {
+    use Batchable;
+
     const RETRY_DELAY = 5;
 
     public $tries = 500;
@@ -23,7 +26,8 @@ class UndeployCheck extends Job
     {
         Log::info(get_class($this) . ' : Started', ['id' => $this->model->id]);
 
-        $this->model->syncDelete();
+        $this->model->deleted = time();
+        $this->model->saveQuietly();
 
         Log::info(get_class($this) . ' : Finished', ['id' => $this->model->id]);
     }
--- app/Jobs/Sync/FloatingIp/Delete.php
+++ app/Jobs/Sync/FloatingIp/Delete.php
@@ -6,28 +6,30 @@ use App\Jobs\Job;
 use App\Jobs\Nsx\FloatingIp\UndeployCheck;
 use App\Jobs\Nsx\Nat\Undeploy as NatUndeploy;
 use App\Jobs\Nsx\Nat\UndeployCheck as NatUndeployCheck;
-use App\Models\V2\FloatingIp;
 use App\Models\V2\Nat;
+use App\Models\V2\Sync;
+use App\Traits\V2\SyncableBatch;
 use Illuminate\Support\Facades\Log;
 
 class Delete extends Job
 {
-    /** @var FloatingIp */
-    private $model;
+    use SyncableBatch;
 
-    public function __construct(FloatingIp $model)
+    private Sync $sync;
+
+    public function __construct(Sync $sync)
     {
-        $this->model = $model;
+        $this->sync = $sync;
     }
 
     public function handle()
     {
-        Log::info(get_class($this) . ' : Started', ['id' => $this->model->id]);
+        Log::info(get_class($this) . ' : Started', ['id' => $this->sync->resource->id]);
 
         $jobs = [];
-        $nats = Nat::where('source_id', $this->model->id)
-            ->orWhere('destination_id', $this->model->id)
-            ->orWhere('translated_id', $this->model->id)
+        $nats = Nat::where('source_id', $this->sync->resource->id)
+            ->orWhere('destination_id', $this->sync->resource->id)
+            ->orWhere('translated_id', $this->sync->resource->id)
             ->get()
             ->filter(function ($model) {
                 return $model instanceof Nat;
@@ -38,10 +40,16 @@ class Delete extends Job
         $nats->each(function ($nat) use (&$jobs) {
             $jobs[] = new NatUndeployCheck($nat);
         });
-        $jobs[] = new UndeployCheck($this->model);
+        $jobs[] = new UndeployCheck($this->sync->resource);
+
+        $this->deleteSyncBatch(
+            [
+                $jobs,
+            ]
+        )->dispatch();
 
         dispatch(array_shift($jobs)->chain($jobs));
 
-        Log::info(get_class($this) . ' : Finished', ['id' => $this->model->id]);
+        Log::info(get_class($this) . ' : Finished', ['id' => $this->sync->resource->id]);
     }
 }
