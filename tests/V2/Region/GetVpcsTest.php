a015d6f175902cabc5b01d9f884464fac6e7b21f
Umair Majeed <umairbmajeed@gmail.com>
2021-03-31 15:41:58 +0100

    fix test

--- tests/V2/Region/GetVpcsTest.php
+++ tests/V2/Region/GetVpcsTest.php
@@ -23,28 +23,13 @@ class GetVpcsTest extends TestCase
         parent::setUp();
         $this->faker = Faker::create();
 
-        $this->regions = factory(Region::class, 2)->create([
-            'name' => $this->faker->country(),
-        ])->each(function ($region) {
-            factory(AvailabilityZone::class, 2)->create([
-                'region_id' => $region->id,
-                'name'      => $this->faker->city(),
-                'is_public' => false,
-            ]);
-        });
-        Model::withoutEvents(function() {
-            $this->vpc = factory(Vpc::class)->create([
-                'id' => 'vpc-test',
-                'name'      => 'VPC '.uniqid(),
-                'region_id' => $this->regions[0]->id,
-            ]);
-        });
+        $this->vpc();
     }
 
     public function testGetVpcCollection()
     {
         $this->get(
-            '/v2/regions/'.$this->regions[0]->id.'/vpcs',
+            '/v2/regions/'.$this->region()->id.'/vpcs',
             [
                 'X-consumer-custom-id' => '0-0',
                 'X-consumer-groups'    => 'ecloud.read',
