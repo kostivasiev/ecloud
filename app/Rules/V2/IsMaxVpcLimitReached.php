a1e9f8fb4a447fd6e66d0e5e64e04557254dd22d
Umair Majeed <umairbmajeed@gmail.com>
2021-04-08 15:51:00 +0100

    Removed null value checks which aren't needed due to strict typing. Thanks @paul.mcnally
    #785 NSX | VPC - Limit number per customer to 10

--- app/Rules/V2/IsMaxVpcLimitReached.php
+++ app/Rules/V2/IsMaxVpcLimitReached.php
@@ -17,18 +17,11 @@ class IsMaxVpcLimitReached implements Rule
 
     public function passes($attribute, $value)
     {
-        if (is_null($this->vpcMaxLimit)) {
-            return false;
-        }
-        $vpc = Vpc::forUser(Auth::user())->get();
-        return ($vpc->count() < $this->vpcMaxLimit);
+        return (Vpc::forUser(Auth::user())->get()->count() < $this->vpcMaxLimit);
     }
 
     public function message()
     {
-        if (is_null($this->vpcMaxLimit)) {
-            return 'The maximum number of VPCs has not been configured';
-        }
         return 'The maximum number of ' . $this->vpcMaxLimit . ' VPCs has been reached';
     }
 }
