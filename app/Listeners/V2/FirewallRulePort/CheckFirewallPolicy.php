5a19a79d675d0a11d186a395f5881ad2a9b00d19
Umair Majeed <umairbmajeed@gmail.com>
2021-04-12 16:40:06 +0100

    Refactored to use Cache lock as mentioned in the CR.
    #697 Changes allowed to firewall rules and ports when firewall policy sync is `in-progress`

--- app/Listeners/V2/FirewallRulePort/CheckFirewallPolicy.php
+++ app/Listeners/V2/FirewallRulePort/CheckFirewallPolicy.php
@@ -2,6 +2,10 @@
 
 namespace App\Listeners\V2\FirewallRulePort;
 
+use App\Exceptions\SyncException;
+use App\Models\V2\Sync;
+use Illuminate\Contracts\Cache\LockTimeoutException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class CheckFirewallPolicy
@@ -10,8 +14,25 @@ class CheckFirewallPolicy
     {
         Log::info(get_class($this) . ' : Started', ['event' => $event]);
 
-        if ($event->model->firewallRule->firewallPolicy->getStatus() === 'in-progress') {
-            return false;
+        $model = $event->model;
+        $lock = Cache::lock("sync." . $model->id, 60);
+        try {
+            $lock->block(60);
+
+            if ($model->firewallRule->firewallPolicy->syncs()->count() == 1 && $model->firewallRule->firewallPolicy->sync->status === Sync::STATUS_FAILED) {
+                Log::warning(get_class($this) . ' : Update blocked, resource has a single failed sync', ['resource_id' => $model->id]);
+                return false;
+            }
+
+            if ($model->firewallRule->firewallPolicy->sync->status === Sync::STATUS_INPROGRESS) {
+                Log::warning(get_class($this) . ' : Update blocked, resource has outstanding sync', ['resource_id' => $model->id]);
+                return false;
+            }
+        } catch (LockTimeoutException $e) {
+            Log::error(get_class($this) . ' : Delete blocked, cannot obtain sync lock', ['resource_id' => $model->id]);
+            throw new SyncException("Cannot obtain sync lock");
+        } finally {
+            $lock->release();
         }
 
         Log::info(get_class($this) . ' : Finished', ['event' => $event]);
