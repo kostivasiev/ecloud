0e1f48562208c5a973562f735c6016beab9372c1
Umair Majeed <umairbmajeed@gmail.com>
2021-04-20 17:09:00 +0100

    Simplified delete cluster, and made it non-blocking
    #602 5 - NSX | Dedicated Hosts - Delete host group listener

--- app/Jobs/Kingpin/HostGroup/DeleteCluster.php
+++ app/Jobs/Kingpin/HostGroup/DeleteCluster.php
@@ -4,13 +4,13 @@ namespace App\Jobs\Kingpin\HostGroup;
 
 use App\Jobs\Job;
 use App\Models\V2\HostGroup;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Exception\ServerException;
+use Illuminate\Bus\Batchable;
 use Illuminate\Support\Facades\Log;
 
 class DeleteCluster extends Job
 {
+    use Batchable;
+
     public $model;
 
     public function __construct(HostGroup $model)
@@ -23,38 +23,10 @@ class DeleteCluster extends Job
         Log::info(get_class($this) . ' : Started', ['id' => $this->model->id]);
 
         $hostGroup = $this->model;
-        $message = null;
-
-        // Check if it already exists and if doesn't skip deleting it
-        $response = $hostGroup->availabilityZone->kingpinService()
-            ->get('/api/v2/vpc/' . $hostGroup->vpc->id . '/hostgroup/' . $hostGroup->id);
-        if (!$response || $response->getStatusCode() !== 200) {
-            $this->fail(new \Exception('Failed to get HostGroup'));
-            return false;
-        }
-
-        try {
-            $response = $hostGroup->availabilityZone->kingpinService()->delete(
-                '/api/v2/vpc/' . $hostGroup->vpc->id . '/hostgroup/' . $hostGroup->id
-            );
-        } catch (ServerException|ClientException $e) {
-            $response = $e->getResponse();
-            $message = $e->getMessage();
-        }
-        if (!$response || $response->getStatusCode() !== 200) {
-            $message = $message ?? 'Failed to delete Host Group ' . $hostGroup->id;
-            $this->fail(new \Exception($message));
-            return false;
-        }
+        $hostGroup->availabilityZone->kingpinService()->delete(
+            '/api/v2/vpc/' . $hostGroup->vpc->id . '/hostgroup/' . $hostGroup->id
+        );
 
         Log::info(get_class($this) . ' : Finished', ['id' => $this->model->id]);
-    }
-
-    public function failed($exception)
-    {
-        $message = ($exception instanceof RequestException && $exception->hasResponse()) ?
-            $exception->getResponse()->getBody()->getContents() :
-            $exception->getMessage();
-        $this->model->setSyncFailureReason($message);
     }
 }
