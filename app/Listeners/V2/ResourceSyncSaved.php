02c9b8b09020dc67387f4b52d89644a3e3d23b62
Umair Majeed <umairbmajeed@gmail.com>
2021-04-21 12:33:28 +0100

    remove dead code

--- app/Listeners/V2/ResourceSyncSaved.php
+++ app/Listeners/V2/ResourceSyncSaved.php
@@ -14,15 +14,6 @@ class ResourceSyncSaved
 
         $event->model->createSync(Sync::TYPE_UPDATE);
 
-/*        if (!$event->model->syncLock) {
-            $existingLock = Cache::pull("sync_saving_lock." . $event->model->id);
-            if ($existingLock) {
-                Log::debug(get_class($this) . ' : Releasing existing sync lock',
-                    ['resource_id' => $event->model->id, 'lock_owner' => $existingLock]);
-                Cache::restoreLock($event->model->syncGetLockKey(), $existingLock)->release();
-            }
-        }*/
-
         Log::info(get_class($this) . ' : Finished', ['resource_id' => $event->model->id]);
     }
 }
