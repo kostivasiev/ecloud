16463660f38aec7b39abbca254e4ebf7fc815c4c
Umair Majeed <umairbmajeed@gmail.com>
2021-04-27 08:59:00 +0100

    CanDetach middleware
    #851 Introduce volume type property

---
+++ app/Http/Middleware/CanDetach.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Middleware;
+
+use App\Exceptions\V2\DetachException;
+use App\Models\V2\Volume;
+use Closure;
+
+class CanDetach
+{
+    /**
+     * @param $request
+     * @param Closure $next
+     * @return mixed
+     * @throws DetachException
+     */
+    public function handle($request, Closure $next)
+    {
+        $volume = Volume::forUser($request->user())->findOrFail($request->route('volumeId'));
+
+        if ($volume->os_volume) {
+            throw new DetachException();
+        }
+
+        return $next($request);
+    }
+}
