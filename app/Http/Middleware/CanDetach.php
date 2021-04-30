80a6b7aed62a5dc0bf379610ada26a32ae6255a0
Umair Majeed <umairbmajeed@gmail.com>
2021-04-30 10:18:48 +0100

    Missed refactor of property name
    #851 Introduce volume type property

--- app/Http/Middleware/CanDetach.php
+++ app/Http/Middleware/CanDetach.php
@@ -17,7 +17,7 @@ class CanDetach
     {
         $volume = Volume::forUser($request->user())->findOrFail($request->route('volumeId'));
 
-        if ($volume->type) {
+        if ($volume->os_volume) {
             throw new DetachException();
         }
 
