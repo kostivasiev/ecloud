5d38290f37fa043a40099733ab83167078c1c330
Umair Majeed <umairbmajeed@gmail.com>
2021-05-04 10:18:43 +0100

    Fixed broken tests and a couple of jobs that hadn't been modified to use the new trait.
    #845 Job logging boilerplate

--- app/Jobs/Network/Deploy.php
+++ app/Jobs/Network/Deploy.php
@@ -14,33 +14,33 @@ class Deploy extends Job
 {
     use Batchable, JobModel;
 
-    private Network $network;
+    private Network $model;
 
     public function __construct(Network $network)
     {
-        $this->network = $network;
+        $this->model = $network;
     }
 
     public function handle()
     {
-        $dhcp = $this->network->router->vpc->dhcps()->where('availability_zone_id', $this->network->router->availability_zone_id)->first();
+        $dhcp = $this->model->router->vpc->dhcps()->where('availability_zone_id', $this->model->router->availability_zone_id)->first();
         if (empty($dhcp)) {
             $this->fail(new Exception('Unable to locate VPC DHCP server for router availability zone'));
             return;
         }
 
-        $subnet = Subnet::fromString($this->network->subnet);
+        $subnet = Subnet::fromString($this->model->subnet);
         //The first address is the network identification and the last one is the broadcast, they cannot be used as regular addresses.
         $networkAddress = $subnet->getStartAddress();
         $gatewayAddress = $networkAddress->getNextAddress();
         $dhcpServerAddress = $gatewayAddress->getNextAddress();
-        $message = 'Deploying Network: ' . $this->network->id . ': ';
+        $message = 'Deploying Network: ' . $this->model->id . ': ';
         Log::info($message . 'Gateway Address: ' . $gatewayAddress->toString() . '/' . $subnet->getNetworkPrefix());
         Log::info($message . 'DHCP Server Address: ' . $dhcpServerAddress->toString() . '/' . $subnet->getNetworkPrefix());
         Log::info($message . 'DHCP ID: ' . $dhcp->id);
 
-        $this->network->router->availabilityZone->nsxService()->patch(
-            'policy/api/v1/infra/tier-1s/' . $this->network->router->id . '/segments/' . $this->network->id,
+        $this->model->router->availabilityZone->nsxService()->patch(
+            'policy/api/v1/infra/tier-1s/' . $this->model->router->id . '/segments/' . $this->model->id,
             [
                 'json' => [
                     'resource_type' => 'Segment',
@@ -63,7 +63,7 @@ class Deploy extends Job
                     'tags' => [
                         [
                             'scope' => config('defaults.tag.scope'),
-                            'tag' => $this->network->router->vpc->id
+                            'tag' => $this->model->router->vpc->id
                         ]
                     ]
                 ]
--- app/Jobs/Network/DeployDiscoveryProfile.php
+++ app/Jobs/Network/DeployDiscoveryProfile.php
@@ -12,28 +12,28 @@ class DeployDiscoveryProfile extends Job
 {
     use Batchable, JobModel;
 
-    private Network $network;
+    private Network $model;
 
     public function __construct(Network $network)
     {
-        $this->network = $network;
+        $this->model = $network;
     }
 
     public function handle()
     {
-        $response = $this->network->router->availabilityZone->nsxService()->get(
-            'policy/api/v1/infra/tier-1s/' . $this->network->router->id . '/segments/' . $this->network->id . '/segment-discovery-profile-binding-maps',
+        $response = $this->model->router->availabilityZone->nsxService()->get(
+            'policy/api/v1/infra/tier-1s/' . $this->model->router->id . '/segments/' . $this->model->id . '/segment-discovery-profile-binding-maps',
         );
         $response = json_decode($response->getBody()->getContents(), true);
         if (!isset($response['results'][0])) {
             $response['results'][0] = [
-                'id' => $this->network->id . '-segment-discovery-profile-binding-maps',
+                'id' => $this->model->id . '-segment-discovery-profile-binding-maps',
             ];
         }
         $response['results'][0]['ip_discovery_profile_path'] = '/infra/ip-discovery-profiles/' . config('network.profiles.ip-discovery-profile');
         $response['results'][0]['mac_discovery_profile_path'] = '/infra/mac-discovery-profiles/' . config('network.profiles.mac-discovery-profile');
-        $this->network->router->availabilityZone->nsxService()->patch(
-            'policy/api/v1/infra/tier-1s/' . $this->network->router->id . '/segments/' . $this->network->id . '/segment-discovery-profile-binding-maps/' . $response['results'][0]['id'],
+        $this->model->router->availabilityZone->nsxService()->patch(
+            'policy/api/v1/infra/tier-1s/' . $this->model->router->id . '/segments/' . $this->model->id . '/segment-discovery-profile-binding-maps/' . $response['results'][0]['id'],
             ['json' => $response['results'][0]]
         );
     }
