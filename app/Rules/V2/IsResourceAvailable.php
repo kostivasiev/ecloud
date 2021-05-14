bc40c8b44e368fef1f40473af9fbffb5bf031255
Umair Majeed <umairbmajeed@gmail.com>
2021-05-14 14:55:44 +0100

    Added resource validation to POST endpoint and modified the test as appropriate to address the issue.
    #898 Validate HostGroup status when creating Host

---
+++ app/Rules/V2/IsResourceAvailable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules\V2;
+
+use App\Models\V2\Task;
+use Illuminate\Contracts\Validation\Rule;
+
+class IsResourceAvailable implements Rule
+{
+    protected $resource;
+
+    public function __construct($resource)
+    {
+        $this->resource = new $resource;
+    }
+
+    public function passes($attribute, $value)
+    {
+        return ($this->resource->findOrFail($value))->sync->status !== Task::STATUS_FAILED;
+    }
+
+    public function message()
+    {
+        return 'The specified :attribute resource is currently in a failed state and cannot be used';
+    }
+
+
+}
