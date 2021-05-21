f26f49d274cc97608cd59c86d8cd42d2692abc8f
Umair Majeed <umairbmajeed@gmail.com>
2021-05-21 10:14:57 +0100

    add rule tests

---
+++ tests/unit/Rules/V2/VolumeNotAttachedToInstanceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Rules\V2;
+
+
+use App\Models\V2\Volume;
+use App\Rules\V2\IsValidSshPublicKey;
+use App\Rules\V2\VolumeNotAttachedToInstance;
+use Tests\TestCase;
+use UKFast\Api\Auth\Consumer;
+
+class VolumeNotAttachedToInstanceTest extends TestCase
+{
+    public function testVolumeNotAttachedSucceeds()
+    {
+        $this->be(new Consumer(1, [config('app.name') . '.read', config('app.name') . '.write']));
+        $volume = factory(Volume::class)->create([
+            'id' => 'vol-test',
+            'availability_zone_id' => $this->availabilityZone()->id,
+            'vpc_id' => $this->vpc()->id
+        ]);
+
+        $rule = new VolumeNotAttachedToInstance($this->instance()->id);
+
+        $result = $rule->passes('volume_id', $volume->id);
+
+        $this->assertTrue($result);
+    }
+
+    public function testVolumeAttachedFails()
+    {
+        $this->be(new Consumer(1, [config('app.name') . '.read', config('app.name') . '.write']));
+        $volume = factory(Volume::class)->create([
+            'id' => 'vol-test',
+            'availability_zone_id' => $this->availabilityZone()->id,
+            'vpc_id' => $this->vpc()->id
+        ]);
+
+        $this->instance()->volumes()->attach($volume);
+
+        $rule = new VolumeNotAttachedToInstance($this->instance()->id);
+
+        $result = $rule->passes('volume_id', $volume->id);
+
+        $this->assertFalse($result);
+    }
+}
