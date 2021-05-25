c3e6f90790c9bb9231acb779583dff44cd9e0594
Umair Majeed <umairbmajeed@gmail.com>
2021-05-25 12:20:49 +0100

    Added DeleteImage job to handle comms with Kingpin
    #759 NSX | Images - delete Image job

---
+++ app/Jobs/Kingpin/Image/DeleteImage.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs\Kingpin\Image;
+
+use App\Jobs\Job;
+use App\Models\V2\Image;
+use App\Traits\V2\LoggableModelJob;
+use Illuminate\Bus\Batchable;
+use Illuminate\Support\Facades\Log;
+
+class DeleteImage extends Job
+{
+    use Batchable, LoggableModelJob;
+
+    public $model;
+
+    public function __construct(Image $model)
+    {
+        $this->model = $model;
+    }
+
+    public function handle()
+    {
+        $image = $this->model;
+        if ($image->availabilityZones()->count() <= 0) {
+            Log::warning(
+                get_class($this) . ' : No availability zones found for Image ' . $image->id . ', skipping'
+            );
+            return;
+        }
+        $availabilityZone = $image->availabilityZones()->first();
+        if ($image->instances()->count() <= 0) {
+            Log::warning(
+                get_class($this) . ' : No Instances found for Image ' . $image->id . ', skipping'
+            );
+            return;
+        }
+        $vpc = $image->instances()->first()->vpc;
+        try {
+            $availabilityZone->kingpinService()->delete(
+                '/api/v2/vpc/' . $vpc->id . '/template/' . $image->id
+            );
+        } catch (\Exception $exception) {
+            Log::info('Exception Code: ' . $exception->getCode());
+            if ($exception->getCode() !== 404) {
+                $this->fail($exception);
+                return;
+            }
+            Log::warning(
+                get_class($this) . ' : Failed to delete Image ' . $image->id . '. Image was not found, skipping'
+            );
+            return;
+        }
+    }
+}
