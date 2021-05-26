341b8a66765fbc9a6ca11e00e92686de9b337f1c
Umair Majeed <umairbmajeed@gmail.com>
2021-05-26 14:55:22 +0100

    Removed test, as is no longer required. Updating host_group_id via the /v2/instances/instanceId endpoint is no longer valid.
    #611 4.1 - NSX | Dedicated Hosts - Move Instance to another Host Group Listener

--- tests/V2/Instances/UpdateTest.php
+++ tests/V2/Instances/UpdateTest.php
@@ -19,37 +19,6 @@ class UpdateTest extends TestCase
         app()->instance(Task::class, $task);
     }
 
-    public function testFailedHostGroupCausesFail()
-    {
-        // Force failure
-        Model::withoutEvents(function () {
-            $model = new Task([
-                'id' => 'sync-test',
-                'failure_reason' => 'Unit Test Failure',
-                'completed' => true,
-                'name' => Sync::TASK_NAME_UPDATE,
-            ]);
-            $model->resource()->associate($this->hostGroup());
-            $model->save();
-        });
-
-        $this->patch(
-            '/v2/instances/' . $this->instance()->id,
-            [
-                'host_group_id' => $this->hostGroup()->id,
-            ],
-            [
-                'X-consumer-custom-id' => '0-0',
-                'X-consumer-groups' => 'ecloud.write',
-            ]
-        )->seeJson(
-            [
-                'title' => 'Validation Error',
-                'detail' => 'The specified host group id resource is currently in a failed state and cannot be used',
-            ]
-        )->assertResponseStatus(422);
-    }
-
     public function testValidDataIsSuccessful()
     {
         Event::fake();
