0eabdbb5f3a9590d83add948c46ad2552e4c9793
Umair Majeed <umairbmajeed@gmail.com>
2021-05-27 14:47:59 +0100

    add task scoping

--- app/Http/Controllers/V2/TaskController.php
+++ app/Http/Controllers/V2/TaskController.php
@@ -16,7 +16,7 @@ class TaskController extends BaseController
 {
     public function index(Request $request, QueryTransformer $queryTransformer)
     {
-        $collection = Task::query();
+        $collection = Task::forUser($request->user());
 
         $queryTransformer->config(Task::class)
             ->transform($collection);
@@ -28,14 +28,8 @@ class TaskController extends BaseController
 
     public function show(Request $request, string $taskId)
     {
-        $task = Task::findOrFail($taskId);
-        $taskResource = $task->resource()->withTrashed()->forUser($request->user())->first();
-        if (!$taskResource) {
-            throw (new ModelNotFoundException)->setModel(Task::class, $taskId);
-        }
-
         return new TaskResource(
-            $taskResource
+            Task::forUser($request->user())->findOrFail($taskId)
         );
     }
 }
--- routes/v2.php
+++ routes/v2.php
@@ -414,9 +414,7 @@ $router->group($baseRouteParameters, function () use ($router) {
 
     /** Task */
     $router->group([], function () use ($router) {
-        $router->group(['middleware' => 'is-admin'], function () use ($router) {
-            $router->get('tasks', 'TaskController@index');
-        });
+        $router->get('tasks', 'TaskController@index');
         $router->get('tasks/{taskId}', 'TaskController@show');
     });
 });
