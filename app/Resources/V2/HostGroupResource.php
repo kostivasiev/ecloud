c48d9e876c7ab82e17e76e01693d2c766cc5afd2
Umair Majeed <umairbmajeed@gmail.com>
2021-06-07 09:06:19 +0100

    Tweaked the resource to include the usage array and directly access the new model attributes.
    #944 Hostgroup available resource

--- app/Resources/V2/HostGroupResource.php
+++ app/Resources/V2/HostGroupResource.php
@@ -16,7 +16,19 @@ class HostGroupResource extends UKFastResource
             'availability_zone_id' => $this->availability_zone_id,
             'host_spec_id' => $this->host_spec_id,
             'windows_enabled' => $this->windows_enabled,
-            'total_used' => $this->getAvailableResources(),
+            'usage' => [
+                'hosts' => $this->hosts->count(),
+                'ram' => [
+                    'capacity' => $this->ram_capacity,
+                    'used' => $this->ram_used,
+                    'available' => $this->ram_available,
+                ],
+                'vcpu' => [
+                    'capacity' => $this->vcpu_capacity,
+                    'used' => $this->vcpu_used,
+                    'available' => $this->vcpu_available,
+                ]
+            ],
             'sync' => $this->sync,
             'created_at' => $this->created_at === null ? null : Carbon::parse(
                 $this->created_at,
