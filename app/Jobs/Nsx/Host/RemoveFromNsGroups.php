d65094ebe38bc870850be81ac429ebfa7af9a4fd
Umair Majeed <umairbmajeed@gmail.com>
2021-06-11 18:06:48 +0100

    remove transport node from ns-groups

---
+++ app/Jobs/Nsx/Host/RemoveFromNsGroups.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Jobs\Nsx\Host;
+
+use App\Jobs\Job;
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Host;
+use App\Traits\V2\LoggableModelJob;
+use Illuminate\Bus\Batchable;
+use Illuminate\Support\Facades\Log;
+
+class RemoveFromNsGroups extends Job
+{
+    use Batchable, LoggableModelJob;
+
+    private $model;
+
+    public function __construct(Host $model)
+    {
+        $this->model = $model;
+    }
+
+    public function handle()
+    {
+        $host = $this->model;
+        $availabilityZone = $host->hostGroup->availabilityZone;
+
+        // On deletion, we need to retrieve NSGroups that a transport node (HOST GROUP) is a member of, and remove it from those groups
+
+        // Get the host MAC address from Conjurer
+        $response = $availabilityZone->conjurerService()->get(
+            '/api/v2/compute/' . $availabilityZone->ucs_compute_name . '/vpc/' . $this->model->hostGroup->vpc->id .'/host/' . $this->model->id
+        );
+        $response = json_decode($response->getBody()->getContents());
+        if (!$response) {
+            $this->fail(new \Exception('Failed to get host compute profile'));
+            return;
+        }
+
+        $macAddress = collect($response->interfaces)->firstWhere('name', 'eth0')->address;
+
+        if (empty($macAddress)) {
+            $message = 'Failed to load eth0 address for host ' . $this->model->id;
+            Log::error($message);
+            $this->fail(new \Exception($message));
+            return;
+        }
+
+        // use that to load host from kingpin - the name is the IP of the node
+        $response = $availabilityZone->kingpinService()->get(
+            '/api/v2/vpc/' . $host->hostGroup->vpc_id . '/hostgroup/' . $host->hostGroup->id . '/host/' . $macAddress
+        );
+        $response = json_decode($response->getBody()->getContents());
+        if (!$response) {
+            $this->fail(new \Exception('Failed to retrieve host from Kingpin'));
+            return;
+        }
+
+        $hostIpAddress = $response->name;
+
+        //use the IP of the node to query NSX for the transport node so that we can get the ID GET https://185.197.63.88/api/v1/search/query?query=resource_type:TransportNode%20AND%20display_name:172.19.0.57
+        $response = $availabilityZone->nsxService()->get('/api/v1/search/query?query=resource_type:TransportNode%20AND%20display_name:' . $hostIpAddress);
+        $response = json_decode($response->getBody()->getContents());
+        if (!$response) {
+            $this->fail(new \Exception('Failed to query transport nodes'));
+            return;
+        }
+
+        if ($response->result_count != 1) {
+            $this->fail(new \Exception('Failed to load TransportNode for host ' . $host->id . 'with IP ' . $hostIpAddress));
+            return;
+        }
+
+       $transportNoteUuid = $response->results[0]->id;
+
+       //using the uuid get the ns groups that have the transport node as a member GET https://185.197.63.88/api/v1/search/query?query=resource_type:NSGroup%20AND%20members.value:ebe3adf5-c920-4442-b7fb-573e28d543c1
+       $response = $availabilityZone->nsxService()->get('/api/v1/search/query?query=resource_type:NSGroup%20AND%20members.value:' . $transportNoteUuid);
+       $response = json_decode($response->getBody()->getContents());
+       if (!$response) {
+           $this->fail(new \Exception('Failed to load NSGroups which contain TransportNode ' . $transportNoteUuid));
+           return;
+       }
+
+       if ($response->result_count < 1) {
+           $this->fail(new \Exception('No NSGroups found which contain TransportNode ' . $transportNoteUuid));
+           return;
+       }
+
+
+       //loop over the ns groups and PUT the endpoint minus the node profile
+       $nsGroups = $response->results;
+
+       foreach ($nsGroups as $nsGroup) {
+           $members = collect($nsGroup->members)->filter(function ($member) use ($transportNoteUuid) {
+               return $member->value != $transportNoteUuid;
+           })->toArray();
+
+           $nsGroup->members = $members;
+
+           $nsGroup->effective_member_count--;
+           $nsGroup->member_count--;
+
+           $availabilityZone->nsxService()->put('/api/v1/ns-groups/' . $nsGroup->id,
+               [
+                   'json' => $nsGroup
+               ]
+           );
+       }
+    }
+}
---
+++ tests/unit/Jobs/Nsx/Host/RemoveFromNsGroupsTest.php
@@ -0,0 +1,134 @@
+<?php
+namespace Tests\unit\Jobs\Nsx\Host;
+
+use App\Jobs\Nsx\Host\RemoveFromNsGroups;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class RemoveFromNsGroupsTest extends TestCase
+{
+    public function testRemoveFromNsGroups()
+    {
+        $this->conjurerServiceMock()
+            ->expects('get')
+            ->withSomeOfArgs(
+                '/api/v2/compute/' . $this->availabilityZone()->ucs_compute_name .
+                '/vpc/' . $this->vpc()->id . '/host/h-test'
+            )->andReturnUsing(function () {
+                return new Response('200', [], json_encode([
+                    'specification' => 'DUAL-4208--32GB',
+                    'name' => 'DUAL-4208--32GB',
+                    'interfaces' => [
+                        [
+                            'name' => 'eth0',
+                            'address' => '00:25:B5:C0:A0:1B',
+                            'type' => 'vNIC'
+                        ]
+                    ]
+                ]));
+            });
+
+        $this->kingpinServiceMock()->expects('get')
+            ->withArgs(['/api/v2/vpc/vpc-test/hostgroup/hg-test/host/00:25:B5:C0:A0:1B'])
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'name' => '172.19.0.38',
+                    'connectionState' => 'connected',
+                    'powerState' => 'poweredOn',
+                    'macAddress' => '00:25:B5:C0:A0:1B',
+                ]));
+            });
+
+        $this->nsxServiceMock()->expects('get')
+            ->with('/api/v1/search/query?query=resource_type:TransportNode%20AND%20display_name:172.19.0.38')
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'results' => [
+                        [
+                            'id' => 'ebe3adf5-c920-4442-b7fb-573e28d543c1'
+                        ],
+                    ],
+                    'result_count' => 1,
+                    'cursor' => 1
+                ]));
+            });
+
+        $this->nsxServiceMock()->expects('get')
+            ->with('/api/v1/search/query?query=resource_type:NSGroup%20AND%20members.value:ebe3adf5-c920-4442-b7fb-573e28d543c1')
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'results' => [
+                        [
+                            'id' => 'fc23f2fc-6a45-4dc7-90d2-64db975c1527',
+                            'members' => [
+                                [
+                                    "op" => "EQUALS",
+                                    "target_resource"=> [
+                                        "is_valid" => true
+                                    ],
+                                    "target_type"=> "TransportNode",
+                                    "resource_type"=> "NSGroupSimpleExpression",
+                                    "value"=> "d3db3ca8-6e24-45d6-be93-36591adbf9c4",
+                                    "target_property"=> "id"
+                                ],
+                                [
+                                    "op" => "EQUALS",
+                                    "target_resource"=> [
+                                            "is_valid" => true
+                                    ],
+                                    "target_type"=> "TransportNode",
+                                    "resource_type"=> "NSGroupSimpleExpression",
+                                    "value"=> "ebe3adf5-c920-4442-b7fb-573e28d543c1",
+                                    "target_property"=> "id"
+                                ]
+                            ],
+                            '_revision' => 9,
+                            'effective_member_count' =>  10,
+                            'member_count' =>  10
+                        ],
+                    ],
+                    "result_count" => 1,
+                    "cursor" => 1
+                ]));
+            });
+
+
+
+        $this->nsxServiceMock()->expects('put')
+            ->withArgs([
+                '/api/v1/ns-groups/fc23f2fc-6a45-4dc7-90d2-64db975c1527',
+                [
+                    'json' => [
+                        'id' => 'fc23f2fc-6a45-4dc7-90d2-64db975c1527',
+                        'members' => [
+                            [
+                                "op" => "EQUALS",
+                                "target_resource"=> [
+                                    "is_valid" => true
+                                ],
+                                "target_type"=> "TransportNode",
+                                "resource_type"=> "NSGroupSimpleExpression",
+                                "value"=> "d3db3ca8-6e24-45d6-be93-36591adbf9c4",
+                                "target_property"=> "id"
+                            ]
+                        ],
+                        '_revision' => 9,
+                        'effective_member_count' =>  10,
+                        'member_count' =>  10
+                    ]
+                ]
+            ])
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([]));
+            });
+
+
+        Event::fake([JobFailed::class]);
+
+        dispatch(new RemoveFromNsGroups($this->host()));
+
+        Event::assertNotDispatched(JobFailed::class);
+    }
+}
