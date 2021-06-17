d7fb92e6af47a455ee9e039f5ea7be1050a22b48
Umair Majeed <umairbmajeed@gmail.com>
2021-06-17 12:23:35 +0100

    As the same billing creation code was needed in the StartRam/StartVcpu/StartWindows billing jobs, I took the code from these three listeners that was doing that work, and used that in those jobs. Removed the code from these listeners which now just dispatch those jobs allowing code-reuse.
    #975 Update billing when moving instances between shared (public) / dedicated (private) host groups

--- app/Listeners/V2/Instance/UpdateVcpuBilling.php
+++ app/Listeners/V2/Instance/UpdateVcpuBilling.php
@@ -3,6 +3,7 @@
 namespace App\Listeners\V2\Instance;
 
 use App\Events\V2\Task\Updated;
+use App\Jobs\Instance\StartVcpuBilling;
 use App\Models\V2\BillingMetric;
 use App\Models\V2\Instance;
 use App\Support\Sync;
@@ -42,25 +43,6 @@ class UpdateVcpuBilling
             }
             $currentActiveMetric->setEndDate($time);
         }
-
-        $billingMetric = app()->make(BillingMetric::class);
-        $billingMetric->resource_id = $instance->id;
-        $billingMetric->vpc_id = $instance->vpc->id;
-        $billingMetric->reseller_id = $instance->vpc->reseller_id;
-        $billingMetric->key = 'vcpu.count';
-        $billingMetric->value = $instance->vcpu_cores;
-        $billingMetric->start = $time;
-
-        $product = $instance->availabilityZone->products()->get()->firstWhere('name', 'vcpu');
-        if (empty($product)) {
-            Log::error(
-                'Failed to load \'vcpu\' billing product for availability zone ' . $instance->availabilityZone->id
-            );
-        } else {
-            $billingMetric->category = $product->category;
-            $billingMetric->price = $product->getPrice($instance->vpc->reseller_id);
-        }
-
-        $billingMetric->save();
+        dispatch(new StartVcpuBilling($instance));
     }
 }
