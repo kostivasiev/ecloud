e90003cc3672a795095b3312f1dfab2be5f3d6ec
Umair Majeed <umairbmajeed@gmail.com>
2021-06-30 09:23:22 +0100

    Removed router_id related tests from Update as router_id is no longer an updateable value.
    #907 NSX | VPN - 'VPN service' endpoint

--- tests/V2/VpnService/UpdateTest.php
+++ tests/V2/VpnService/UpdateTest.php
@@ -11,74 +11,16 @@ use Tests\TestCase;
 
 class UpdateTest extends TestCase
 {
-    protected $vpn;
+    protected $vpnService;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->vpn = factory(VpnService::class)->create([
+        $this->vpnService = factory(VpnService::class)->create([
             'name' => 'Unit Test VPN',
             'router_id' => $this->router()->id,
         ]);
-    }
-
-    public function testNotOwnedRouterResourceIsFailed()
-    {
-        $this->vpc()->reseller_id = 3;
-        $this->vpc()->saveQuietly();
-
-        $this->patch(
-            '/v2/vpn-services/' . $this->vpn->id,
-            [
-                'name' => 'Unit Test VPN (Updated)',
-                'router_id' => $this->router()->id,
-            ],
-            [
-                'X-consumer-custom-id' => '1-0',
-                'X-consumer-groups' => 'ecloud.write',
-            ]
-        )
-            ->seeJson([
-                'title' => 'Validation Error',
-                'detail' => 'The specified router id was not found',
-                'status' => 422,
-                'source' => 'router_id'
-            ])
-            ->assertResponseStatus(422);
-    }
-
-    public function testFailedRouterCausesFailure()
-    {
-        // Force failure
-        Model::withoutEvents(function () {
-            $model = new Task([
-                'id' => 'sync-test',
-                'failure_reason' => 'Unit Test Failure',
-                'completed' => true,
-                'name' => Sync::TASK_NAME_UPDATE,
-            ]);
-            $model->resource()->associate($this->router());
-            $model->save();
-        });
-
-        $data = [
-            'name' => 'Unit Test VPN (Updated)',
-            'router_id' => $this->router()->id,
-        ];
-        $this->patch(
-            '/v2/vpn-services/' . $this->vpn->id,
-            $data,
-            [
-                'X-consumer-custom-id' => '0-0',
-                'X-consumer-groups' => 'ecloud.write',
-            ]
-        )->seeJson(
-            [
-                'title' => 'Validation Error',
-                'detail' => 'The specified router id resource is currently in a failed state and cannot be used',
-            ]
-        )->assertResponseStatus(422);
     }
 
     public function testValidDataIsSuccessful()
@@ -88,7 +30,7 @@ class UpdateTest extends TestCase
             'router_id' => $this->router()->id,
         ];
         $this->patch(
-            '/v2/vpn-services/' . $this->vpn->id,
+            '/v2/vpn-services/' . $this->vpnService->id,
             $data,
             [
                 'X-consumer-custom-id' => '0-0',
@@ -96,7 +38,7 @@ class UpdateTest extends TestCase
             ]
         )->assertResponseStatus(202);
 
-        $vpnItem = VpnService::findOrFail($this->vpn->id);
+        $vpnItem = VpnService::findOrFail($this->vpnService->id);
         $this->assertEquals($data['router_id'], $vpnItem->router_id);
         $this->assertEquals($data['name'], $vpnItem->name);
     }
