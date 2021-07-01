5982826275f2416f35e0c931aea6ec94e60b87f4
Umair Majeed <umairbmajeed@gmail.com>
2021-07-01 14:11:55 +0100

    Fixed original migration and added new migration for column rename.
    #908 NSX | VPN - 'local endpoint' endpoint

---
+++ database/migrations/2021_07_01_140953_rename_fip_id_column_in_vpn_endpoints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameFipIdColumnInVpnEndpointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('ecloud')->table('vpn_endpoints', function (Blueprint $table) {
+            $table->renameColumn('fip_id', 'floating_ip_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('ecloud')->table('vpn_endpoints', function (Blueprint $table) {
+            $table->renameColumn('floating_ip_id', 'fip_id');
+        });
+    }
+}
