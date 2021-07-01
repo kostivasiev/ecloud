2e626eb3bcb8d35ea0ab964217fc47e500330bb6
Umair Majeed <umairbmajeed@gmail.com>
2021-07-01 12:47:37 +0100

    Added ProcessBillingTest and BillingMetricTrait to help simplify additional metric testing.
    #1012 ProcessBilling failure on deleted VPC

---
+++ tests/V2/Console/Commands/Vpc/ProcessBillingTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Tests\V2\Console\Commands\Vpc;
+
+use Tests\TestCase;
+use Tests\V2\Console\BillingMetricTrait;
+
+class ProcessBillingTest extends TestCase
+{
+    use BillingMetricTrait;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->billingMetricSetup();
+    }
+
+    public function testVcpuCountBilling()
+    {
+        $code = 'vcpu.count';
+        $price = 0.01;
+        $quantity = 1;
+        $totalCost = $this->createBillingMetricAndCost($code, $price, $quantity);
+
+        $this->command->handle();
+
+        $metricPrice = $this->getLineArgumentPrice($code);
+
+        $this->assertNotNull($metricPrice);
+        $this->assertEquals($totalCost, $metricPrice);
+    }
+}
