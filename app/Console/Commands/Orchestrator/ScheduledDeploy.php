cf0ad25f6f2dd51af000c4dc342d39a7c73950ee
Umair Majeed <umairbmajeed@gmail.com>
2021-07-02 14:02:55 +0100

    Added ScheduledDeploy command, test for command and changes to Kernel to schedule the command to run every minute in production environment.
    #990 auto deploy saved configurations on requested date

---
+++ app/Console/Commands/Orchestrator/ScheduledDeploy.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Console\Commands\Orchestrator;
+
+use App\Models\V2\OrchestratorBuild;
+use App\Models\V2\OrchestratorConfig;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class ScheduledDeploy extends Command
+{
+    protected $signature = 'orchestrator:deploy {--D|debug} {--T|test-run}';
+    protected $description = 'Scheduled Orchestration Deployments';
+
+    public \DateTimeZone $timeZone;
+    public Carbon $startDate;
+    public Carbon $endDate;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->timeZone = new \DateTimeZone(config('app.timezone'));
+        $this->endDate = Carbon::now($this->timeZone);
+        $this->startDate = $this->endDate->subSeconds(59);
+    }
+
+    public function handle()
+    {
+        $this->info('Processing Orchestrations Start');
+        OrchestratorConfig::doesntHave('orchestratorBuilds')
+            ->whereBetween('deploy_on', [$this->startDate, $this->endDate])
+            ->each(function ($orchestratorConfig) {
+                $this->line('Deploying Config ' . $orchestratorConfig->id);
+                if (!$this->option('test-run')) {
+                    $orchestratorBuild = app()->make(OrchestratorBuild::class);
+                    $orchestratorBuild->orchestratorConfig()->associate($orchestratorConfig);
+                    $orchestratorBuild->syncSave();
+                }
+            });
+        $this->info('Processing Orchestrations End');
+
+        return Command::SUCCESS;
+    }
+
+}
---
+++ tests/V2/Console/Commands/Orchestrator/ScheduledDeployTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace Tests\V2\Console\Commands\Orchestrator;
+
+use App\Console\Commands\Orchestrator\ScheduledDeploy;
+use App\Models\V2\OrchestratorBuild;
+use App\Models\V2\OrchestratorConfig;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class ScheduledDeployTest extends TestCase
+{
+
+    protected $command;
+    protected string $startDate;
+    protected string $endDate;
+    protected OrchestratorConfig $orchestratorConfig;
+
+    protected $infoArgument;
+    protected array $lineArgument;
+    protected $lineArgumentItem;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->startDate = '2021-07-01 13:39:00';
+        $this->endDate = '2021-07-02 13:40:00';
+        $this->lineArgument = [];
+
+        $this->command = \Mockery::mock(ScheduledDeploy::class)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+        $this->command->startDate = Carbon::createFromTimeString($this->startDate);
+        $this->command->endDate = Carbon::createFromTimeString($this->endDate);
+
+        $this->command->shouldReceive('info')
+            ->with(\Mockery::capture($this->infoArgument))->andReturnUsing(function () {
+                return true;
+            });
+        $this->command->shouldReceive('line')
+            ->with(\Mockery::capture($this->lineArgumentItem))->andReturnUsing(function () {
+                $this->lineArgument[] = $this->lineArgumentItem;
+                return true;
+            });
+        $this->command->shouldReceive('option')->with('test-run')->andReturnTrue();
+
+        app()->bind(OrchestratorBuild::class, function () {
+            $orchestratorBuild = \Mockery::mock(OrchestratorBuild::class)->makePartial();
+            $orchestratorBuild->shouldReceive('orchestratorConfig->associate')
+                ->withAnyArgs()
+                ->andReturnTrue();
+            $orchestratorBuild->shouldReceive('syncSave')->andReturnTrue();
+            return $orchestratorBuild;
+        });
+    }
+
+    public function assertConfigProcessed($configId)
+    {
+        $configFound = false;
+        foreach ($this->lineArgument as $lineArgument) {
+            if (strpos($lineArgument, $configId) !== -1) {
+                $configFound = true;
+                break;
+            }
+        }
+        $this->assertTrue($configFound);
+    }
+
+    public function testWithConfigs()
+    {
+        $orchestratorConfigs = factory(OrchestratorConfig::class, 3)->create([
+            'deploy_on' => $this->startDate,
+        ]);
+        $this->command->handle();
+        foreach ($orchestratorConfigs as $orchestratorConfig) {
+            $this->assertConfigProcessed($orchestratorConfig->id);
+        }
+    }
+
+    public function testWithConfigNotDue()
+    {
+        factory(OrchestratorConfig::class)->create([
+            'deploy_on' => Carbon::createFromTimeString('2031-07-02 13:40:00')
+        ]);
+        $this->command->handle();
+        $this->assertEmpty($this->lineArgument);
+    }
+
+    public function testWithNoConfig()
+    {
+        $this->command->handle();
+        $this->assertEmpty($this->lineArgument);
+    }
+}
