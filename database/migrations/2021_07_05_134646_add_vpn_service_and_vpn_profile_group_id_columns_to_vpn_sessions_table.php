650c746052f68416abc012ef4ca073f56f2ac906
Umair Majeed <umairbmajeed@gmail.com>
2021-07-05 14:10:18 +0100

    Refactored migration so the naming is correct for migration action.
    #910 NSX | VPN - 'Session' endpoint (IPSEC session)

--- database/migrations/2021_07_05_134646_add_vpn_service_and_vpn_profile_group_id_columns_to_vpn_sessions_table.php
+++ database/migrations/2021_07_05_134646_add_vpn_service_and_vpn_profile_group_id_columns_to_vpn_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddVpnServiceColumnToVpnSessionsTable extends Migration
+class AddVpnServiceAndVpnProfileGroupIdColumnsToVpnSessionsTable extends Migration
 {
     /**
      * Run the migrations.
