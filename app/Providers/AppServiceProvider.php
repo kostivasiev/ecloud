01bf93da8a400ae1a374c5900e59330c4e86896a
Umair Majeed <umairbmajeed@gmail.com>
2021-07-06 09:38:38 +0100

    Updated morphMap
    #919 NSX | VPN - 'profile' endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ use App\Models\V2\OrchestratorBuild;
 use App\Models\V2\Router;
 use App\Models\V2\Volume;
 use App\Models\V2\Vpc;
+use App\Models\V2\VpnProfile;
 use App\Models\V2\VpnService;
 use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Relations\Relation;
@@ -59,7 +60,8 @@ class AppServiceProvider extends ServiceProvider
             'vpc' => Vpc::class,
             'dhcp' => Dhcp::class,
             'net' => Network::class,
-            'obuild' => OrchestratorBuild::class
+            'obuild' => OrchestratorBuild::class,
+            'vpnp' => VpnProfile::class,
         ]);
 
         Queue::exceptionOccurred(function (JobExceptionOccurred $event) {
