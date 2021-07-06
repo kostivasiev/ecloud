83f1525ba9fbf1919bc8e69d446015cb1bd7d550
Umair Majeed <umairbmajeed@gmail.com>
2021-07-06 08:51:20 +0100

    Fixed test and coding standards cleanup
    #916 NSX | VPN - Delete  'VPN service' job

--- tests/V2/VpnService/DeleteTest.php
+++ tests/V2/VpnService/DeleteTest.php
@@ -3,14 +3,8 @@
 namespace Tests\V2\VpnService;
 
 use App\Events\V2\Task\Created;
-use App\Models\V2\AvailabilityZone;
-use App\Models\V2\Region;
-use App\Models\V2\Router;
-use App\Models\V2\Vpc;
 use App\Models\V2\VpnService;
-use Faker\Factory as Faker;
 use Illuminate\Support\Facades\Event;
-use Laravel\Lumen\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class DeleteTest extends TestCase
@@ -36,9 +30,6 @@ class DeleteTest extends TestCase
                 'X-consumer-custom-id' => '0-0',
                 'X-consumer-groups' => 'ecloud.write',
             ]
-        )
-            ->assertResponseStatus(202);
-        $vpnItem = VpnService::withTrashed()->findOrFail($this->vpn->id);
-        $this->assertNotNull($vpnItem->deleted_at);
+        )->assertResponseStatus(202);
     }
 }
