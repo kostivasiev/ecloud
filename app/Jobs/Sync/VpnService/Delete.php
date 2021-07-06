43059872f38fb9a5228a3bf92f08ba06ebbfac58
Umair Majeed <umairbmajeed@gmail.com>
2021-07-06 08:12:54 +0100

    WIP
    #916 NSX | VPN - Delete  'VPN service' job

---
+++ app/Jobs/Sync/VpnService/Delete.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs\Sync\VpnService;
+
+use App\Jobs\Job;
+use App\Jobs\Nsx\VpnService\Undeploy;
+use App\Models\V2\Task;
+use App\Traits\V2\LoggableTaskJob;
+use App\Traits\V2\TaskableBatch;
+
+class Delete extends Job
+{
+    use TaskableBatch, LoggableTaskJob;
+
+    private $task;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    public function handle()
+    {
+        $this->deleteTaskBatch([
+            [
+                new Undeploy($this->task->resource),
+            ]
+        ])->dispatch();
+    }
+}
