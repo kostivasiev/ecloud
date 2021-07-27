f0fe194c0483882bec910435a3222299fed7af1f
Umair Majeed <umairbmajeed@gmail.com>
2021-07-27 14:27:03 +0100

    fix tests

--- tests/V2/Console/Commands/FloatingIp/PopulateAvailabilityZoneIdTest.php
+++ tests/V2/Console/Commands/FloatingIp/PopulateAvailabilityZoneIdTest.php
@@ -1,11 +1,15 @@
 <?php
 namespace Tests\V2\Console\Commands\FloatingIp;
 
+use App\Console\Commands\FloatingIp\PopulateAvailabilityZoneId;
 use Illuminate\Console\Command;
 use Tests\TestCase;
 
 class PopulateAvailabilityZoneIdTest extends TestCase
 {
+    protected array $infoArgument;
+    protected $infoArgumentItem;
+
     public function testSuccess()
     {
         $this->floatingIp()->availability_zone_id = null;
@@ -19,5 +23,37 @@ class PopulateAvailabilityZoneIdTest extends TestCase
 
         $this->floatingIp()->refresh();
         $this->assertEquals($this->availabilityZone()->id, $this->floatingIp()->availability_zone_id);
+    }
+
+    public function testFailedToLoadAvailabilityZone()
+    {
+        $this->command = \Mockery::mock(PopulateAvailabilityZoneId::class)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $this->command->shouldReceive('line')
+            ->andReturnUsing(function () {
+                return true;
+            });
+        $this->command->shouldReceive('error')
+            ->andReturnUsing(function () {
+                return true;
+            });
+
+        $this->command->shouldReceive('info')
+            ->with(\Mockery::capture($this->infoArgumentItem))->andReturnUsing(function () {
+                $this->infoArgument[] = $this->infoArgumentItem;
+                return true;
+            });
+
+        $this->floatingIp()->availability_zone_id = null;
+        $this->floatingIp()->save();
+        $this->vpc()->delete();
+
+        $this->assertNull($this->floatingIp()->availability_zone_id);
+
+        $this->command->handle();
+
+        $this->assertEquals('1 errors found, id\'s: fip-test', $this->infoArgument[0]);
     }
 }
