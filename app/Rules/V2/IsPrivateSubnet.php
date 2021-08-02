831fba588f6f691a74f56a978287c5c83078ee61
Umair Majeed <umairbmajeed@gmail.com>
2021-08-02 17:29:45 +0100

    Added restricted subnet 192.168.0.0/16 to non-admin users
    #1077 restrict access to 192.168.0.0/16

--- app/Rules/V2/IsPrivateSubnet.php
+++ app/Rules/V2/IsPrivateSubnet.php
@@ -3,6 +3,7 @@
 namespace App\Rules\V2;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 use IPLib\Range\Subnet;
 use IPLib\Range\Type;
 
@@ -14,8 +15,11 @@ class IsPrivateSubnet implements Rule
 {
     public function passes($attribute, $value)
     {
+        if (!Auth::user()->isAdmin() && $value == '192.168.0.0/16') {
+            return false;
+        }
         $subnet = Subnet::fromString($value);
-        return ($subnet) ? $subnet->getRangeType() === Type::T_PRIVATENETWORK : false;
+        return ($subnet) && $subnet->getRangeType() === Type::T_PRIVATENETWORK;
     }
 
     /**
@@ -23,6 +27,6 @@ class IsPrivateSubnet implements Rule
      */
     public function message()
     {
-        return 'The :attribute must be a private CIDR range';
+        return 'The :attribute must be a valid private CIDR range';
     }
 }
