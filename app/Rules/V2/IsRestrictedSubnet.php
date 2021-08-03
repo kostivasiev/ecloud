eaa2b933f2d69eba57edfc0b036856a039c24c04
Umair Majeed <umairbmajeed@gmail.com>
2021-08-03 08:33:18 +0100

    Created custom rule for restricted subnet
    #1077 restrict access to 192.168.0.0/16

---
+++ app/Rules/V2/IsRestrictedSubnet.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Rules\V2;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+
+class IsRestrictedSubnet implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        if (Auth::user()->isAdmin()) {
+            return true;
+        }
+        $restricted = \IPLib\Factory::rangeFromString('192.168.0.0/16');
+        $chosen = \IPLib\Factory::rangeFromString($value);
+
+        return !$restricted->containsRange($chosen);
+    }
+
+    public function message()
+    {
+        return 'The :attribute must be a valid private CIDR range';
+    }
+
+}
