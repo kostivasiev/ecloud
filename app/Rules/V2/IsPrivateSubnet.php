296055a3c346b2c6d7e5005ed472f045af10416c
Umair Majeed <umairbmajeed@gmail.com>
2021-08-03 08:28:24 +0100

    Reverted changes to rule
    #1077 restrict access to 192.168.0.0/16

--- app/Rules/V2/IsPrivateSubnet.php
+++ app/Rules/V2/IsPrivateSubnet.php
@@ -3,7 +3,6 @@
 namespace App\Rules\V2;
 
 use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Support\Facades\Auth;
 use IPLib\Range\Subnet;
 use IPLib\Range\Type;
 
@@ -15,11 +14,8 @@ class IsPrivateSubnet implements Rule
 {
     public function passes($attribute, $value)
     {
-        if (!Auth::user()->isAdmin() && $value == '192.168.0.0/16') {
-            return false;
-        }
         $subnet = Subnet::fromString($value);
-        return ($subnet) && $subnet->getRangeType() === Type::T_PRIVATENETWORK;
+        return ($subnet) ? $subnet->getRangeType() === Type::T_PRIVATENETWORK : false;
     }
 
     /**
@@ -27,6 +23,6 @@ class IsPrivateSubnet implements Rule
      */
     public function message()
     {
-        return 'The :attribute must be a valid private CIDR range';
+        return 'The :attribute must be a private CIDR range';
     }
 }
