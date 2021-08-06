52509656dc607d6dcff4aa245b998925087e02d4
Umair Majeed <umairbmajeed@gmail.com>
2021-08-06 08:32:57 +0100

    Added feature test to cover the "If there's a price then bill it" methodology.
    #1017 make billing metrics dynamic

--- tests/V2/Console/Commands/Vpc/ProcessBillingTest.php
+++ tests/V2/Console/Commands/Vpc/ProcessBillingTest.php
@@ -28,4 +28,19 @@ class ProcessBillingTest extends TestCase
         $this->assertNotNull($metricPrice);
         $this->assertEquals($totalCost, $metricPrice);
     }
+
+    public function testIfThereIsAPriceBillIt()
+    {
+        $code = 'mynew.metric';
+        $price = 0.01;
+        $quantity = 1;
+        $totalCost = $this->createBillingMetricAndCost($code, $price, $quantity);
+
+        $this->command->handle();
+
+        $metricPrice = $this->getLineArgumentPrice($code);
+
+        $this->assertNotNull($metricPrice);
+        $this->assertEquals($totalCost, $metricPrice);
+    }
 }
