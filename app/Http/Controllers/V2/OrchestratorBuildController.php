ee52c882f6e5510e2e60e8566fd0d860448bb10f
Umair Majeed <umairbmajeed@gmail.com>
2021-08-17 11:47:23 +0100

    adds filtering for orchestrator_config_id

--- app/Http/Controllers/V2/OrchestratorBuildController.php
+++ app/Http/Controllers/V2/OrchestratorBuildController.php
@@ -16,40 +16,7 @@ class OrchestratorBuildController extends BaseController
 {
     public function index(Request $request, QueryTransformer $queryTransformer)
     {
-        if ($request->hasAny([
-            'orchestrator_config_id',
-            'orchestrator_config_id:eq',
-            'orchestrator_config_id:neq',
-        ])) {
-            $buildIds = OrchestratorBuild::forUser(Auth::user())->get();
-
-            if ($request->has('orchestrator_config_id') || $request->has('orchestrator_config_id:eq')) {
-                if ($request->has('orchestrator_config_id')) {
-                    $orchestratorConfigId = $request->get('orchestrator_config_id');
-                    $request->query->remove('orchestrator_config_id');
-                } else {
-                    $orchestratorConfigId = $request->get('orchestrator_config_id:eq');
-                    $request->query->remove('orchestrator_config_id:eq');
-                }
-                $buildIds = $buildIds->reject(function ($orchestratorBuild) use ($orchestratorConfigId) {
-                    return $orchestratorBuild->orchestrator_config_id != $orchestratorConfigId;
-                });
-            }
-
-            if ($request->has('orchestrator_config_id:neq')) {
-                $orchestratorConfigId = $request->get('orchestrator_config_id:neq');
-                $request->query->remove('orchestrator_config_id:neq');
-                $buildIds = $buildIds->reject(function ($orchestratorBuild) use ($orchestratorConfigId) {
-                    return $orchestratorBuild->orchestrator_config_id == $orchestratorConfigId;
-                });
-            }
-
-            $collection = OrchestratorBuild::whereIn('id', $buildIds->map(function ($orchestratorBuild) {
-                return $orchestratorBuild->id;
-            }));
-        } else {
-            $collection = OrchestratorBuild::forUser(Auth::user());
-        }
+        $collection = OrchestratorBuild::forUser(Auth::user());
 
         $queryTransformer->config(OrchestratorBuild::class)
             ->transform($collection);
