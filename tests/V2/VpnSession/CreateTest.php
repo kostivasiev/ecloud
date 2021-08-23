344f59848a61d8580d337f5465c79d3f061d719d
Umair Majeed <umairbmajeed@gmail.com>
2021-08-23 15:13:47 +0100

    Fix tests

--- tests/V2/VpnSession/CreateTest.php
+++ tests/V2/VpnSession/CreateTest.php
@@ -1,11 +1,12 @@
 <?php
 namespace Tests\V2\VpnSession;
 
-use App\Models\V2\FloatingIp;
+use App\Events\V2\Task\Created;
 use App\Models\V2\VpnEndpoint;
 use App\Models\V2\VpnProfileGroup;
 use App\Models\V2\VpnService;
 use App\Models\V2\VpnSession;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 use UKFast\Api\Auth\Consumer;
 
@@ -50,7 +51,7 @@ class CreateTest extends TestCase
 
     public function testCreateResource()
     {
-        $counter = 0;
+        Event::fake([Created::class]);
         $vpnService = factory(VpnService::class)->create([
             'name' => 'test-service',
             'router_id' => $this->router()->id,
