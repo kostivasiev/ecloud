53c5a5f97575233d3b1caa5e585356a5f26eec0c
Umair Majeed <umairbmajeed@gmail.com>
2021-09-09 15:53:02 +0100

    use fip instead of NAT

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Orchestrator\ScheduledDeploy::class,
         \App\Console\Commands\FloatingIp\SetPolymorphicRelationship::class,
         \App\Console\Commands\FloatingIp\PopulateAvailabilityZoneId::class,
-        \App\Console\Commands\Nat\PopulateFipNatSequence::class,
+        \App\Console\Commands\Fip\PopulateFipNatSequence::class,
     ];
 
     /**
