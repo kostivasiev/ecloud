45d709893d0ced75450a087249a0da57ca3b373f
Umair Majeed <umairbmajeed@gmail.com>
2021-09-16 15:22:46 +0100

    decrease backoff

--- app/Traits/V2/Jobs/AwaitResources.php
+++ app/Traits/V2/Jobs/AwaitResources.php
@@ -10,7 +10,7 @@ trait AwaitResources
 {
     public $tries = 60;
 
-    public $backoff = 30;
+    public $backoff = 5;
 
     protected function awaitSyncableResources(Array $resources = [])
     {
