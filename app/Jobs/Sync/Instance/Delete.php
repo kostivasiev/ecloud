36d1fa57603535ff39b311de27dc1310bc7b7fea
Umair Majeed <umairbmajeed@gmail.com>
2021-09-27 09:40:29 +0100

    Commented out jobs in order to unblock
    #1067 eCloud VPC | Volume Groups - Attach a volume group to an instance

--- app/Jobs/Sync/Instance/Delete.php
+++ app/Jobs/Sync/Instance/Delete.php
@@ -33,7 +33,8 @@ class Delete extends Job
             [
                 new PowerOff($this->task->resource, true),
                 new Undeploy($this->task->resource),
-                new VolumeGroupDetach($this->task),
+// Commented out in order to unblock https://gitlab.devops.ukfast.co.uk/ukfast/api.ukfast/ecloud/-/issues/1067
+//                new VolumeGroupDetach($this->task),
                 new DeleteVolumes($this->task->resource),
                 new UnassignFloatingIP($this->task->resource),
                 new DeleteNics($this->task->resource),
--- app/Jobs/Sync/Instance/Update.php
+++ app/Jobs/Sync/Instance/Update.php
@@ -72,8 +72,9 @@ class Update extends Job
             $this->updateTaskBatch([
                 [
                     new ComputeUpdate($this->task->resource),
-                    new VolumeGroupAttach($this->task),
-                    new VolumeGroupDetach($this->task),
+// Commented out in order to unblock https://gitlab.devops.ukfast.co.uk/ukfast/api.ukfast/ecloud/-/issues/1067
+//                    new VolumeGroupAttach($this->task),
+//                    new VolumeGroupDetach($this->task),
                 ]
             ])->dispatch();
         }
