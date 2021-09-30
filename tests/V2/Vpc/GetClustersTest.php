5791acc0a2ee60c7c49550fcd9e7d7ad2249aa87
Umair Majeed <umairbmajeed@gmail.com>
2021-09-30 13:38:40 +0100

    updated related tests to have lbs_id

--- tests/V2/Vpc/GetClustersTest.php
+++ tests/V2/Vpc/GetClustersTest.php
@@ -43,7 +43,7 @@ class GetClustersTest extends TestCase
                 'id'     => $this->lbc->id,
                 'name'   => $this->lbc->name,
                 'vpc_id' => $this->lbc->vpc_id,
-                'nodes'  => $this->lbc->nodes,
+                'lbs_id' => $this->lbc->lbs_id,
             ])
             ->assertResponseStatus(200);
     }
