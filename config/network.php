7f9cffabbe6ed7bd7df321686a4037451977c5e3
Umair Majeed <umairbmajeed@gmail.com>
2021-09-30 13:25:33 +0100

    Added standard and advanced submets to config
    #1078 eCloud VPC | LBv2 - management networking

--- config/network.php
+++ config/network.php
@@ -6,5 +6,9 @@ return [
         'spoofguard-profile' => 'ecloud-spoofguard-profile',
         'ip-discovery-profile' => 'ecloud-ip-discovery-profile',
         'mac-discovery-profile' => 'ecloud-mac-discovery-profile',
+    ],
+    'subnet' => [
+        'standard' => '192.168.0.0/17',
+        'advanced' => '192.168.128.0/17',
     ]
 ];
