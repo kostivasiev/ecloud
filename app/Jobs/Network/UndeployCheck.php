087d56520b3d52823e9e40048b24ec813526f31b
Umair Majeed <umairbmajeed@gmail.com>
2021-09-30 13:23:34 +0100

    Tweaked job as it wasn't idempotent. When deploying resources that didn't complete correctly, cleanup meant that the resource might not exist on NSX to delete. This job prior to tweaking wouldn't allow the skipping of this stage if the resource didn't exist on NSX so fixed that.
    #1078 eCloud VPC | LBv2 - management networking

--- app/Jobs/Network/UndeployCheck.php
+++ app/Jobs/Network/UndeployCheck.php
@@ -5,6 +5,7 @@ namespace App\Jobs\Network;
 use App\Jobs\Job;
 use App\Models\V2\Network;
 use App\Traits\V2\LoggableModelJob;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Bus\Batchable;
 use Illuminate\Support\Facades\Log;
 
@@ -25,10 +26,20 @@ class UndeployCheck extends Job
 
     public function handle()
     {
-        $response = $this->model->router->availabilityZone->nsxService()->get(
-            'policy/api/v1/infra/tier-1s/' . $this->model->router->id . '/segments/?include_mark_for_delete_objects=true'
-        );
-        $response = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->model->router->availabilityZone->nsxService()->get(
+                'policy/api/v1/infra/tier-1s/' . $this->model->router->id . '/segments/?include_mark_for_delete_objects=true'
+            );
+            $response = json_decode($response->getBody()->getContents());
+        } catch (RequestException $exception) {
+            if ($exception->hasResponse() && $exception->getResponse()->getStatusCode() != 404) {
+                Log::warning(
+                    'Network ' . $this->model->id . ' not found, skipping'
+                );
+                return;
+            }
+            throw $exception;
+        }
         foreach ($response->results as $result) {
             if ($this->model->id === $result->id) {
                 Log::info(
