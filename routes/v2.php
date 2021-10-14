26373381bdab0ed29bc5c95c32eb2646e9784e92
Umair Majeed <umairbmajeed@gmail.com>
2021-10-14 12:58:36 +0100

    GET /instances/{id}/floating-ips endpoint

--- routes/v2.php
+++ routes/v2.php
@@ -207,6 +207,7 @@ $router->group($baseRouteParameters, function () use ($router) {
         $router->get('instances/{instanceId}/volumes', 'InstanceController@volumes');
         $router->get('instances/{instanceId}/nics', 'InstanceController@nics');
         $router->get('instances/{instanceId}/tasks', 'InstanceController@tasks');
+        $router->get('instances/{instanceId}/floating-ips', 'InstanceController@floatingIps');
         $router->put('instances/{instanceId}/lock', 'InstanceController@lock');
         $router->put('instances/{instanceId}/unlock', 'InstanceController@unlock');
         $router->post('instances/{instanceId}/console-session', 'InstanceController@consoleSession');
