811749be6e141e4893b1701c31a9ead9c62cd346
Umair Majeed <umairbmajeed@gmail.com>
2021-10-15 14:55:00 +0100

    Renamed from subnet to management_range as suggested
    #1078 eCloud VPC | LBv2 - management networking

--- config/network.php
+++ config/network.php
@@ -7,7 +7,7 @@ return [
         'ip-discovery-profile' => 'ecloud-ip-discovery-profile',
         'mac-discovery-profile' => 'ecloud-mac-discovery-profile',
     ],
-    'subnet' => [
+    'management_range' => [
         'standard' => '192.168.0.0/17',
         'advanced' => '192.168.128.0/17',
     ],
