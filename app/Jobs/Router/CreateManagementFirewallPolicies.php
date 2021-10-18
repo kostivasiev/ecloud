6a9bf0ab10edf5b953d6a3a79a2602faf265680a
Umair Majeed <umairbmajeed@gmail.com>
2021-10-18 15:18:26 +0100

    Updated Firewall and Network policy job to await resources exactly like the other jobs
    #1078 eCloud VPC | LBv2 - management networking

--- app/Jobs/Router/CreateManagementFirewallPolicies.php
+++ app/Jobs/Router/CreateManagementFirewallPolicies.php
@@ -10,6 +10,7 @@ use App\Models\V2\Task;
 use App\Traits\V2\Jobs\AwaitResources;
 use App\Traits\V2\LoggableModelJob;
 use Illuminate\Bus\Batchable;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 
 class CreateManagementFirewallPolicies extends Job
@@ -31,54 +32,68 @@ class CreateManagementFirewallPolicies extends Job
     public function handle()
     {
         if (!empty($this->task->data['management_router_id']) && !empty($this->task->data['management_network_id'])) {
-            $managementRouter = Router::find($this->task->data['management_router_id']);
-            if ($managementRouter) {
-                Log::info(get_class($this) . ' - Create Firewall Policy and Rules Start', [
-                    'router_id' => $managementRouter->id,
-                ]);
+            $firewallPolicy = false;
+            if (empty($this->task->data['firewall_policy_id'])) {
+                $managementRouter = Router::find($this->task->data['management_router_id']);
+                if ($managementRouter) {
+                    Log::info(get_class($this) . ' - Create Firewall Policy and Rules Start', [
+                        'router_id' => $managementRouter->id,
+                    ]);
 
-                $firewallPolicy = new FirewallPolicy([
-                    'name' => 'Management_Firewall_Policy_for_' . $managementRouter->id,
-                    'router_id' => $managementRouter->id,
-                    'sequence' => 0,
-                ]);
-                $firewallPolicy->save();
+                    $firewallPolicy = new FirewallPolicy([
+                        'name' => 'Management_Firewall_Policy_for_' . $managementRouter->id,
+                        'router_id' => $managementRouter->id,
+                        'sequence' => 0,
+                    ]);
+                    $firewallPolicy->save();
 
-                // Allow inbound 4222
-                $firewallRule = new FirewallRule([
-                    'name' => 'Allow_Ed_on_Port_4222_inbound_' . $managementRouter->id,
-                    'sequence' => 10,
-                    'firewall_policy_id' => $firewallPolicy->id,
-                    'source' => 'ANY',
-                    'destination' => 'ANY',
-                    'action' => 'ALLOW',
-                    'direction' => 'IN',
-                    'enabled' => true
-                ]);
-                $firewallRule->save();
-                $firewallRulePort = new FirewallRulePort([
-                    'firewall_rule_id' => $firewallRule->id,
-                    'protocol' => 'TCP',
-                    'source' => '4222',
-                    'destination' => '4222'
-                ]);
-                $firewallRulePort->save();
+                    // Allow inbound 4222
+                    $firewallRule = new FirewallRule([
+                        'name' => 'Allow_Ed_on_Port_4222_inbound_' . $managementRouter->id,
+                        'sequence' => 10,
+                        'firewall_policy_id' => $firewallPolicy->id,
+                        'source' => 'ANY',
+                        'destination' => 'ANY',
+                        'action' => 'ALLOW',
+                        'direction' => 'IN',
+                        'enabled' => true
+                    ]);
+                    $firewallRule->save();
+                    $firewallRulePort = new FirewallRulePort([
+                        'firewall_rule_id' => $firewallRule->id,
+                        'protocol' => 'TCP',
+                        'source' => '4222',
+                        'destination' => '4222'
+                    ]);
+                    $firewallRulePort->save();
 
-                // Block all outbound
-                (new FirewallRule([
-                    'name' => 'Block_all_outbound_' . $managementRouter->id,
-                    'sequence' => 10,
-                    'firewall_policy_id' => $firewallPolicy->id,
-                    'source' => 'ANY',
-                    'destination' => 'ANY',
-                    'action' => 'REJECT',
-                    'direction' => 'OUT',
-                    'enabled' => true
-                ]))->save();
-                $firewallPolicy->syncSave();
+                    // Block all outbound
+                    (new FirewallRule([
+                        'name' => 'Block_all_outbound_' . $managementRouter->id,
+                        'sequence' => 10,
+                        'firewall_policy_id' => $firewallPolicy->id,
+                        'source' => 'ANY',
+                        'destination' => 'ANY',
+                        'action' => 'REJECT',
+                        'direction' => 'OUT',
+                        'enabled' => true
+                    ]))->save();
+                    $firewallPolicy->syncSave();
 
-                Log::info(get_class($this) . ' - Create Firewall Policy and Rules End', [
-                    'router_id' => $managementRouter->id,
+                    $this->task->data = Arr::add($this->task->data, 'firewall_policy_id', $firewallPolicy->id);
+                    $this->task->saveQuietly();
+
+                    Log::info(get_class($this) . ' - Create Firewall Policy and Rules End', [
+                        'router_id' => $managementRouter->id,
+                    ]);
+                }
+            } else {
+                $firewallPolicy = FirewallPolicy::findOrFail($this->task->data['firewall_policy_id']);
+            }
+
+            if ($firewallPolicy) {
+                $this->awaitSyncableResources([
+                    $firewallPolicy->id,
                 ]);
             }
         }
--- app/Jobs/Router/CreateManagementNetworkPolicies.php
+++ app/Jobs/Router/CreateManagementNetworkPolicies.php
@@ -11,6 +11,7 @@ use App\Models\V2\Task;
 use App\Traits\V2\Jobs\AwaitResources;
 use App\Traits\V2\LoggableModelJob;
 use Illuminate\Bus\Batchable;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 
 class CreateManagementNetworkPolicies extends Job
@@ -32,55 +33,69 @@ class CreateManagementNetworkPolicies extends Job
     public function handle()
     {
         $router = $this->model;
-        if ($router->vpc->advanced_networking) {
-            if (!empty($this->task->data['management_router_id']) && !empty($this->task->data['management_network_id'])) {
-                $managementNetwork = Network::find($this->task->data['management_network_id']);
-                if ($managementNetwork) {
-                    Log::info(get_class($this) . ' - Create Network Policy and Rules Start', [
-                        'network_id' => $managementNetwork->id,
-                    ]);
+        if (!empty($this->task->data['management_router_id']) && !empty($this->task->data['management_network_id'])) {
+            $networkPolicy = false;
+            if ($router->vpc->advanced_networking) {
+                if (empty($this->task->data['network_policy_id'])) {
+                    $managementNetwork = Network::find($this->task->data['management_network_id']);
+                    if ($managementNetwork) {
+                        Log::info(get_class($this) . ' - Create Network Policy and Rules Start', [
+                            'network_id' => $managementNetwork->id,
+                        ]);
 
-                    $networkPolicy = new NetworkPolicy([
-                        'name' => 'Management_Network_Policy_for_' . $managementNetwork->id,
-                        'network_id' => $managementNetwork->id,
-                    ]);
-                    $networkPolicy->save();
+                        $networkPolicy = new NetworkPolicy([
+                            'name' => 'Management_Network_Policy_for_' . $managementNetwork->id,
+                            'network_id' => $managementNetwork->id,
+                        ]);
+                        $networkPolicy->save();
 
-                    // Allow inbound 4222
-                    $networkRule = new NetworkRule([
-                        'name' => 'Allow_Ed_on_Port_4222_inbound_' . $managementNetwork->id,
-                        'sequence' => 10,
-                        'network_policy_id' => $networkPolicy->id,
-                        'source' => 'ANY',
-                        'destination' => 'ANY',
-                        'action' => 'ALLOW',
-                        'direction' => 'IN',
-                        'enabled' => true
-                    ]);
-                    $networkRule->save();
-                    $networkRulePort = new NetworkRulePort([
-                        'network_rule_id' => $networkRule->id,
-                        'protocol' => 'TCP',
-                        'source' => '4222',
-                        'destination' => '4222'
-                    ]);
-                    $networkRulePort->save();
+                        // Allow inbound 4222
+                        $networkRule = new NetworkRule([
+                            'name' => 'Allow_Ed_on_Port_4222_inbound_' . $managementNetwork->id,
+                            'sequence' => 10,
+                            'network_policy_id' => $networkPolicy->id,
+                            'source' => 'ANY',
+                            'destination' => 'ANY',
+                            'action' => 'ALLOW',
+                            'direction' => 'IN',
+                            'enabled' => true
+                        ]);
+                        $networkRule->save();
+                        $networkRulePort = new NetworkRulePort([
+                            'network_rule_id' => $networkRule->id,
+                            'protocol' => 'TCP',
+                            'source' => '4222',
+                            'destination' => '4222'
+                        ]);
+                        $networkRulePort->save();
 
-                    // Block all outbound
-                    (new NetworkRule([
-                        'name' => 'Block_all_outbound_' . $managementNetwork->id,
-                        'sequence' => 10,
-                        'network_policy_id' => $networkPolicy->id,
-                        'source' => 'ANY',
-                        'destination' => 'ANY',
-                        'action' => 'REJECT',
-                        'direction' => 'OUT',
-                        'enabled' => true
-                    ]))->save();
-                    $networkPolicy->syncSave();
+                        // Block all outbound
+                        (new NetworkRule([
+                            'name' => 'Block_all_outbound_' . $managementNetwork->id,
+                            'sequence' => 10,
+                            'network_policy_id' => $networkPolicy->id,
+                            'source' => 'ANY',
+                            'destination' => 'ANY',
+                            'action' => 'REJECT',
+                            'direction' => 'OUT',
+                            'enabled' => true
+                        ]))->save();
+                        $networkPolicy->syncSave();
 
-                    Log::info(get_class($this) . ' - Create Network Policy and Rules End', [
-                        'network_id' => $managementNetwork->id,
+                        $this->task->data = Arr::add($this->task->data, 'network_policy_id', $networkPolicy->id);
+                        $this->task->saveQuietly();
+
+                        Log::info(get_class($this) . ' - Create Network Policy and Rules End', [
+                            'network_id' => $managementNetwork->id,
+                        ]);
+                    }
+                } else {
+                    $networkPolicy = NetworkPolicy::findOrFail($this->task->data['network_policy_id']);
+                }
+
+                if ($networkPolicy) {
+                    $this->awaitSyncableResources([
+                        $networkPolicy->id,
                     ]);
                 }
             }
