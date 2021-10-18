a7d8f84e078862c722c86d805de1dac11c4e9be3
Umair Majeed <umairbmajeed@gmail.com>
2021-10-18 16:13:04 +0100

    update event faker on NewIDTest

--- tests/V2/NewIDTest.php
+++ tests/V2/NewIDTest.php
@@ -2,13 +2,12 @@
 
 namespace Tests\V2;
 
-use App\Listeners\V2\TaskCreated;
+use App\Events\V2\Task\Created;
 use App\Models\V2\AvailabilityZone;
 use App\Models\V2\Region;
 use App\Models\V2\Router;
 use App\Models\V2\Vpc;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
@@ -38,8 +37,6 @@ class NewIDTest extends TestCase
             'vpc_id' => $this->vpc()->id,
             'availability_zone_id' => $this->availabilityZone->id
         ]);
-        Event::fake(TaskCreated::class);
-        Bus::fake();
     }
 
     public function testFormatOfAvailabilityZoneID()
@@ -63,6 +60,7 @@ class NewIDTest extends TestCase
 
     public function testFormatOfRoutersId()
     {
+        Event::fake(Created::class);
         App::shouldReceive('environment')->zeroOrMoreTimes()->andReturn('local');
         $this->post('/v2/routers', [
             'name' => 'Manchester Router 1',
@@ -77,11 +75,15 @@ class NewIDTest extends TestCase
             $this->generateRegExp(Router::class),
             (json_decode($this->response->getContent()))->data->id
         );
+        Event::assertDispatched(Created::class);
     }
 
     public function testDevEnvironmentId()
     {
+        Event::fake(Created::class);
+
         App::shouldReceive('environment')->zeroOrMoreTimes()->andReturn('local');
+
         $this->post('/v2/routers', [
             'name' => 'Manchester Router 1',
             'vpc_id' => $this->vpc()->id,
@@ -95,10 +97,13 @@ class NewIDTest extends TestCase
             $this->generateRegExp(Router::class),
             (json_decode($this->response->getContent()))->data->id
         );
+
+        Event::assertDispatched(Created::class);
     }
 
     public function testProductionEnvironmentId()
     {
+        Event::fake(Created::class);
         App::shouldReceive('environment')->zeroOrMoreTimes()->andReturn('production');
         $this->post('/v2/routers', [
             'name' => 'Manchester Router 1',
@@ -113,6 +118,7 @@ class NewIDTest extends TestCase
             $this->generateRegExp(Router::class),
             (json_decode($this->response->getContent()))->data->id
         );
+        Event::assertDispatched(Created::class);
     }
 
     /**
