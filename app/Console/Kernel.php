9d910dced1bbd74c1cb257064982fbf7214b4d17
Umair Majeed <umairbmajeed@gmail.com>
2021-10-20 13:54:37 +0100

    remove artisan script

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Health\FindOrphanedNics::class,
         \App\Console\Commands\Task\TimeoutStuck::class,
         \App\Console\Commands\IpAddress\PopulateNetworkId::class,
-        \App\Console\Commands\Router\PopulateisManagementProperty::class,
     ];
 
     /**
