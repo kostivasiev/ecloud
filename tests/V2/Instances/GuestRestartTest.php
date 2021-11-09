b2b64dc1118b6c317fd81409fcba310b6c8ba75d
Umair Majeed <umairbmajeed@gmail.com>
2021-11-09 15:08:34 +0000

    Fixed test and coding standards
    #1243 stop compute charges when instance is powered down

--- tests/V2/Instances/GuestRestartTest.php
+++ tests/V2/Instances/GuestRestartTest.php
@@ -11,9 +11,28 @@ class GuestRestartTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->kingpinServiceMock()->shouldReceive('put')->withArgs(['/api/v2/vpc/' . $this->vpc()->id . '/instance/' . $this->instance()->id . '/power/guest/restart'])->andReturn(
-            new Response(200)
-        );
+        $this->kingpinServiceMock()
+            ->shouldReceive('put')
+            ->withArgs(['/api/v2/vpc/' . $this->vpc()->id . '/instance/' . $this->instance()->id . '/power/guest/restart'])
+            ->andReturn(
+                new Response(200)
+            );
+        $this->kingpinServiceMock()->expects('get')
+            ->withArgs(['/api/v2/vpc/' . $this->instance()->vpc_id . '/instance/' . $this->instance()->id])
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'powerState' => 'poweredOn',
+                    'toolsRunningStatus' => 'guestToolsRunning',
+                ]));
+            });
+        $this->kingpinServiceMock()->expects('get')
+            ->withArgs(['/api/v2/vpc/' . $this->instance()->vpc_id . '/instance/' . $this->instance()->id])
+            ->andReturnUsing(function () {
+                return new Response(200, [], json_encode([
+                    'powerState' => 'poweredOff',
+                    'toolsRunningStatus' => 'guestToolsRunning',
+                ]));
+            });
     }
 
     public function testGuestRestart()
