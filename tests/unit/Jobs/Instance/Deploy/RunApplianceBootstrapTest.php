0e7ddab1f78debb9f1beaec8e5b2cf09ec39f209
Umair Majeed <umairbmajeed@gmail.com>
2021-11-18 12:43:38 +0000

    Updated Accounts service with some helper methods to get the primary contact id and primary contact email address without touching anything else in order not to break other functionality.
    Modified RunApplianceBootstrap to grab the primary_contact_email and generate a password for plesk images if one is not available.
    
    Added mock generation for AccountsService to Testcase.
    
    #1261 update orchestrator to process plesk images

--- tests/unit/Jobs/Instance/Deploy/RunApplianceBootstrapTest.php
+++ tests/unit/Jobs/Instance/Deploy/RunApplianceBootstrapTest.php
@@ -4,10 +4,13 @@ namespace Tests\unit\Jobs\Instance\Deploy;
 
 use App\Jobs\Instance\Deploy\RunApplianceBootstrap;
 use App\Models\V2\Credential;
+use App\Models\V2\ImageMetadata;
 use App\Models\V2\ImageParameter;
+use App\Services\V2\PasswordService;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\Facades\Event;
+use Mustache_Engine;
 use Tests\TestCase;
 
 class RunApplianceBootstrapTest extends TestCase
@@ -70,6 +73,80 @@ class RunApplianceBootstrapTest extends TestCase
             });
 
         dispatch(new RunApplianceBootstrap($this->instance()));
+
+        Event::assertNotDispatched(JobFailed::class);
+    }
+
+    public function testGetsAdminEmailAndPassword()
+    {
+        factory(ImageParameter::class)->create([
+            'image_id' => $this->image()->id,
+            'name' => 'Plesk Admin Password',
+            'key' => 'plesk_admin_password',
+            'type' => 'Password',
+            'description' => 'Plesk Admin Password',
+            'required' => true,
+            'validation_rule' => '/\w+/',
+        ]);
+
+        factory(ImageMetadata::class)->create([
+            'key' => 'ukfast.license.identifier',
+            'value' => 'PLESK-12-VPS-WEB-HOST-1M',
+            'image_id' => $this->image()->id
+        ]);
+
+        factory(ImageMetadata::class)->create([
+            'key' => 'ukfast.license.type',
+            'value' => 'plesk',
+            'image_id' => $this->image()->id
+        ]);
+
+        $this->image()->setAttribute(
+            'script_template',
+            '{{{ plesk_admin_email_address }}} {{{ plesk_admin_password }}}'
+        )->save();
+
+        $credential = app()->make(Credential::class);
+        $credential->fill([
+            'name' => 'root',
+            'username' => 'root',
+            'password' => 'somepassword'
+        ]);
+        $this->instance()->credentials()->save($credential);
+
+        $this->accountsServiceMock()
+            ->allows('getPrimaryContactId')
+            ->andReturns('111');
+        $this->accountsServiceMock()
+            ->allows('getPrimaryContactEmail')
+            ->andReturns('elmer.fudd@example.com');
+
+        $this->kingpinServiceMock()
+            ->expects('post')
+            ->withArgs([
+                '/api/v2/vpc/' . $this->instance()->vpc->id .
+                '/instance/' . $this->instance()->id .
+                '/guest/linux/script',
+                [
+                    'json' => [
+                        'encodedScript' => base64_encode('elmer.fudd@example.com somepassword'),
+                        'username' => 'root',
+                        'password' => 'somepassword'
+                    ]
+                ]
+            ])
+            ->andReturnUsing(function () {
+                return new Response(200);
+            });
+
+        dispatch(new RunApplianceBootstrap($this->instance()));
+
+        $pleskAdminPassword = ($this->instance()
+            ->credentials()
+            ->where('name', '=', 'plesk_admin_password')
+            ->first())
+            ->password;
+        $this->assertEquals('somepassword', $pleskAdminPassword);
 
         Event::assertNotDispatched(JobFailed::class);
     }
