5c1203f9e8c2199dd2a1d8994a9d21db9a47d5e0
Umair Majeed <umairbmajeed@gmail.com>
2021-11-22 11:06:12 +0000

    Added artisan command to cleanup networking.advanced metrics. Added billingMetrics relationship to VPC model as this was missing (the belongsTo exists in the billingMetric), which greatly simplifies the queries required.
    Also added an appropriate test for the command.
    
    #1264 eCloud VPC | Advanced networking is being billed for everyone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Task\TimeoutStuck::class,
         \App\Console\Commands\IpAddress\PopulateNetworkId::class,
         \App\Console\Commands\VPC\ConvertBilling::class,
+        \App\Console\Commands\Billing\CleanupAdvancedNetworking::class,
     ];
 
     /**
