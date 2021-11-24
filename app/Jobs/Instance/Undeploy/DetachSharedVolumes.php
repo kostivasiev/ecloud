80dcad3ea523be5bb59465859b1598f93219d770
Umair Majeed <umairbmajeed@gmail.com>
2021-11-24 17:28:26 +0000

    Adds DetachSharedVolumes job to instance undeploy

---
+++ app/Jobs/Instance/Undeploy/DetachSharedVolumes.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Jobs\Instance\Undeploy;
+
+use App\Jobs\Job;
+use App\Jobs\Tasks\Instance\VolumeDetach;
+use App\Models\V2\Instance;
+use App\Models\V2\Task;
+use App\Traits\V2\Jobs\AwaitTask;
+use App\Traits\V2\LoggableModelJob;
+use Illuminate\Bus\Batchable;
+use Illuminate\Support\Facades\Log;
+
+class DetachSharedVolumes extends Job
+{
+    use Batchable, LoggableModelJob, AwaitTask;
+
+    public $tries = 60;
+    public $backoff = 5;
+
+    private Task $task;
+    private Instance $model;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+        $this->model = $this->task->resource;
+    }
+
+    public function handle()
+    {
+        $this->model->volumes()->where('is_shared', '=', true)
+            ->each(function ($volume) {
+                if (isset($this->task->data['volume_detach_task_id'])) {
+                    $task = Task::findOrFail($this->task->data['volume_detach_task_id']);
+                    if (!$task->completed) {
+                        $this->awaitTaskWithRelease($task);
+                    }
+                    $this->task->setAttribute('data', null)->saveQuietly();
+                }
+
+                Log::info('Detaching volume from instance', ['instance_id' => $this->model->id, 'volume_id' => $volume->id]);
+                $task = $this->model->createTask('volume_detach', VolumeDetach::class, ['volume_id' => $volume->id]);
+                $this->task->setAttribute('data', ['volume_detach_task_id' => $task->id])->saveQuietly();
+                $this->awaitTaskWithRelease($task);
+            });
+    }
+}
