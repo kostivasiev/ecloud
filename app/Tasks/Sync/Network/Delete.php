e1d3074191db3cc0f20e55936ce076d6fb3755da
Umair Majeed <umairbmajeed@gmail.com>
2021-12-13 15:02:09 +0000

    move rest of delete sync

--- app/Tasks/Sync/Network/Delete.php
+++ app/Tasks/Sync/Network/Delete.php
@@ -5,6 +5,10 @@ namespace App\Tasks\Sync\Network;
 use App\Jobs\Network\AwaitNetworkPolicyRemoval;
 use App\Jobs\Network\AwaitPortRemoval;
 use App\Jobs\Network\DeleteNetworkPolicy;
+use App\Jobs\Network\Undeploy;
+use App\Jobs\Network\UndeployCheck;
+use App\Jobs\Network\UndeployDiscoveryProfiles;
+use App\Jobs\Network\UndeployQoSProfiles;
 use App\Jobs\Network\UndeploySecurityProfiles;
 use App\Tasks\Task;
 
@@ -17,6 +21,10 @@ class Delete extends Task
             AwaitNetworkPolicyRemoval::class,
             AwaitPortRemoval::class,
             UndeploySecurityProfiles::class,
+            UndeployDiscoveryProfiles::class,
+            UndeployQoSProfiles::class,
+            Undeploy::class,
+            UndeployCheck::class,
         ];
     }
 }
