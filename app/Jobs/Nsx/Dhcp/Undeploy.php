ed89512d2439e998db173bfec6e3a32475aa90e0
Umair Majeed <umairbmajeed@gmail.com>
2021-12-20 15:51:21 +0000

    move to tasks

--- app/Jobs/Nsx/Dhcp/Undeploy.php
+++ app/Jobs/Nsx/Dhcp/Undeploy.php
@@ -2,26 +2,16 @@
 
 namespace App\Jobs\Nsx\Dhcp;
 
-use App\Jobs\Job;
-use App\Models\V2\Dhcp;
-use App\Traits\V2\LoggableModelJob;
-use Illuminate\Bus\Batchable;
+use App\Jobs\TaskJob;
 
-class Undeploy extends Job
+class Undeploy extends TaskJob
 {
-    use Batchable, LoggableModelJob;
-
-    private Dhcp $model;
-
-    public function __construct(Dhcp $dhcp)
-    {
-        $this->model = $dhcp;
-    }
-
     public function handle()
     {
-        $this->model->availabilityZone->nsxService()->delete(
-            '/policy/api/v1/infra/dhcp-server-configs/' . $this->model->id
+        $dhcp = $this->task->resource;
+
+        $dhcp->availabilityZone->nsxService()->delete(
+            '/policy/api/v1/infra/dhcp-server-configs/' . $dhcp->id
         );
     }
 }
