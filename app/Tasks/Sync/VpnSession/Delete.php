eb87b723ff3c39d1f5adf1a93a2ef0c37ed28f0d
Umair Majeed <umairbmajeed@gmail.com>
2022-01-05 09:27:43 +0000

    move rest of delete jobs

--- app/Tasks/Sync/VpnSession/Delete.php
+++ app/Tasks/Sync/VpnSession/Delete.php
@@ -3,7 +3,10 @@
 namespace App\Tasks\Sync\VpnSession;
 
 use App\Jobs\Nsx\VpnSession\UndeployCheck;
+use App\Jobs\VpnSession\DeletePreSharedKey;
 use App\Jobs\VpnSession\Nsx\Undeploy;
+use App\Jobs\VpnSession\RemoveNetworks;
+use App\Jobs\VpnSession\SyncNetworkNoSNats;
 use App\Tasks\Task;
 
 class Delete extends Task
@@ -13,6 +16,9 @@ class Delete extends Task
         return [
             Undeploy::class,
             UndeployCheck::class,
+            RemoveNetworks::class,
+            SyncNetworkNoSNats::class,
+            DeletePreSharedKey::class,
         ];
     }
 }
