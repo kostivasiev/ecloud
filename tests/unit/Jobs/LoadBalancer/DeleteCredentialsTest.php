23ca34ea95c14735eb04871e9cf20e7617341634
Umair Majeed <umairbmajeed@gmail.com>
2022-01-06 09:16:14 +0000

    Merge in Master

--- tests/unit/Jobs/LoadBalancer/DeleteCredentialsTest.php
+++ tests/unit/Jobs/LoadBalancer/DeleteCredentialsTest.php
@@ -4,7 +4,6 @@ namespace Tests\unit\Jobs\LoadBalancer;
 
 use App\Events\V2\Task\Created;
 use App\Jobs\LoadBalancer\DeleteCredentials;
-use App\Models\V2\Credential;
 use App\Models\V2\Task;
 use App\Support\Sync;
 use Illuminate\Database\Eloquent\Model;
