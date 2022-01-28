ab8c3e0d7b2b71d13086a4aa9ed0b8ae1a99ff6c
Umair Majeed <umairbmajeed@gmail.com>
2022-01-28 07:45:05 +0000

    added command for vpc support

---
+++ app/Console/Commands/VPC/ConvertVpcSupportToFlag.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Console\Commands\VPC;
+
+use App\Listeners\V2\Vpc\UpdateSupportEnabledBilling;
+use App\Models\V2\BillingMetric;
+use App\Models\V2\VpcSupport;
+use App\Rules\V2\PaymentRequired;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * This script does the following:-
+ *
+ * 1. Gets current active VPC Supports and puts them into a new billing metric
+ * 1a. (Optional) Gets history of VPC Supports and puts them into new billing metric.
+ * 2. Verifies the data is still valid.
+ */
+class ConvertVpcSupportToFlag extends Command
+{
+    protected $signature = 'vpc:convert-support-flag {--T|test-run}';
+    protected $description = 'Converts VPC Support entries to flags tracked by billing metrics.';
+
+    private Collection $vpcSupportActive;
+    private Collection $vpcSupportHistory;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->vpcSupportActive = VpcSupport::withoutTrashed()->get();
+        $this->vpcSupportHistory = VpcSupport::onlyTrashed()->get();
+    }
+
+    public function handle()
+    {
+        $i = 0;
+        if ($this->ask("Would you like to include history? (Y/N)") === 'Y') {
+            foreach ($this->vpcSupportHistory as $vpcHistory) {
+                $this->saveSupport($vpcHistory->vpc, $vpcHistory->start_date, $vpcHistory->end_date ?? null, false);
+                $i++;
+            }
+        }
+
+        foreach ($this->vpcSupportActive as $vpcActive) {
+            $this->saveSupport($vpcActive->vpc, $vpcActive->start_date, $vpcActive->end_date ?? null);
+            $i++;
+        }
+
+        $this->info(sprintf('Successfully moved %s VPC Support entries to flags.', $i));
+    }
+
+    private function saveSupport($vpc, Carbon $start, ?Carbon $end = null, $active = true)
+    {
+        $billingMetric = app()->make(BillingMetric::class);
+        $billingMetric->resource_id = $vpc->id;
+        $billingMetric->vpc_id = $vpc->id;
+        $billingMetric->reseller_id = $vpc->reseller_id;
+        $billingMetric->start = $start;
+        $billingMetric->name = UpdateSupportEnabledBilling::getFriendlyName();
+        $billingMetric->key = UpdateSupportEnabledBilling::getKeyName();
+        $billingMetric->value = 1;
+        if ($end !== null) {
+            $billingMetric->end = $end;
+        } elseif ($active === false) {
+            $billingMetric->end = Carbon::now();
+        }
+        if (!$this->option('test-run')) {
+            $billingMetric->save();
+        }
+    }
+}
---
+++ app/Models/V2/VpcSupport.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Models\V2;
+
+use App\Traits\V2\CustomKey;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use UKFast\Api\Auth\Consumer;
+use UKFast\DB\Ditto\Factories\FilterFactory;
+use UKFast\DB\Ditto\Factories\SortFactory;
+use UKFast\DB\Ditto\Filter;
+use UKFast\DB\Ditto\Filterable;
+use UKFast\DB\Ditto\Sortable;
+
+class VpcSupport extends Model implements Filterable, Sortable
+{
+    use CustomKey, SoftDeletes;
+
+    protected $table = 'vpc_support';
+    public $keyPrefix = 'sup';
+    public $incrementing = false;
+    public $timestamps = true;
+    protected $keyType = 'string';
+    protected $connection = 'ecloud';
+
+    protected $fillable = [
+        'id',
+        'vpc_id',
+        'start_date',
+        'end_date',
+    ];
+
+    public function vpc()
+    {
+        return $this->belongsTo(Vpc::class);
+    }
+
+    /**
+     * @param $query
+     * @param $user
+     * @return mixed
+     */
+    public function scopeForUser($query, Consumer $user)
+    {
+        if (!$user->isScoped()) {
+            return $query;
+        }
+        return $query->whereHas('vpc', function ($query) use ($user) {
+            $query->where('reseller_id', $user->resellerId());
+        });
+    }
+
+    public function getActiveAttribute()
+    {
+        if (is_null($this->start_date)) {
+            // no start date yet
+            return false;
+        }
+
+        if (strtotime($this->start_date) > time()) {
+            // start date is in the future
+            return false;
+        }
+
+        // start date is in the past
+
+        if (is_null($this->end_date)) {
+            // no end date yet
+            return true;
+        }
+
+        if (strtotime($this->end_date) < time()) {
+            // end date is in the past
+            return false;
+        }
+
+        if (strtotime($this->end_date) > time()) {
+            // end date is in the future
+            return true;
+        }
+
+        //should never hit this point but just in case
+        return false;
+    }
+
+    /**
+     * @param FilterFactory $factory
+     * @return array|Filter[]
+     */
+    public function filterableColumns(FilterFactory $factory)
+    {
+        return [
+            $factory->create('id', Filter::$stringDefaults),
+            $factory->create('vpc_id', Filter::$stringDefaults),
+            $factory->create('start_date', Filter::$dateDefaults),
+            $factory->create('end_date', Filter::$dateDefaults),
+            $factory->create('created_at', Filter::$dateDefaults),
+            $factory->create('updated_at', Filter::$dateDefaults),
+        ];
+    }
+
+    /**
+     * @param SortFactory $factory
+     * @return array|\UKFast\DB\Ditto\Sort[]
+     * @throws \UKFast\DB\Ditto\Exceptions\InvalidSortException
+     */
+    public function sortableColumns(SortFactory $factory)
+    {
+        return [
+            $factory->create('id'),
+            $factory->create('vpc_id'),
+            $factory->create('start_date'),
+            $factory->create('end_date'),
+            $factory->create('created_at'),
+            $factory->create('updated_at'),
+        ];
+    }
+
+    /**
+     * @param SortFactory $factory
+     * @return array|\UKFast\DB\Ditto\Sort|\UKFast\DB\Ditto\Sort[]|null
+     * @throws \UKFast\DB\Ditto\Exceptions\InvalidSortException
+     */
+    public function defaultSort(SortFactory $factory)
+    {
+        return [
+            $factory->create('created_at', 'desc'),
+        ];
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function databaseNames()
+    {
+        return [
+            'id' => 'id',
+            'vpc_id' => 'vpc_id',
+            'start_date' => 'start_date',
+            'end_date' => 'end_date',
+            'created_at' => 'created_at',
+            'updated_at' => 'updated_at',
+        ];
+    }
+}
