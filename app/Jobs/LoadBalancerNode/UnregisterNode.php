fcafb70cc72964342a320ea841b7dbe69fcaaed7
Umair Majeed <umairbmajeed@gmail.com>
2022-02-03 16:03:12 +0000

    Fixed idempotency, catch error type. If 404 thrown then skip the job otherwise bubble the exception. Check the return status of the SDK call, if false (fail) throw an exception, otherwise log success.
    #1328 update delete node sync job

--- app/Jobs/LoadBalancerNode/UnregisterNode.php
+++ app/Jobs/LoadBalancerNode/UnregisterNode.php
@@ -7,26 +7,43 @@ use UKFast\Admin\Loadbalancers\AdminClient;
 
 class UnregisterNode extends TaskJob
 {
+    /**
+     * @throws \Exception
+     */
     public function handle()
     {
         $loadBalancerNode = $this->task->resource;
-        $client = app()->make(AdminClient::class)
-            ->setResellerId($loadBalancerNode->loadBalancer->getResellerId());
-        try {
-            $client->nodes()->deleteById($loadBalancerNode->node_id);
-        } catch (\Exception $e) {
-            $this->info('Unregistering instance as a loadbalancer node failed', [
+        $success = false;
+
+        if (empty($this->task->data['load_balancer_node_id'])) {
+            $client = app()->make(AdminClient::class)
+                ->setResellerId($loadBalancerNode->loadBalancer->getResellerId());
+            try {
+                $success = $client->nodes()->deleteById($loadBalancerNode->node_id);
+            } catch (\Exception $e) {
+                if ($e->getCode() !== 404) {
+                    throw $e;
+                }
+                $this->info('Loadbalancer node not found, skipping', [
+                    'id' => $loadBalancerNode->id,
+                    'instance_id' => $loadBalancerNode->instance_id,
+                    'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
+                ]);
+                return;
+            }
+            if (!$success) {
+                $this->fail(new \Exception('Failed to unregister loadbalancer node', [
+                    'id' => $loadBalancerNode->id,
+                    'instance_id' => $loadBalancerNode->instance_id,
+                    'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
+                ]));
+            }
+            $this->info('Unregistered instance as loadbalancer node', [
                 'id' => $loadBalancerNode->id,
                 'instance_id' => $loadBalancerNode->instance_id,
-                'load_balancer_id' => $loadBalancerNode->load_balancer_id,
+                'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
             ]);
-            return;
+            $this->task->updateData('load_balancer_node_id', $loadBalancerNode->node_id);
         }
-        $this->info('Unregistering instance as loadbalancer node', [
-            'id' => $loadBalancerNode->id,
-            'instance_id' => $loadBalancerNode->instance_id,
-            'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
-        ]);
-        $loadBalancerNode->setAttribute('node_id', null)->saveQuietly();
     }
 }
