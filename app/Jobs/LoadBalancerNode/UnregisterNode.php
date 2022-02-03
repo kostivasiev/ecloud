d00ffbcc70a99a2ec58aaed5526aa15ca5fd1b81
Umair Majeed <umairbmajeed@gmail.com>
2022-02-03 16:17:23 +0000

    Removed success test, altered exception reference to ApiException and better trap that.
    #1328 update delete node sync job

--- app/Jobs/LoadBalancerNode/UnregisterNode.php
+++ app/Jobs/LoadBalancerNode/UnregisterNode.php
@@ -4,6 +4,7 @@ namespace App\Jobs\LoadBalancerNode;
 
 use App\Jobs\TaskJob;
 use UKFast\Admin\Loadbalancers\AdminClient;
+use UKFast\SDK\Exception\ApiException;
 
 class UnregisterNode extends TaskJob
 {
@@ -13,15 +14,14 @@ class UnregisterNode extends TaskJob
     public function handle()
     {
         $loadBalancerNode = $this->task->resource;
-        $success = false;
 
         if (empty($this->task->data['load_balancer_node_id'])) {
             $client = app()->make(AdminClient::class)
                 ->setResellerId($loadBalancerNode->loadBalancer->getResellerId());
             try {
-                $success = $client->nodes()->deleteById($loadBalancerNode->node_id);
-            } catch (\Exception $e) {
-                if ($e->getCode() !== 404) {
+                $client->nodes()->deleteById($loadBalancerNode->node_id);
+            } catch (ApiException $e) {
+                if ($e->getStatusCode() !== 404) {
                     throw $e;
                 }
                 $this->info('Loadbalancer node not found, skipping', [
@@ -30,13 +30,6 @@ class UnregisterNode extends TaskJob
                     'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
                 ]);
                 return;
-            }
-            if (!$success) {
-                $this->fail(new \Exception('Failed to unregister loadbalancer node', [
-                    'id' => $loadBalancerNode->id,
-                    'instance_id' => $loadBalancerNode->instance_id,
-                    'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
-                ]));
             }
             $this->info('Unregistered instance as loadbalancer node', [
                 'id' => $loadBalancerNode->id,
