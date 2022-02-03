207818b5ab5502e7645343cb5ee94572bae33294
Umair Majeed <umairbmajeed@gmail.com>
2022-02-03 07:03:33 +0000

    fix tests + PSR

--- tests/unit/Jobs/Sync/FloatingIp/UpdateTest.php
+++ tests/unit/Jobs/Sync/FloatingIp/UpdateTest.php
@@ -34,7 +34,7 @@ class UpdateTest extends TestCase
         $job->handle();
 
         Bus::assertBatched(function (PendingBatch $batch) {
-            return $batch->jobs->count() == 1 && count($batch->jobs->all()[0]) == 1;
+            return $batch->jobs->count() == 1 && count($batch->jobs->all()[0]) == 2;
         });
     }
 }
