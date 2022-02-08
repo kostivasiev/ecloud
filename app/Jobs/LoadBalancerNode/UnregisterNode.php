c33343b6110339a63279d0d0556de53ae73a1bbc
Umair Majeed <umairbmajeed@gmail.com>
2022-02-08 16:51:31 +0000

    Removed conditional as suggested in https://gitlab.devops.ukfast.co.uk/ukfast/api.ukfast/ecloud/-/merge_requests/1138#note_884185
    #1328 update delete node sync job

--- app/Jobs/LoadBalancerNode/UnregisterNode.php
+++ app/Jobs/LoadBalancerNode/UnregisterNode.php
@@ -15,28 +15,25 @@ class UnregisterNode extends TaskJob
     {
         $loadBalancerNode = $this->task->resource;
 
-        if (empty($this->task->data['load_balancer_node_id'])) {
-            $client = app()->make(AdminClient::class)
-                ->setResellerId($loadBalancerNode->loadBalancer->getResellerId());
-            try {
-                $client->nodes()->deleteById($loadBalancerNode->node_id);
-            } catch (ApiException $e) {
-                if ($e->getStatusCode() !== 404) {
-                    throw $e;
-                }
-                $this->info('Loadbalancer node not found, skipping', [
-                    'id' => $loadBalancerNode->id,
-                    'instance_id' => $loadBalancerNode->instance_id,
-                    'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
-                ]);
-                return;
+        $client = app()->make(AdminClient::class)
+            ->setResellerId($loadBalancerNode->loadBalancer->getResellerId());
+        try {
+            $client->nodes()->deleteById($loadBalancerNode->node_id);
+        } catch (ApiException $e) {
+            if ($e->getStatusCode() !== 404) {
+                throw $e;
             }
-            $this->info('Unregistered instance as loadbalancer node', [
+            $this->info('Loadbalancer node not found, skipping', [
                 'id' => $loadBalancerNode->id,
                 'instance_id' => $loadBalancerNode->instance_id,
                 'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
             ]);
-            $this->task->updateData('load_balancer_node_id', $loadBalancerNode->node_id);
+            return;
         }
+        $this->info('Unregistered instance as loadbalancer node', [
+            'id' => $loadBalancerNode->id,
+            'instance_id' => $loadBalancerNode->instance_id,
+            'cluster_id' => $loadBalancerNode->loadBalancer->config_id,
+        ]);
     }
 }
