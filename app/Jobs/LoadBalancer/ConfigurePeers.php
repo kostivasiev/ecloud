937a22ae0f36e88f1dc7214ff0e0b4d93b8a60fc
Umair Majeed <umairbmajeed@gmail.com>
2022-02-10 15:04:44 +0000

    Uncommented job, simplified implementation and tweaked test.
    #1402 Reinstate ConfigurePeers job

--- app/Jobs/LoadBalancer/ConfigurePeers.php
+++ app/Jobs/LoadBalancer/ConfigurePeers.php
@@ -10,18 +10,8 @@ class ConfigurePeers extends TaskJob
     public function handle()
     {
         $loadbalancer = $this->task->resource;
-        if ($loadbalancer->config_id === null) {
-            $this->info('Loadbalancer has not been assigned a cluster id, skipping', [
-                'cluster_id' => $loadbalancer->config_id,
-            ]);
-            return;
-        }
         $client = app()->make(AdminClient::class)
             ->setResellerId($loadbalancer->getResellerId());
-        $response = $client->clusters()->configurePeers($loadbalancer->config_id);
-        if (!$response) {
-            $this->fail(new \Exception('Loadbalancer failed to configure it\'s peers'));
-            return;
-        }
+        $client->clusters()->configurePeers($loadbalancer->config_id);
     }
 }
