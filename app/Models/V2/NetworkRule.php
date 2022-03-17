2c70c5fa0b84a6d187a7eb8901506146eaed095c
Umair Majeed <umairbmajeed@gmail.com>
2022-03-17 16:01:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/Models/V2/NetworkRule.php
+++ app/Models/V2/NetworkRule.php
@@ -5,7 +5,6 @@ namespace App\Models\V2;
 use App\Traits\V2\CustomKey;
 use App\Traits\V2\DefaultName;
 use App\Traits\V2\DeletionRules;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
