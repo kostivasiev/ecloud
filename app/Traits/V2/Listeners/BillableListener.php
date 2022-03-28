cda0c9be80f605e2a59b7209980fc004e88fdc55
Umair Majeed <umairbmajeed@gmail.com>
2022-03-28 12:47:09 +0100

    Tweaked BillableListener for use with billing items that check deleted event, and to allow the RESOURCE constant to be an array.
    #1257 Update billing listeners to use BillableListener trait

--- app/Traits/V2/Listeners/BillableListener.php
+++ app/Traits/V2/Listeners/BillableListener.php
@@ -14,11 +14,29 @@ trait BillableListener
             return false;
         }
 
+        return $this->validateEventStatusAndResource($event);
+    }
+
+    protected function validateNotDeletedResourceEvent(Updated $event): bool
+    {
+        if ($event->model->name == Sync::TASK_NAME_DELETE) {
+            return false;
+        }
+
+        return $this->validateEventStatusAndResource($event);
+    }
+
+    protected function validateEventStatusAndResource(Updated $event): bool
+    {
         if (!$event->model->completed) {
             return false;
         }
 
-        if (get_class($event->model->resource) != static::RESOURCE) {
+        if (is_array(static::RESOURCE)) {
+            if (!in_array($event->model->resource::class, static::RESOURCE)) {
+                return false;
+            }
+        } else if ($event->model->resource::class != static::RESOURCE) {
             return false;
         }
 
