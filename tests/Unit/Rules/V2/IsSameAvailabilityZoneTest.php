8076d330ed1047fdaf84ee43f8b19c0711ef67b5
Umair Majeed <umairbmajeed@gmail.com>
2022-03-29 14:04:01 +0100

    added another test for failing

--- tests/Unit/Rules/V2/IsSameAvailabilityZoneTest.php
+++ tests/Unit/Rules/V2/IsSameAvailabilityZoneTest.php
@@ -221,4 +221,19 @@ class IsSameAvailabilityZoneTest extends TestCase
 
         $this->assertTrue($result);
     }
+
+    public function testAssigningAvailabilityZoneModelFails()
+    {
+        $rule = new IsSameAvailabilityZone($this->availabilityZone2->id);
+
+        $floatingIp = FloatingIp::factory()->create([
+            'id' => 'fip-test',
+            'vpc_id' => $this->vpc()->id,
+            'availability_zone_id' => $this->availabilityZone()->id,
+        ]);
+
+        $result = $rule->passes('floating_ip_id', $floatingIp->id);
+
+        $this->assertFalse($result);
+    }
 }
