491cba298a1e388730be0d93cb1fffa165033778
Umair Majeed <umairbmajeed@gmail.com>
2022-04-05 14:49:49 +0100

    updated code to match instruxtions on ticket

--- app/Console/Commands/LogicMonitor/RegisterExistingInstancesWithLogicMonitor.php
+++ app/Console/Commands/LogicMonitor/RegisterExistingInstancesWithLogicMonitor.php
@@ -54,7 +54,18 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
 
         foreach ($routers as $router) {
             $network = $router->network();
-            // identify LM collector for target AZ from monitoring API
+
+            // check 'system' policy exists, if not then create
+            /** @var FirewallPolicy $firewallPolicy */
+            $systemPolicy = $router->whereHas('firewallPolicies', function ($query) {
+                $query->where('name', '=', 'System');
+            })->first();
+
+            if (!$systemPolicy) {
+                $this->createSystemPolicy($router);
+            }
+
+            // check for collector in routers AZ, if none then skip
             $client = $adminMonitoringClient
                 ->setResellerId($router->getResellerId());
 
@@ -75,6 +86,26 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
                 break;
             }
 
+            // check LM rule exists, if does then skip
+            if (!$this->hasRule($firewallPolicy, $this->policyRules[0])) {
+                foreach ($this->policyRules as $rule) {
+                    $firewallRule = app()->make(FirewallRule::class);
+                    $firewallRule->fill($rule);
+                    $firewallRule->source = $ipAddresses;
+                    $firewallRule->firewallPolicy()->associate($firewallPolicy);
+                    $firewallRule->save();
+
+                    foreach ($rule['ports'] as $port) {
+                        $firewallRulePort = app()->make(FirewallRulePort::class);
+                        $firewallRulePort->fill($port);
+                        $firewallRulePort->firewallRule()->associate($firewallRule);
+                        $firewallRulePort->save();
+                    }
+                    $firewallPolicy->syncSave();
+                }
+            }
+
+            // create LM rule to open ports inbound from collectors IP
             foreach ($network->policies() as $policy) {
                 $ipAddresses = [];
                 foreach ($collectors as $collector) {
@@ -99,51 +130,23 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
                     }
                 }
             }
-
-            // Create firewall rule in system policy allowing inbound traffic from the collector
-            /** @var FirewallPolicy $firewallPolicy */
-            $firewallPolicy = $router->firewallPolicies()->where('name', '=', 'System')->first();
-
-            if (!$firewallPolicy) {
-                $firewallPolicy = $this->createSystemPolicy($router);
-            }
-
-            //  check if these rules exist, if they do, dont make
-            if (!$this->hasRule($firewallPolicy, $this->policyRules[0])) {
-                foreach ($this->policyRules as $rule) {
-                    $firewallRule = app()->make(FirewallRule::class);
-                    $firewallRule->fill($rule);
-                    $firewallRule->source = $ipAddresses;
-                    $firewallRule->firewallPolicy()->associate($firewallPolicy);
-                    $firewallRule->save();
-
-                    foreach ($rule['ports'] as $port) {
-                        $firewallRulePort = app()->make(FirewallRulePort::class);
-                        $firewallRulePort->fill($port);
-                        $firewallRulePort->firewallRule()->associate($firewallRule);
-                        $firewallRulePort->save();
-                    }
-                    $firewallPolicy->syncSave();
-                }
-            }
         }
 
         $instances = Instance::withoutTrashed()->get();
 
         foreach ($instances as $instance) {
-            if (empty($instance->deploy_data['floating_ip_id'])) {
-                Log::info(get_class($this) . ' : No floating IP assigned to the instance, skipping');
-                break;
-            }
-            $floatingIp = FloatingIp::find($instance->deploy_data['floating_ip_id']);
-            if (!$floatingIp) {
-                Log::error(new \Exception('Failed to load floating IP for instance', [
-                    'instance_id' => $instance->id,
-                    'floating_ip_id' => $instance->deploy_data['floating_ip_id']
-                ]));
+            // check if device is registered, if so then skip
+            $device = $adminMonitoringClient->devices()->getAll([
+                'reference_type' => 'server',
+                'reference_id:eq' => $instance->id
+            ]);
+
+            if (!empty($device)) {
+                Log::info($this::class . ' : The device is already registered, skipping');
                 break;
             }
 
+            // check account exists for customer, if not then create
             $accounts = $adminMonitoringClient->setResellerId($instance->vpc->reseller_id)->accounts()->getAll();
             if (!empty($accounts)) {
                 $this->saveLogicMonitorAccountId($accounts[0]->id, $instance);
@@ -168,17 +171,22 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
             $this->saveLogicMonitorAccountId($id, $instance);
             Log::info($this::class . ' : Logic Monitor account created: ' . $id);
 
-            $device = $adminMonitoringClient->devices()->getAll([
-                'reference_type' => 'server',
-                'reference_id:eq' => $instance->id
-            ]);
-
-            if (!empty($device)) {
-                Log::info($this::class . ' : The device is already registered, skipping');
+            // check if fIP assigned, if not then skip
+            if (empty($instance->deploy_data['floating_ip_id'])) {
+                Log::info(get_class($this) . ' : No floating IP assigned to the instance, skipping');
+                break;
+            }
+            $floatingIp = FloatingIp::find($instance->deploy_data['floating_ip_id']);
+            if (!$floatingIp) {
+                Log::error(new \Exception('Failed to load floating IP for instance', [
+                    'instance_id' => $instance->id,
+                    'floating_ip_id' => $instance->deploy_data['floating_ip_id']
+                ]));
                 break;
             }
 
-            // Load the collector for the availability zone the instance is deploying into
+
+            // check if LM creds exist, if not create
             $availabilityZone = $instance->availabilityZone;
 
             $collectorsPage = $adminMonitoringClient->collectors()->getPage(1, 15, [
@@ -197,10 +205,12 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
                 ->where('username', 'lm.' . $instance->id)
                 ->first();
             if (!$logicMonitorCredentials) {
+                //TODO: Create LogicMonitorCredentials
                 Log::error(new \Exception('Failed to load logic monitor credentials for instance ' . $instance->id));
                 break;
             }
 
+            // register device
             $response = $adminMonitoringClient->devices()->createEntity(new Device([
                 'reference_type' => 'server',
                 'reference_id' => $instance->id,
@@ -240,7 +250,14 @@ class RegisterExistingInstancesWithLogicMonitor extends Command
 
     private function createSystemPolicy($router)
     {
-        //TODO: Unsure what default system policy details are.
+        $policyConfig = config('firewall.system');
+
+        $systemPolicy = app()->make(FirewallPolicy::class);
+        $systemPolicy->fill($policyConfig);
+        $systemPolicy->router_id = $router->id;
+        $systemPolicy->syncSave();
+
+        return $systemPolicy;
     }
 
     /**
