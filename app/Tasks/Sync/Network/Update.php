a4341d780f26fb5cc662bb1fabb5f2c3e1de80ac
Umair Majeed <umairbmajeed@gmail.com>
2022-04-06 11:05:08 +0100

    Removed a require that is no longer being used.
    #1463 add logicmonitor job to instance deploy

--- app/Tasks/Sync/Network/Update.php
+++ app/Tasks/Sync/Network/Update.php
@@ -3,7 +3,6 @@
 namespace App\Tasks\Sync\Network;
 
 use App\Jobs\Network\AwaitRouterSync;
-use App\Jobs\Network\CreateSystemRules;
 use App\Jobs\Network\Deploy;
 use App\Jobs\Network\DeployDiscoveryProfile;
 use App\Jobs\Network\DeploySecurityProfile;
