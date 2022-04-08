80a3976a352eedf1a335bab75453276f946a4424
Umair Majeed <umairbmajeed@gmail.com>
2022-04-08 09:58:17 +0100

    send test emails command

---
+++ app/Console/Commands/SendTestEmails.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Mail\Mailer;
+
+class SendTestEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(Mailer $mailer)
+    {
+        $addresses = [
+            config('alerts.billing.to'),
+            config('alerts.capacity.default.to'),
+            config('alerts.capacity.floating_ip.to'),
+            config('alerts.capacity.floating_ip.cc'),
+            config('alerts.health.to'),
+            config('alerts.capacity.dev.to'),
+        ];
+        $text = 'Test';
+        $mailer->raw($text, function ($m) use ($addresses) {
+            $m->to($addresses)->subject("Test Email");
+        });
+
+        return 0;
+    }
+}
