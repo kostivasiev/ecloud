b896779f3463b01a4f38b6dee64ab8614fe3e7b5
Umair Majeed <umairbmajeed@gmail.com>
2022-04-25 09:31:19 +0100

    added migration

--- database/migrations/2022_04_25_082749_drop_ip_address_from_nics.php
+++ database/migrations/2022_04_25_082749_drop_ip_address_from_nics.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('nics', function (Blueprint $table) {
+        Schema::connection('ecloud')->table('nics', function (Blueprint $table) {
             $table->dropColumn('ip_address');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('nics', function (Blueprint $table) {
+        Schema::connection('ecloud')->table('nics', function (Blueprint $table) {
             $table->ipAddress('ip_address')->after('network_id')->nullable();
         });
     }
