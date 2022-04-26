d8a28571f324d8c3e0af8acd91c646d825751af6
Umair Majeed <umairbmajeed@gmail.com>
2022-04-26 14:29:02 +0100

    Added migration to add indexes to instance_volume pivot table
    #1489 Advanced Ditto Replacement

---
+++ database/migrations/2022_04_26_132235_add_indexes_to_instance_volume_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instance_volume', function (Blueprint $table) {
+            $table->index('instance_id', 'instance_volume_instance_id');
+            $table->index('volume_id', 'instance_volume_volume_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instance_volume', function (Blueprint $table) {
+            $table->dropIndex('instance_volume_instance_id');
+            $table->dropIndex('instance_volume_volume_id');
+        });
+    }
+};
