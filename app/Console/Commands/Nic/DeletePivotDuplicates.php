5c5884f17fbb6d12fa5cb29dcfeec79210a50c31
Umair Majeed <umairbmajeed@gmail.com>
2022-05-12 16:21:26 +0100

    delete duplicates from ip_address_nic pivot

---
+++ app/Console/Commands/Nic/DeletePivotDuplicates.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands\Nic;
+
+use App\Models\V2\Nic;
+use App\Console\Commands\Command;
+
+class DeletePivotDuplicates extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nic:delete-pivot-duplicates {--T|test-run}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete pivot duplicates';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $updated = 0;
+
+        Nic::all()->each(function ($nic) use ($updated) {
+            $records = $nic->ipAddresses()->pluck('id');
+            $unique = $records->unique();
+
+            if ($unique->count() != $records->count()) {
+                $this->info('Removing duplicates for ' . $nic->id);
+
+                if (!$this->option('test-run')) {
+                    $nic->ipAddresses()->sync([]);
+                    $nic->ipAddresses()->sync($records->unique()->toArray());
+                }
+                $updated++;
+            }
+
+            $this->info($updated . ' records updated');
+        });
+
+        return 0;
+    }
+}
