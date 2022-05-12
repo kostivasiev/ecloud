b4257763bba0a30302525d3bde959d38e338c5b9
Umair Majeed <umairbmajeed@gmail.com>
2022-05-12 09:54:42 +0100

    removed instance seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,6 @@ class DatabaseSeeder extends Seeder
         $this->call(VpcSeeder::class);
         $this->call(RouterSeeder::class);
         $this->call(NetworkSeeder::class);
-        $this->call(InstanceSeeder::class);
     }
 }
 
