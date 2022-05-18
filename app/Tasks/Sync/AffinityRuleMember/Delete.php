ee1476723dfec6d12b8d129f643f18c1f02ec3e5
Umair Majeed <umairbmajeed@gmail.com>
2022-05-18 09:58:49 +0100

    added jobs to delete for AffinityRuleMember

--- app/Tasks/Sync/AffinityRuleMember/Delete.php
+++ app/Tasks/Sync/AffinityRuleMember/Delete.php
@@ -2,6 +2,11 @@
 
 namespace App\Tasks\Sync\AffinityRuleMember;
 
+use App\Jobs\AffinityRule\AwaitRuleCreation;
+use App\Jobs\AffinityRule\AwaitRuleDeletion;
+use App\Jobs\AffinityRule\CheckMemberState;
+use App\Jobs\AffinityRule\CreateAffinityRule;
+use App\Jobs\AffinityRule\DeleteExistingRule;
 use App\Tasks\Task;
 
 class Delete extends Task
@@ -9,6 +14,11 @@ class Delete extends Task
     public function jobs()
     {
         return [
+            CheckMemberState::class,
+            DeleteExistingRule::class,
+            AwaitRuleDeletion::class,
+            CreateAffinityRule::class,
+            AwaitRuleCreation::class,
         ];
     }
 }
