9e2076bcc7abce744ae17f81d2a3c43e2fde7c88
Umair Majeed <umairbmajeed@gmail.com>
2022-05-18 11:42:58 +0100

    removed unneeded

--- app/Tasks/Sync/AffinityRuleMember/Delete.php
+++ app/Tasks/Sync/AffinityRuleMember/Delete.php
@@ -2,11 +2,6 @@
 
 namespace App\Tasks\Sync\AffinityRuleMember;
 
-use App\Jobs\AffinityRule\AwaitRuleCreation;
-use App\Jobs\AffinityRule\AwaitRuleDeletion;
-use App\Jobs\AffinityRule\CheckMemberState;
-use App\Jobs\AffinityRule\CreateAffinityRule;
-use App\Jobs\AffinityRule\DeleteExistingRule;
 use App\Tasks\Task;
 
 class Delete extends Task
@@ -14,11 +9,6 @@ class Delete extends Task
     public function jobs()
     {
         return [
-            CheckMemberState::class,
-            DeleteExistingRule::class,
-            AwaitRuleDeletion::class,
-            CreateAffinityRule::class,
-            AwaitRuleCreation::class,
         ];
     }
 }
