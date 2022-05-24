320faedbced437e9cf388d72abaf66c538e19e62
Umair Majeed <umairbmajeed@gmail.com>
2022-05-24 15:01:54 +0100

    fix test

--- app/Jobs/Nsx/Nic/CreateDHCPLease.php
+++ app/Jobs/Nsx/Nic/CreateDHCPLease.php
@@ -15,28 +15,28 @@ class CreateDHCPLease extends TaskJob
         $router = $nic->network->router;
         $nsxService = $router->availabilityZone->nsxService();
 
-        if (!$nic->ipAddresses()->withType(IpAddress::TYPE_DHCP)->exists()) {
-            /**
-             * Get DHCP static bindings to determine used IP addresses on the network
-             * @see https://185.197.63.88/policy/api_includes/method_ListSegmentDhcpStaticBinding.html
-             */
-            $cursor = null;
-            $assignedIpsNsx = collect();
-            do {
-                $response = $nsxService->get('/policy/api/v1/infra/tier-1s/' . $router->id . '/segments/' . $network->id . '/dhcp-static-binding-configs?cursor=' . $cursor);
-                $response = json_decode($response->getBody()->getContents());
-                foreach ($response->results as $dhcpStaticBindingConfig) {
-                    if (!empty($nic->ip_address)
-                        && $dhcpStaticBindingConfig->id == $nic->id
-                        && $dhcpStaticBindingConfig->ip_address == $nic->ip_address) {
-                        Log::info("DHCP IP address already assigned, skipping");
-                        return true;
-                    }
-                    $assignedIpsNsx->push($dhcpStaticBindingConfig->ip_address);
+        /**
+         * Get DHCP static bindings to determine used IP addresses on the network
+         * @see https://185.197.63.88/policy/api_includes/method_ListSegmentDhcpStaticBinding.html
+         */
+        $cursor = null;
+        $assignedIpsNsx = collect();
+        do {
+            $response = $nsxService->get('/policy/api/v1/infra/tier-1s/' . $router->id . '/segments/' . $network->id . '/dhcp-static-binding-configs?cursor=' . $cursor);
+            $response = json_decode($response->getBody()->getContents());
+            foreach ($response->results as $dhcpStaticBindingConfig) {
+                if (!empty($nic->ip_address)
+                    && $dhcpStaticBindingConfig->id == $nic->id
+                    && $dhcpStaticBindingConfig->ip_address == $nic->ip_address) {
+                    Log::info("DHCP IP address already assigned, skipping");
+                    return true;
                 }
-                $cursor = $response->cursor ?? null;
-            } while (!empty($cursor));
+                $assignedIpsNsx->push($dhcpStaticBindingConfig->ip_address);
+            }
+            $cursor = $response->cursor ?? null;
+        } while (!empty($cursor));
 
+        if (!$nic->ipAddresses()->withType(IpAddress::TYPE_DHCP)->exists()) {
             $ipAddress = $nic->assignIpAddress($assignedIpsNsx->toArray(), IpAddress::TYPE_DHCP);
         } else {
             $ipAddress = $nic->ipAddresses()->withType(IpAddress::TYPE_DHCP)->first();
