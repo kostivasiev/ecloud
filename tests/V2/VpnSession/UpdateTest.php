eb5f05884134fd5e1924a8b7a285e042d48cda96
Umair Majeed <umairbmajeed@gmail.com>
2022-05-30 15:18:55 +0100

    Update tests/V2/VpnSession/UpdateTest.php

--- tests/V2/VpnSession/UpdateTest.php
+++ tests/V2/VpnSession/UpdateTest.php
@@ -169,7 +169,7 @@ class UpdateTest extends TestCase
                 'psk' => $this->preSharedKey,
             ]
         );
-        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals(202, $response->getStatusCode());
         Event::assertDispatched(Created::class);
 
         $this->vpnSession->refresh();
