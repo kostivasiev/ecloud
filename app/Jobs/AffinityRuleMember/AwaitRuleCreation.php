8b9780e01a4f04063e8604de8c12c76a368ee264
Umair Majeed <umairbmajeed@gmail.com>
2022-06-07 08:31:52 +0100

    Removed looping elements and instances of Log::info, tweaked tests to cope with changes.
    #1537 Job to remove member from vmware group and delete group (if not needed)

--- app/Jobs/AffinityRuleMember/AwaitRuleCreation.php
+++ app/Jobs/AffinityRuleMember/AwaitRuleCreation.php
@@ -2,30 +2,20 @@
 
 namespace App\Jobs\AffinityRuleMember;
 
-use Illuminate\Support\Facades\Log;
-
 class AwaitRuleCreation extends AwaitRuleDeletion
 {
     public function handle()
     {
-        if (empty($this->task->data['created_rules'])) {
-            Log::info('No rules to create, skipping', [
+        $hostGroupId = $this->affinityRuleMember->instance->getHostGroupId();
+        if (!$this->affinityRuleExists($hostGroupId)) {
+            $this->info('Rule creation not complete, waiting', [
                 'affinity_rule_id' => $this->affinityRuleMember->affinityRule->id,
+                'host_group_id' => $hostGroupId
             ]);
+            $this->release($this->backoff);
             return;
         }
-        $createdRules = $this->task->data['created_rules'];
-        foreach ($createdRules as $hostGroupId) {
-            if (!$this->affinityRuleExists($hostGroupId)) {
-                Log::info('Rule creation not complete, waiting', [
-                    'affinity_rule_id' => $this->affinityRuleMember->affinityRule->id,
-                    'host_group_id' => $hostGroupId
-                ]);
-                $this->release($this->backoff);
-                return;
-            }
-        }
-        Log::info('Rule creation complete', [
+        $this->info('Rule creation complete', [
             'affinity_rule_id' => $this->affinityRuleMember->affinityRule->id,
         ]);
     }
