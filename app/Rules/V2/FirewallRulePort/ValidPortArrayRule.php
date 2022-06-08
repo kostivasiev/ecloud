720d8318dbf54eba800b3fc807fe9462deec274c
Umair Majeed <umairbmajeed@gmail.com>
2022-06-08 16:11:32 +0100

    Removed port range validations from the rule endpoint which were missed previously.
    #1574 Can't add port ranges to rules on Routers in eCloud VPC

--- app/Rules/V2/FirewallRulePort/ValidPortArrayRule.php
+++ app/Rules/V2/FirewallRulePort/ValidPortArrayRule.php
@@ -31,10 +31,6 @@ class ValidPortArrayRule implements Rule
                 $this->isValidPortSourceDestination($port['destination']))) {
                 return false;
             }
-            if (!$this->isUniquePortRange($ports, $port, 'source') ||
-                !$this->isUniquePortRange($ports, $port, 'destination')) {
-                return false;
-            }
         }
         return true;
     }
@@ -65,45 +61,5 @@ class ValidPortArrayRule implements Rule
     {
         $rule = new ValidFirewallRulePortSourceDestination();
         return $rule->passes('', $port);
-    }
-
-    private function isUniquePortRange(Collection $ports, array $item, string $property): bool
-    {
-        $matchCount = 0;
-        $itemParts = explode('-', $item[$property]);
-        if (count($itemParts) < 2) {
-            return true;
-        }
-
-        foreach ($ports as $port) {
-            $destinationParts = explode('-', $port['destination']);
-            $sourceParts = explode('-', $port['source']);
-
-            if ($port['protocol'] !== $item['protocol']) {
-                return true;
-            }
-
-            if (count($destinationParts) > 1 && count($sourceParts) > 1) {
-                if ((($sourceParts[0] >= $itemParts[0] && $itemParts[1] <= $sourceParts[1]) ||
-                    ($sourceParts[0] >= $itemParts[0] && $sourceParts[1] <= $itemParts[1])) &&
-                    (($destinationParts[0] >= $itemParts[0] && $itemParts[1] <= $destinationParts[1]) ||
-                    ($destinationParts[0] >= $itemParts[0] && $destinationParts[1] <= $itemParts[1]))
-                ) {
-                    var_dump('Total Match');
-                    $matchCount++;
-                } elseif (($sourceParts[0] >= $itemParts[0] && $itemParts[0] <= $sourceParts[1]) &&
-                    ($sourceParts[0] >= $itemParts[1] && $sourceParts[1] <= $itemParts[1])
-                ) {
-                    var_dump('Source Match');
-                    $matchCount++;
-                } elseif (($destinationParts[0] >= $itemParts[0] && $itemParts[0] <= $destinationParts[1]) &&
-                    ($destinationParts[0] >= $itemParts[1] && $destinationParts[1] <= $itemParts[1])
-                ) {
-                    var_dump('Destination Match');
-                    $matchCount++;
-                }
-            }
-        }
-        return ($matchCount > 1);
     }
 }
