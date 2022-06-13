c075af6f6cdd5d620eaecf23361be692bad8d4a8
Umair Majeed <umairbmajeed@gmail.com>
2022-06-13 13:33:06 +0100

    Added seeder and http request tests
    #1484 affinity rule check when host group changed

---
+++ database/seeders/HostSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\V2\AvailabilityZone;
+use App\Models\V2\Host;
+use App\Models\V2\HostGroup;
+use App\Models\V2\HostSpec;
+use App\Models\V2\Vpc;
+use Illuminate\Database\Seeder;
+
+class HostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        HostGroup::withoutEvents(function () {
+            $hostSpec = HostSpec::factory()
+                ->create([
+                    'id' => 'hs-aaaaaaaa',
+                ]);
+            $hostGroup = HostGroup::factory()
+                ->for(Vpc::find('vpc-aaaaaaaa'))
+                ->for(AvailabilityZone::find('az-aaaaaaaa'))
+                ->for($hostSpec)
+                ->create([
+                    'id' => 'hg-aaaaaaaa',
+                ]);
+            Host::factory()
+                ->for($hostGroup)
+                ->create([
+                    'id' => 'h-aaaaaaaa',
+                    'name' => 'Test Host',
+                    'mac_address' => '00:00:5e:00:53:af',
+                ]);
+        });
+    }
+}
