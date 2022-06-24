bae40777ca960b465b1f7d0cf9a041123b5ac791
Umair Majeed <umairbmajeed@gmail.com>
2022-06-24 09:30:33 +0100

    Updated HostSeeder to incorporate changes in ResourceTierSeeder, and removed that other seeder.
    #1485 confim hostgroup has availability

--- database/seeders/HostSeeder.php
+++ database/seeders/HostSeeder.php
@@ -6,11 +6,15 @@ use App\Models\V2\AvailabilityZone;
 use App\Models\V2\Host;
 use App\Models\V2\HostGroup;
 use App\Models\V2\HostSpec;
+use App\Models\V2\ResourceTier;
 use App\Models\V2\Vpc;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class HostSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      *
@@ -18,26 +22,32 @@ class HostSeeder extends Seeder
      */
     public function run()
     {
-        HostGroup::withoutEvents(function () {
-            $hostSpec = HostSpec::factory()
-                ->create([
-                    'id' => 'hs-aaaaaaaa',
-                    'ucs_specification_name' => 'DUAL-4208--32GB',
-                ]);
-            $hostGroup = HostGroup::factory()
-                ->for(Vpc::find('vpc-aaaaaaaa'))
-                ->for(AvailabilityZone::find('az-aaaaaaaa'))
-                ->for($hostSpec)
-                ->create([
-                    'id' => 'hg-aaaaaaaa',
-                ]);
-            Host::factory()
-                ->for($hostGroup)
-                ->create([
-                    'id' => 'h-aaaaaaaa',
-                    'name' => 'Test Host',
-                    'mac_address' => '00:00:5e:00:53:af',
-                ]);
-        });
+        $availabilityZone = AvailabilityZone::find('az-aaaaaaaa');
+
+        $hostSpec = HostSpec::factory()
+            ->create([
+                'id' => 'hs-aaaaaaaa',
+                'ucs_specification_name' => 'DUAL-4208--32GB',
+            ]);
+        $availabilityZone->hostSpecs()->sync($hostSpec);
+
+        $hostGroup = HostGroup::factory()
+            ->for(Vpc::find('vpc-aaaaaaaa'))
+            ->for($availabilityZone)
+            ->for($hostSpec)
+            ->create([
+                'id' => 'hg-aaaaaaaa',
+            ]);
+
+        Host::factory()
+            ->for($hostGroup)
+            ->create([
+                'id' => 'h-aaaaaaaa',
+                'name' => 'Test Host',
+                'mac_address' => '00:00:5e:00:53:af',
+            ]);
+
+        $resourceTier = ResourceTier::find('rt-123456');
+        $resourceTier->hostGroups()->attach($hostGroup);
     }
 }
