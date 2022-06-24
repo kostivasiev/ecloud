5d71f784708e500d911c8299f744580c57640902
Umair Majeed <umairbmajeed@gmail.com>
2022-06-24 14:17:05 +0100

    Command removed but entry still in the kernel
    #1483 update existing instances to shared hostgroup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,6 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Health\FindOrphanedNics::class,
         \App\Console\Commands\Image\Populate::class,
         \App\Console\Commands\Instance\SetHostGroupToStandard::class,
-        \App\Console\Commands\IpAddress\PopulateNetworkId::class,
         \App\Console\Commands\Kingpin\TestAuth::class,
         \App\Console\Commands\Make\MakeTaskJob::class,
         \App\Console\Commands\Make\MakeTest::class,
