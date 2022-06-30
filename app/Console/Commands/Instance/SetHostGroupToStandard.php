5aaa38a668162848a56699d939f6faa8ba5388ea
Umair Majeed <umairbmajeed@gmail.com>
2022-06-30 09:53:25 +0100

    Updated and simplified. Requires the changes in #1591
    #1483 update existing instances to shared hostgroup

--- app/Console/Commands/Instance/SetHostGroupToStandard.php
+++ app/Console/Commands/Instance/SetHostGroupToStandard.php
@@ -17,26 +17,14 @@ class SetHostGroupToStandard extends Command
             $query->whereNull('host_group_id');
             $query->orWhere('host_group_id', '=', '');
         })->each(function (Instance $instance) {
-            $hostGroupId = $instance->getHostGroupId();
-            $hostGroup = HostGroup::find($instance->getHostGroupId());
-            if (!$hostGroup) {
-                $this->info('Creating hostgroup `' . $hostGroupId . '`');
+            if ($instance->host_group_id === null) {
+                $hostGroup = $instance->availabilityZone->getDefaultHostGroup();
+
+                $this->info('Assigning hostgroup ' . $hostGroup->id . ' to ' . $instance->id);
                 if (!$this->option('test-run')) {
-                    $hostGroup = new HostGroup([
-                        'id' => $hostGroupId,
-                        'vpc_id' => $instance->vpc->id,
-                        'availability_zone_id' => $instance->availabilityZone->id,
-                        'host_spec_id' => 'hs-test', // <--- need to determine this
-                        'windows_enabled' => !($instance->platform == 'Linux'),
-                    ]);
-                    $hostGroup->save();
+                    $instance->hostGroup()->associate($hostGroup);
+                    $instance->save();
                 }
-            }
-            // Now we have a hostgroup, we need to attach it to the instance
-            $this->info('Assigning hostgroup ' . $hostGroupId . ' to ' . $instance->id);
-            if (!$this->option('test-run')) {
-                $instance->hostGroup()->associate($hostGroup);
-                $instance->save();
             }
         });
     }
