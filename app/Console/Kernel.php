5e2607ab3b856403b07f10eeb0e7fe7e826667c5
Umair Majeed <umairbmajeed@gmail.com>
2022-07-06 16:26:48 +0100

    Added artisan command and runner configuration to run the backfill over http using artisan-runner
    #1608 Back fill Fastdesk VPN's in to eCloud VPC DB

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FastDesk\BackfillVpn;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -22,6 +23,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\Conjurer\TestAuth::class,
         \App\Console\Commands\Credentials\Show::class,
         \App\Console\Commands\DiscountPlan\SendReminderEmails::class,
+        \App\Console\Commands\FastDesk\BackfillVpn::class,
         \App\Console\Commands\FloatingIp\PopulateForIpRange::class,
         \App\Console\Commands\Health\FindOrphanedNats::class,
         \App\Console\Commands\Health\FindOrphanedNics::class,
