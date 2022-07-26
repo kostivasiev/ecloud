597be86f74680b30740bbb966dfc9f9149cbcd0d
Umair Majeed <umairbmajeed@gmail.com>
2022-07-26 08:49:16 +0100

    Expanded validation types to include single ip's, subnets and ip ranges - all valid values for this endpoint.
    #1617 Firewall/network policies - IPv6 addresses allowed

--- app/Rules/V2/ValidateIpTypesAreConsistent.php
+++ app/Rules/V2/ValidateIpTypesAreConsistent.php
@@ -5,6 +5,7 @@ namespace App\Rules\V2;
 use Illuminate\Contracts\Validation\Rule;
 use IPLib\Address\IPv4;
 use IPLib\Address\IPv6;
+use IPLib\Factory;
 use IPLib\Range\Subnet;
 
 class ValidateIpTypesAreConsistent implements Rule
@@ -24,7 +25,10 @@ class ValidateIpTypesAreConsistent implements Rule
         if (empty($value) || empty($this->otherIpValue)) {
             return false;
         }
-        return (($this->isIPv4($value) && $this->isIPv4($this->otherIpValue)) ||
+
+        return (($this->isIPv4Subnet($value) && $this->isIPv4Subnet($this->otherIpValue)) ||
+            ($this->isIPv6Subnet($value) && $this->isIPv6Subnet($this->otherIpValue)) ||
+            ($this->isIPv4($value) && $this->isIPv4($this->otherIpValue)) ||
             ($this->isIPv6($value) && $this->isIPv6($this->otherIpValue))
         );
     }
@@ -34,13 +38,45 @@ class ValidateIpTypesAreConsistent implements Rule
         return 'The source and destination attributes must be of the same IP type IPv4/IPv6';
     }
 
-    public function isIPv4($ipAddressRange): bool
+    public function isIPv4($ipAddress): bool
     {
-        return get_class((Subnet::parseString($ipAddressRange))->getStartAddress()) == IPv4::class;
+        $parsed = Factory::parseAddressString($this->getFirstElementIfRange($ipAddress));
+        if ($parsed === null) {
+            return false;
+        }
+        return get_class($parsed) == IPv4::class;
     }
 
-    public function isIPv6($ipAddressRange): bool
+    public function isIPv4Subnet($ipAddressSubnet): bool
     {
-        return get_class((Subnet::parseString($ipAddressRange))->getStartAddress()) == IPv6::class;
+        $subnet = Subnet::parseString($ipAddressSubnet);
+        if ($subnet === null) {
+            return false;
+        }
+        return get_class($subnet->getStartAddress()) == IPv4::class;
+    }
+
+    public function isIPv6($ipAddress): bool
+    {
+        $parsed = Factory::parseAddressString($this->getFirstElementIfRange($ipAddress));
+        if ($parsed === null) {
+            return false;
+        }
+        return get_class($parsed) == IPv6::class;
+    }
+
+    public function isIPv6Subnet($ipAddressSubnet): bool
+    {
+        $subnet = Subnet::parseString($ipAddressSubnet);
+        if ($subnet === null) {
+            return false;
+        }
+        return get_class($subnet->getStartAddress()) == IPv6::class;
+    }
+
+    public function getFirstElementIfRange($ipAddressRange): string
+    {
+        $parts = explode('-', $ipAddressRange);
+        return (count($parts) > 1) ? $parts[0] : $ipAddressRange;
     }
 }
--- tests/Unit/Rules/V2/ValidateIpTypesAreConsistentTest.php
+++ tests/Unit/Rules/V2/ValidateIpTypesAreConsistentTest.php
@@ -17,31 +17,79 @@ class ValidateIpTypesAreConsistentTest extends TestCase
 
     public function testIPv4IsConsistent()
     {
+        // IP
+        $this->rule->otherIpValue = '192.168.10.12';
+        $this->assertTrue($this->rule->passes('source', '10.1.1.0'));
+
+        // Subnet
         $this->rule->otherIpValue = '192.168.10.12/24';
         $this->assertTrue($this->rule->passes('source', '10.1.1.0/24'));
+
+        // Range
+        $this->rule->otherIpValue = '192.168.10.12-192.168.10.122';
+        $this->assertTrue($this->rule->passes('source', '10.1.1.0-10.1.1.100'));
     }
 
     public function testIpv6IsConsistent()
     {
+        // IP
+        $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f';
+        $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f'));
+
+        // Subnet
         $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f/24';
         $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f/24'));
+
+        // Range
+        $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f-78a6:9d0e:1937:ce40:312c:6718:0f98:ffff';
+        $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f-3492:e2e1:3616:951f:ddfb:295f:d6ba:ffff'));
     }
 
     public function testIpMismatchesFail()
     {
+        // IP
+        $this->rule->otherIpValue = '192.168.10.12';
+        $this->assertFalse($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f/24'));
+
+        $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f';
+        $this->assertFalse($this->rule->passes('source', '10.1.1.0/24'));
+
+        // Subnet
         $this->rule->otherIpValue = '192.168.10.12/24';
         $this->assertFalse($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f/24'));
 
         $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f/24';
         $this->assertFalse($this->rule->passes('source', '10.1.1.0/24'));
+
+        // Range
+        $this->rule->otherIpValue = '192.168.10.12-192.168.10.122';
+        $this->assertFalse($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f/24'));
+
+        $this->rule->otherIpValue = '78a6:9d0e:1937:ce40:312c:6718:0f98:400f-78a6:9d0e:1937:ce40:312c:6718:0f98:ffff';
+        $this->assertFalse($this->rule->passes('source', '10.1.1.0/24'));
     }
 
     public function testSucceedsIfEitherValueIsAny()
     {
+        // Subnet
+        $this->rule->otherIpValue = 'ANY';
+        $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f'));
+
+        $this->rule->otherIpValue = '192.168.10.12';
+        $this->assertTrue($this->rule->passes('source', 'ANY'));
+
+        // Subnet
         $this->rule->otherIpValue = 'ANY';
         $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f/24'));
 
         $this->rule->otherIpValue = '192.168.10.12/24';
+        $this->assertTrue($this->rule->passes('source', 'ANY'));
+
+        // Range
+        $this->rule->otherIpValue = 'ANY';
+        $this->assertTrue($this->rule->passes('source', '3492:e2e1:3616:951f:ddfb:295f:d6ba:1c0f-3492:e2e1:3616:951f:ddfb:295f:d6ba:ffff'));
+
+        $this->rule->otherIpValue = '192.168.10.12-192.168.10.121';
         $this->assertTrue($this->rule->passes('source', 'ANY'));
     }
 }
