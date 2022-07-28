7a6881d9ab8a140b524a0bf79a466c6c041c2aba
Umair Majeed <umairbmajeed@gmail.com>
2022-07-28 11:31:38 +0100

    moved ip assignment check to nic delete task and out of listener

--- tests/Unit/Listeners/V2/Nic/NicEventSubscriberTest.php
+++ tests/Unit/Listeners/V2/Nic/NicEventSubscriberTest.php
@@ -49,32 +49,4 @@ class NicEventSubscriberTest extends TestCase
             return $event->model->name == Sync::TASK_NAME_DELETE;
         });
     }
-
-    public function testDeleteLoadBalancerNetworkNicsClusterIpAssigned()
-    {
-        Event::fake([Created::class]);
-
-        // Create a VIP and assign a cluster IP to it.
-        $this->vip()->assignClusterIp();
-
-        // Create a load balancer node and instance
-        $this->loadBalancerNode();
-
-        // Create a NIC on the instance/mode
-        $this->nic()->setAttribute('instance_id', $this->loadBalancerInstance()->id)->saveQuietly();
-
-        $this->assertCount(1, $this->loadBalancerInstance()->nics);
-
-        // Assign a DHCP address to the NIC
-        $this->nic()->assignIpAddress();
-
-        // Assign a cluster IP to the NIC
-        $this->nic()->ipAddresses()->save($this->vip()->ipAddress);
-
-        $task = $this->createSyncDeleteTask($this->loadBalancerNetwork());
-
-        $this->subscriber->handleTaskCreatedEvent(new Created($task));
-
-        Event::assertNotDispatched(Created::class);
-    }
 }
