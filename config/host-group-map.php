bc51d81507e27ed12ed3115765508563d89dc06b
Umair Majeed <umairbmajeed@gmail.com>
2022-08-08 15:06:12 +0100

    Update config/host-group-map.php

--- config/host-group-map.php
+++ config/host-group-map.php
@@ -6,9 +6,14 @@
 
 
 return [
-    // G0
+    // G0 Dev
     'az-aaaaaaaa' => [
         'hg-99f9b758' => '1001', // Standard CPU
+    ],
+
+    // G0
+    'az-7e7f811e' => [
+        'hg-99f9b758' => 'StandardCPU-01', // Standard CPU
     ],
 
     // MAN5 Manchester West
@@ -17,26 +22,26 @@ return [
         'hg-cf1bae59' => 'HighCPU-01',
     ],
 
+    // LON1 London Central
     'az-4fcc2a10' => [
-        // LON1 London Central
         'hg-89e95d15' => 'StandardCPU-01',
         'hg-add46c9a' => 'HighCPU-01',
     ],
 
+    // LON2 London West
     'az-b3cbcbe7' => [
-        // LON2 London West
         'hg-89ca4ae8' => 'StandardCPU-01',
         'hg-593e3885' => 'HighCPU-01',
     ],
 
+    // MAN4 Manchester South
     'az-4c31a488' => [
-        // MAN4 Manchester South
         'hg-56271b1e' => 'StandardCPU-01',
         'hg-a670426f' => 'HighCPU-01',
     ],
 
+    // AMS Amsterdam East
     'az-a75a74fa' => [
-        // AMS Amsterdam East
         'hg-ed7a097e' => 'StandardCPU-01',
         'hg-b2aeea8a' => 'HighCPU-01',
 //    '' => 'StandardCPU-Windows',
